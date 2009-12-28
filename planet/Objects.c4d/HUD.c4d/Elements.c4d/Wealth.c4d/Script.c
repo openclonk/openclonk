b474add38006f77e897a995a92d75721ab6daa5b
Tobias Zwick <newton@westnordost.de>
2009-12-28 23:16:09 +0100

    wealth display with more graphics

--- planet/Objects.c4d/HUD.c4d/Elements.c4d/Wealth.c4d/Script.c
+++ planet/Objects.c4d/HUD.c4d/Elements.c4d/Wealth.c4d/Script.c
@@ -12,4 +12,13 @@ public func Update()
 	var val = GetWealth(GetOwner());
 	
 	CustomMessage(Format("@%d",val), this, GetOwner(), 0, 75);
+	
+	var num;
+	if(val < 180) num = 4;
+	if(val < 120) num = 3;
+	if(val < 70) num = 2;
+	if(val < 30) num = 1;
+	if(val < 10) num = 0;
+	
+	SetGraphics(Format("%d",num));
 }
