e3553f8291c792ef51dcedb41b9bec323f703373
Sven Eberhardt <sven2@uni-bremen.de>
2010-01-03 20:18:25 +0100

    sky race

---
+++ planet/Skyrace.c4s/Script.c
@@ -0,0 +1,32 @@
+/* Sky race */
+
+func InitializePlayer(int plr)
+{
+  return JoinPlayer(plr);
+}
+
+private func JoinPlayer(int plr)
+{
+  var obj=GetCrew(plr);
+  obj->DoEnergy(100000);
+  obj->SetPosition(10+Random(50), LandscapeHeight()/2-15);
+  return true;
+}
+
+
+/* Relaunch */
+
+public func RelaunchPlayer(int plr)
+{
+  var clnk=CreateObject(CLNK,0,0,plr);
+  clnk->MakeCrewMember(plr);
+  SetCursor(plr,clnk);
+  SelectCrew(plr, clnk, 1);
+  Log(RndRelaunchMsg(), GetPlayerName(plr));
+  return JoinPlayer(plr);
+}
+
+private func RndRelaunchMsg()
+{
+  return Translate(Format("RelaunchMsg%d", Random(4)));
+}                           
