713818fca1c285beb9b2ade28fcfbbb18cf2c03b
Charles Spurrill <carlonfire@gmail.com>
2010-02-21 18:36:03 -0800

    added bubble effect
    The clonk likely needs an "enter-liquid" callback, though

---
+++ planet/Objects.c4d/Effects.c4d/Bubble.c4d/Script.c
@@ -0,0 +1,64 @@
+/*-- Bubble --*/
+
+local time;
+local alpha;
+
+global func Bubble(int iamount)
+{
+	if(iamount==nil || iamount==0) iamount=3;
+
+	CastObjects(BUBL,iamount);
+}
+
+protected func Initialize()
+{
+	alpha=255;
+	DoCon(RandomX(25,100));
+	AddEffect("Move",this,1,1,this);
+}
+
+public func FxMoveTimer(pTarget, iEffectNumber, iEffectTime)
+{
+	if(GBackLiquid(0,-3)==false)
+	{
+		AddEffect("Fade",pTarget,1,1,pTarget);
+	}
+
+	SetYDir(GetYDir()+Random(2));
+
+	if(Random(30)==1 && pTarget->GetCon()>100)
+	{
+		var i=3;
+		while(i>0)
+		{
+			i=--i;
+			var bubble=CreateObject(BUBL);
+			bubble->SetCon(pTarget->GetCon()/15*10);
+			bubble->SetYDir(pTarget->GetYDir());
+		}
+		RemoveObject();
+		return -1;
+	}
+
+	//Jittery movement
+	var dir;
+	if(Random(2)==1) dir=-1;
+	else	dir=1;
+	if(GetXDir()<6 && GetXDir()>-6)
+	{
+		SetXDir(GetXDir()+dir);
+	}
+}
+
+
+public func FxFadeTimer(pTarget)
+{
+	if(alpha<=0)
+	{
+		RemoveEffect("Move",this);
+		RemoveObject();
+		return -1;
+	}
+	SetClrModulation(RGBa(255,255,255,alpha));
+	alpha=alpha-5;
+}
