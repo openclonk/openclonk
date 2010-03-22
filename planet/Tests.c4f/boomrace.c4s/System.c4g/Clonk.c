f83bbb58842d4593d97615864142fb60d7e2b46a
Benjamin Herr <ben@0x539.de>
2010-03-23 00:02:29 +0100

    fix newlines in boomrace.c4s's Clonk's Script.c

--- planet/Tests.c4f/boomrace.c4s/System.c4g/Clonk.c
+++ planet/Tests.c4f/boomrace.c4s/System.c4g/Clonk.c
@@ -1,1 +1,12 @@
-#appendto Clonkprotected func RejectCollect(id objid, object obj){	if(objid != Boompack) return true;	return _inherited(objid, obj);}public func MaxContentsCount() { return 1; }
+#appendto Clonk
+
+protected func RejectCollect(id objid, object obj)
+{
+	if(objid != Boompack) return true;
+	return _inherited(objid, obj);
+}
+
+public func MaxContentsCount()
+{
+	return 1;
+}
