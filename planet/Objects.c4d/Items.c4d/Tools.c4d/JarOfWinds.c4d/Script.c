04a19006eda462a9d9a574c2ecdea135c1fb1b6c
Charles Spurrill <carlonfire@gmail.com>
2010-03-29 19:42:54 -0700

    wind jar cleanup & polishing

--- planet/Objects.c4d/Items.c4d/Tools.c4d/JarOfWinds.c4d/Script.c
+++ planet/Objects.c4d/Items.c4d/Tools.c4d/JarOfWinds.c4d/Script.c
@@ -2,19 +2,15 @@
 	Jar of Winds
 	Author: MimmoO
 
-	Collect air until youre full, then release it with a blast.
+	Collect air until you're full, then release it with a blast.
 
 --*/
 
 #strict 2
 
-local ReloadTimer;
-local Loaded;
 local Amount;
 local MaxCap;
-
-local yOffset;
-local iBarrel;
+local charging;
 
 local JarTrans;
 
@@ -33,6 +29,8 @@ protected func HoldingEnabled() { return true; }
 
 protected func ControlUseStart(object pClonk, ix, iy)
 {
+	Sound("WindCharge.ogg",false,nil,nil,1);
+	charging=1;
 	Amount=0;
 	return 1;
 }
@@ -48,13 +46,12 @@ public func ControlUseHolding(object pClonk, ix, iy)
 	//not in material
 	if(Amount<=MaxCap)
 	{
-	
 		var R=RandomX(-25,25);
 		var D=RandomX(19,130);
 		var SX=Sin(180-Angle(0,0,ix,iy)+R,D,);
 		var SY=Cos(180-Angle(0,0,ix,iy)+R,D);
 		
-		if(!GBackSolid(SX,SY) && !GBackLiquid(SX,SY)) //when on a random spot in frotn is air...
+		if(!GBackSolid(SX,SY) && !GBackLiquid(SX,SY)) //when on a random spot in front is air...
 	 	{
 	 	Amount+=2; 				//Air is sucked in.
 	 
@@ -70,42 +67,44 @@ public func ControlUseHolding(object pClonk, ix, iy)
  		}
  	}
  	
- 	else //were full? say it!
+ 	else //Is it full? Say so!
  	{
- 		Message("Full!",pClonk);	
- 	}
-
-    
+ 		Message("Full!",pClonk);
+		if(charging==1) ChargeSoundStop();
+		charging=0;
+ 	}    
 }
 
 protected func ControlUseStop(object pClonk, ix, iy)
 {
 	JarTrans = Trans_Translate(-1500,2000,0);
-	if(Amount>(MaxCap/5)) //can fire, enough air in?
+	if(Amount>(MaxCap/5)) //Is there enough air to fire?
 	{
 	 // Fire	
 		FireWeapon(pClonk, ix, iy);
 		return 1;	 
 	}
-	else Message("Not.",pClonk);	
-	//Sound(" :-( ");
+	ChargeSoundStop();
+	charging=0;
 return 1;
 }
 
-
-
+protected func ChargeSoundStop()
+{
+	Sound("WindCharge.ogg",false,nil,nil,-1);
+	Sound("WindChargeStop.ogg");
+}
 
 private func FireWeapon(object pClonk,iX,iY)
 {
-	
 	var iAngle=Angle(0,0,iX,iY);
-
+	
+	ChargeSoundStop();
 	Sound("WindGust.ogg");
 
 	//Find Victims to push
 	for(var i=10; i<32; i++)
 	{
-		
 		var R=RandomX(-20,20);
 		var SX=Sin(180-Angle(0,0,iX,iY)+R,i);
 		var SY=Cos(180-Angle(0,0,iX,iY)+R,i);
@@ -118,7 +117,6 @@ private func FireWeapon(object pClonk,iX,iY)
 					Cos(180-Angle(0,0,iX,iY)+(R),(Amount/2)+25),
 					Max(i+30,90),
 					);	
-		
 	 }
 	}	
 	
@@ -126,13 +124,12 @@ private func FireWeapon(object pClonk,iX,iY)
 	var cosspeed=Cos(180-Angle(0,0,iX,iY)+(R/2),(Amount)+15);
 	
 	if(pClonk->GetAction() != "Walk")
-	{									//Makes the clonk be pushed backwards a bit
+	{									//Makes the clonk firing it be pushed backwards a bit
 		var x=pClonk->GetXDir();
 		var y=pClonk->GetYDir();
 		pClonk->SetXDir((x)-(sinspeed/3));
 		pClonk->SetYDir((y)-(cosspeed/3)); 
 	}
-	
 	
 	for( var obj in FindObjects(
 					Find_Or(
@@ -140,34 +137,23 @@ private func FireWeapon(object pClonk,iX,iY)
 						Find_Distance(18,Sin(180-Angle(0,0,iX,iY),40),Cos(180-Angle(0,0,iX,iY),40)),
 						Find_Distance(25,Sin(180-Angle(0,0,iX,iY),70),Cos(180-Angle(0,0,iX,iY),70))
 						   ),
-						Find_Not(Find_Category(C4D_Structure))				
+						Find_Not(Find_Category(C4D_Structure))			
 					     )
 	   )
-	   
-	
-	   
+	      
 	{
 		if(obj!=pClonk && PathFree(pClonk->GetX(),pClonk->GetY(),obj->GetX(),obj->GetY()))
 	    {
+		//enemys are pushed back
 		  var x=obj->GetXDir();
 		  var y=obj->GetYDir();
 		  obj->SetXDir((x)+sinspeed);
-		  obj->SetYDir((y)+cosspeed); 
-		  									//enemys are pushed back	
+		  obj->SetYDir((y)+cosspeed);
 	    }	
 	}
 }
 
 
-
-
-
-
-
 func Definition(def) {
   SetProperty("Name", "$Name$", def);
 }
-		  									//enemys are pushed back	
-
-
-
