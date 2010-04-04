492ead6e2c98411b84102d6f94e5a8b608e6fd95
Charles Spurrill <carlonfire@gmail.com>
2010-04-03 23:10:36 -0700

    #225 and Wind-Jar code cleanup

--- planet/Objects.c4d/Items.c4d/Tools.c4d/JarOfWinds.c4d/Script.c
+++ planet/Objects.c4d/Items.c4d/Tools.c4d/JarOfWinds.c4d/Script.c
@@ -18,7 +18,7 @@ public func GetCarryPhase() { return 600; }
 
 protected func Initialize()
 {
-	MaxCap=50; //Changes duration and power of the Jar
+	MaxCap = 50; //Changes duration and power of the Jar
 	SetR(-45);
 	JarTrans = Trans_Translate(-1500,2000,0);
 }
@@ -28,37 +28,37 @@ protected func HoldingEnabled() { return true; }
 protected func ControlUseStart(object pClonk, ix, iy)
 {
 	Sound("WindCharge.ogg",false,nil,nil,1);
-	charging=1;
-	Amount=0;
+	charging = 1;
+	Amount = 0;
 	return 1;
 }
 
 public func ControlUseHolding(object pClonk, ix, iy)
 {
-	JarTrans = Trans_Mul(Trans_Translate(-1500,2000,0), Trans_Rotate(Angle(0,0,ix,iy)*(-1+2*pClonk->GetDir()),0,0,1));
+	JarTrans = Trans_Mul(Trans_Translate(-1500,2000,0), Trans_Rotate(Angle(0,0,ix,iy) * (-1 + 2 * pClonk->GetDir()),0,0,1));
 	// TODO a bit hacky, but i fear using animations here also leads to the bug like the musket and co. So I'll make the animation when the bug is fixed
 	pClonk->UpdateAttach();
 	//Angle Finder
 	if(GBackSolid()) return -1;
 	if(GBackLiquid()) return -1;	
 	//not in material
-	if(Amount<=MaxCap)
+	if(Amount <= MaxCap)
 	{
 		var R=RandomX(-25,25);
 		var D=RandomX(19,130);
-		var SX=Sin(180-Angle(0,0,ix,iy)+R,D,);
-		var SY=Cos(180-Angle(0,0,ix,iy)+R,D);
+		var SX=Sin(180 - Angle(0,0,ix,iy) + R,D);
+		var SY=Cos(180 - Angle(0,0,ix,iy) + R,D);
 		
 		if(!GBackSolid(SX,SY) && !GBackLiquid(SX,SY)) //when on a random spot in front is air...
 		{
-			Amount+=2; //Air is sucked in.
+			Amount += 2; //Air is sucked in.
 
-			Message("Loading...|%3.0d",pClonk,(Amount*99)/MaxCap);	
+			Message("Loading...|%3.0d",pClonk,(Amount * 99) / MaxCap);	
 
 			CreateParticle("AirIntake",
 				SX,SY,
-				Sin(180-Angle(0,0,ix,iy)+R,-D/2),
-				Cos(180-Angle(0,0,ix,iy)+R,-D/2),
+				Sin(180 - Angle(0,0,ix,iy) + R,-D / 2),
+				Cos(180 - Angle(0,0,ix,iy) + R,-D / 2),
 				RandomX(35,80),
 				RGBa(255,255,255,128)
 			);
@@ -68,8 +68,8 @@ public func ControlUseHolding(object pClonk, ix, iy)
 	else //Is it full? Say so!
 	{
 		Message("Full!",pClonk);
-		if(charging==1) ChargeSoundStop();
-		charging=0;
+		if(charging == 1) ChargeSoundStop();
+		charging = 0;
 	}
 }
 
@@ -83,8 +83,14 @@ protected func ControlUseStop(object pClonk, ix, iy)
 		return 1;
 	}
 	ChargeSoundStop();
-	charging=0;
+	charging = 0;
 return 1;
+}
+
+func ControlUseCancel()
+{
+	ChargeSoundStop();
+	charging = 0;
 }
 
 protected func ChargeSoundStop()
@@ -103,47 +109,47 @@ private func FireWeapon(object pClonk,iX,iY)
 	//Find Victims to push
 	for(var i=10; i<32; i++)
 	{
-		var R=RandomX(-20,20);
-		var SX=Sin(180-Angle(0,0,iX,iY)+R,i);
-		var SY=Cos(180-Angle(0,0,iX,iY)+R,i);
+		var R = RandomX(-20,20);
+		var SX = Sin(180 - Angle(0,0,iX,iY) + R,i);
+		var SY = Cos(180 - Angle(0,0,iX,iY) + R,i);
 		
 	if(!GBackSolid(SX,SY))
 	{
 		CreateParticle("Air",
 					SX,SY,
-					Sin(180-Angle(0,0,iX,iY)+(R),(Amount/2)+25),
-					Cos(180-Angle(0,0,iX,iY)+(R),(Amount/2)+25),
-					Max(i+30,90),
+					Sin(180 - Angle(0,0,iX,iY) + (R),(Amount / 2) + 25),
+					Cos(180 - Angle(0,0,iX,iY) + (R),(Amount / 2) + 25),
+					Max(i + 30, 90),
 					);	
 	}
 	}
 	
-	var sinspeed=Sin(180-Angle(0,0,iX,iY)+(R/2),(Amount)+15);
-	var cosspeed=Cos(180-Angle(0,0,iX,iY)+(R/2),(Amount)+15);
+	var sinspeed = Sin(180 - Angle(0,0,iX,iY) + (R / 2),(Amount) + 15);
+	var cosspeed = Cos(180 - Angle(0,0,iX,iY) + (R / 2),(Amount) + 15);
 	
 	if(pClonk->GetAction() != "Walk")
 	{									//Makes the clonk firing it be pushed backwards a bit
-		var x=pClonk->GetXDir();
-		var y=pClonk->GetYDir();
-		pClonk->SetXDir((x)-(sinspeed/3));
-		pClonk->SetYDir((y)-(cosspeed/3)); 
+		var x = pClonk->GetXDir();
+		var y = pClonk->GetYDir();
+		pClonk->SetXDir((x) - (sinspeed / 3));
+		pClonk->SetYDir((y) - (cosspeed / 3)); 
 	}
 	
 	for( var obj in FindObjects(
 		Find_Or(
-			Find_Distance(10,Sin(180-Angle(0,0,iX,iY),20),Cos(180-Angle(0,0,iX,iY),20)),
-			Find_Distance(18,Sin(180-Angle(0,0,iX,iY),40),Cos(180-Angle(0,0,iX,iY),40)),
-			Find_Distance(25,Sin(180-Angle(0,0,iX,iY),70),Cos(180-Angle(0,0,iX,iY),70))
+			Find_Distance(10,Sin(180 - Angle(0,0,iX,iY),20),Cos(180 - Angle(0,0,iX,iY),20)),
+			Find_Distance(18,Sin(180 - Angle(0,0,iX,iY),40),Cos(180 - Angle(0,0,iX,iY),40)),
+			Find_Distance(25,Sin(180 - Angle(0,0,iX,iY),70),Cos(180 - Angle(0,0,iX,iY),70))
 		),
 		Find_Not(Find_Category(C4D_Structure))
 	)) {
-		if(obj!=pClonk && PathFree(pClonk->GetX(),pClonk->GetY(),obj->GetX(),obj->GetY()))
+		if(obj != pClonk && PathFree(pClonk->GetX(),pClonk->GetY(),obj->GetX(),obj->GetY()))
 		{
 		//enemys are pushed back
-			var x=obj->GetXDir();
-			var y=obj->GetYDir();
-			obj->SetXDir((x)+sinspeed);
-			obj->SetYDir((y)+cosspeed);
+			var x = obj->GetXDir();
+			var y = obj->GetYDir();
+			obj->SetXDir((x) + sinspeed);
+			obj->SetYDir((y) + cosspeed);
 		}
 	}
 }
