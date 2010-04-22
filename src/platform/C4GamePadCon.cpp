92e506f45855e19c2445b2e5de3ef2e4d5b14a95
TomyLobo <tomylobo@hg.openclonk.org>
2010-04-22 11:39:13 +0200

    Changed C4GamePadControl::Execute() to C4GamePadControl::Execute(bool send_axis_strength_changes) to match declaration.

--- src/platform/C4GamePadCon.cpp
+++ src/platform/C4GamePadCon.cpp
@@ -369,7 +369,7 @@ void C4GamePadOpener::SetGamePad(int iGamepad)
 // Dedicated server and everything else with neither Win32 nor SDL.
 
 C4GamePadControl::C4GamePadControl() { Log("WARNING: Engine without Gamepad support"); }
-C4GamePadControl::~C4GamePadControl() { }
+C4GamePadControl::~C4GamePadControl(bool send_axis_strength_changes) { }
 void C4GamePadControl::Execute() { }
 int C4GamePadControl::GetGamePadCount() { return 0; }
 bool C4GamePadControl::AnyButtonDown() { return false; }
