7fd0c9884aa50b97cfb48d7ae4862b612c527f09
Charles Spurrill <carlonfire@gmail.com>
2010-04-27 18:16:15 -0700

    Tut3 - checkpoint improvments and tut arrows

--- planet/Tutorial.c4f/Tutorial03.c4s/Script.c
+++ planet/Tutorial.c4f/Tutorial03.c4s/Script.c
@@ -10,7 +10,6 @@ func Initialize()
 
 	var TutGoal = CreateObject(Goal_Tutorial);
 	TutGoal->SetStartpoint(40,446);
-	TutGoal->AddCheckpoint(300,463,"Javelin");
 	
 	// Dialogue options -> repeat round.
 	SetNextMission("Tutorial.c4f\\Tutorial03.c4s", "$MsgRepeatRound$", "$MsgRepeatRoundDesc$");
@@ -24,6 +23,7 @@ func Script1()
 func Script15()
 {
 	TutMsg("$MsgIntro1$");
+	FindObject(Find_ID(Goal_Tutorial))->AddCheckpoint(300,480,"Javelin")->SetBaseGraphics(Javelin);
 }
 
 func Checkpoint_Javelin()
@@ -34,6 +34,7 @@ func Checkpoint_Javelin()
 	var target = CreateObject(PracticeTarget,189,430,NO_OWNER);
 	target->SetAction("Attach",balloon);
 	target->AddEffect("Check",target,1,1,target);
+	TutArrowShowPos(189,430,-45,50);
 }
 
 func Checkpoint_Bow1()
@@ -49,13 +50,24 @@ func Checkpoint_Bow2()
 	clonk->FindContents(Javelin)->RemoveObject();
 	clonk->CreateContents(Bow);
 	clonk->CreateContents(Arrow);
+	TutArrowShowPos(1253,251,55,100);
 }
 
 
 global func OnTargetDeath(int target)
 {
-	if(target == 1) TutMsg("$MsgJavelin1$");
-	if(target == 2) TutMsg("$MsgJavelin2$");
+	if(target == 1)
+	{
+		TutMsg("$MsgJavelin1$");
+		TutArrowShowPos(350,300,45,50);
+	}
+
+	if(target == 2)
+	{
+		TutMsg("$MsgJavelin2$");
+		TutArrowShowPos(538,319,60,50);
+	}
+
 	if(target == 3)
 	{
 		var ladder = CreateObject(Ropeladder,AbsX(530),AbsY(388));
@@ -64,7 +76,33 @@ global func OnTargetDeath(int target)
 
 		var goal = FindObject(Find_ID(Goal_Tutorial));
 		goal->AddCheckpoint(696,305,"Bow1");
-		goal->AddCheckpoint(1034,456,"Bow2");
+		goal->AddCheckpoint(1034,456,"Bow2")->SetBaseGraphics(Bow);
+		TutArrowClear();
+	}
+
+	if(target == 4)
+	{
+		TutArrowShowPos(980,190,-45,100);
+	}
+
+	if(target == 5)
+	{
+		TutArrowShowPos(834,535,-100,40);
+	}
+
+	if(target == 6)
+	{
+		TutArrowShowPos(1380,185,60,150);
+	}
+
+	if(target == 7)
+	{
+		TutArrowShowPos(1412,472,170,130);
+	}
+
+	if(target == 8)
+	{
+		TutArrowClear();
 	}
 
 	var clonk = FindObject(Find_ID(Clonk),Find_OCF(OCF_Alive));
