938da3b30cecf71493cfa6bea81d4d6cc09618b4
Maikel de Vries <maikeldevries@hotmail.com>
2010-05-03 19:48:54 +0200

    ¨Some¨ realism to javelin throwing

--- planet/Objects.c4d/Items.c4d/Weapons.c4d/Javelin.c4d/Script.c
+++ planet/Objects.c4d/Items.c4d/Weapons.c4d/Javelin.c4d/Script.c
@@ -96,11 +96,19 @@ public func DoThrow(object clonk, int angle)
 	
 	// how fast the javelin is thrown depends very much on
 	// the speed of the clonk
-	var speed = 1200 * clonk->GetPhysical("Throw") / 12000 + 150 * Abs(clonk->GetXDir());
-	var xdir = Sin(angle,+speed);
-	var ydir = Cos(angle,-speed);
-	javelin->SetXDir(xdir,1000);
-	javelin->SetYDir(ydir,1000);
+	//var speed = 1200 * clonk->GetPhysical("Throw") / 12000 + 150 * Abs(clonk->GetXDir());
+	//var xdir = Sin(angle,+speed);
+	//var ydir = Cos(angle,-speed);
+	// The clonk can convert some, indicated by div in percentages, of its own kinetic energy to change the momenta of the javelin, a miracle.
+	var div = 60; // 40% is converted to the direction of the throwing angle.
+	var xdir = clonk->GetXDir(1000);
+	var ydir = clonk->GetYDir(1000);
+	var speed = clonk->GetPhysical("Throw") / 8 + (100 - div) * Sqrt(xdir**2 + ydir**2) / 100;
+	var jav_x = div * xdir / 100 + Sin(angle, speed);
+	var jav_y = div * ydir / 100 - Cos(angle, speed);
+		
+	javelin->SetXDir(jav_x, 1000);
+	javelin->SetYDir(jav_y, 1000);
 	
 	javelin->AddEffect("Flight",javelin,1,1,javelin,nil);
 	javelin->AddEffect("HitCheck",javelin,1,1,nil,nil,clonk);
