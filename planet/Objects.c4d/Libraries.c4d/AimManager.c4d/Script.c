ae01f24ad651905cbc4e64ad30b0ce50901b11b8
Richard Gerum <randrian@hg.openclonk.org>
2010-05-30 11:02:41 +0200

    fixed cancelling and reaiming of javelin (#295)

--- planet/Objects.c4d/Libraries.c4d/AimManager.c4d/Script.c
+++ planet/Objects.c4d/Libraries.c4d/AimManager.c4d/Script.c
@@ -232,6 +232,8 @@ public func StartAim(object weapon)
 		AddEffect("IntAimCheckProcedure", this, 1,  1, this);
 	}
 
+	aim_stop = 0;
+
 	if(aim_set["AnimationAim"] != nil)
 	{
 		if(aim_set["AimMode"] == AIM_Position)
