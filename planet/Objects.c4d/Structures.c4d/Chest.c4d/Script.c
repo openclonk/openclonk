e23838331611f1e884a4d26ec61c09b5f0ff9e57
Charles Spurrill <carlonfire@gmail.com>
2010-05-31 18:52:57 -0700

    3D chest with sounds

--- planet/Objects.c4d/Structures.c4d/Chest.c4d/Script.c
+++ planet/Objects.c4d/Structures.c4d/Chest.c4d/Script.c
@@ -8,6 +8,11 @@
 local content_menu;
 local ctrl_clonk;
 
+protected func Initialize()
+{
+	PlayAnimation("Open", 1, Anim_Linear(0, 0, 1, 20, ANIM_Hold), Anim_Const(1000));
+}
+
 // Opens a menu when the clonk grabs this chest.
 protected func Grabbed(object by_object, bool grab)
 {
@@ -21,13 +26,30 @@ protected func Grabbed(object by_object, bool grab)
 			content_menu->AddItem(content->GetID());
 		content_menu->AddItem(GetID());
 		content_menu->Show();    
+		OpenClose(true);
 	}
 	else // If let go close content menu.
 	{
 		if (content_menu) 
 			content_menu->Close();
+		OpenClose();
 	}
 	return;
+}
+
+private func OpenClose(bool open)
+{
+	if(open == true) //On open
+	{
+		PlayAnimation("Open", 5, Anim_Linear(0, 0, GetAnimationLength("Open"), 40, ANIM_Remove), Anim_Linear(0, 0, 1000, 15, ANIM_Remove));
+		Sound("Open.ogg");
+	}
+
+	if(open != true) //On close
+	{
+		PlayAnimation("Close", 5, Anim_Linear(0, 0, GetAnimationLength("Close"), 40, ANIM_Remove), Anim_Linear(0, 0, 1000, 15, ANIM_Remove));
+		Sound("Close.ogg");
+	}
 }
 
 // Callback from ringmenu.
@@ -82,4 +104,5 @@ protected func RejectCollect(id def, object obj)
 protected func Definition(def) 
 {
 	SetProperty("Name", "Chest", def);
+	SetProperty("PictureTransformation",Trans_Mul(Trans_Rotate(-20,1,0,0),Trans_Rotate(20,0,1,0),Trans_Scale(950),Trans_Translate(1000,1,0,0)),def);
 }
