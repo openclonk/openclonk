911f5b0037700b0a3e7a5c066d4b63b7b45d0ccf
Charles Spurrill <carlonfire@gmail.com>
2010-06-20 15:22:08 -0700

    Cannon mouse control

--- planet/Objects.c4d/Vehicles.c4d/Cannon.c4d/Script.c
+++ planet/Objects.c4d/Vehicles.c4d/Cannon.c4d/Script.c
@@ -7,8 +7,6 @@
 
 local aim_anim;
 local olddir;
-local aimangle;
-local aimdir;
 local grabber;
 
 static const Fire_Velocity = 175;
@@ -19,7 +17,6 @@ protected func Initialize()
 {
 	SetAction("Roll");
 	olddir = GetDir();
-	aimangle = 0;
 	aim_anim =  PlayAnimation("Aim", 1,  Anim_Const(0),Anim_Const(1000));
 }
 
@@ -34,8 +31,8 @@ protected func ContactRight()
 	if(Stuck() && !Random(5)) SetRDir(RandomX(-7, +7));
 }
 
-//One space for blackpowder, one space for shot.
-private func MaxContents() { return 2; }
+//Only one object fits in barrel
+private func MaxContents() { return 1; }
 
 protected func RejectCollect(id idObj,object pObj)
 {
@@ -51,13 +48,66 @@ protected func RejectCollect(id idObj,object pObj)
 
 public func ControlUseStart(object clonk, int ix, int iy)
 {
-	//ammo requirements
-	if(!FindObject(Find_Container(this),Find_ID(Blackpowder)))//needs some powder
-	{
-		PlayerMessage(clonk->GetOwner(),"$TxtNeedspowder$");
-		return 1;
-	}
-	if(!FindObject(Find_Container(this),Find_Category(C4D_Object),Find_Not(Find_ID(Blackpowder))))//needs a shot
+	if(clonk->GetOwner() != GetOwner()) SetOwner(clonk->GetOwner());
+	AddEffect("TrajAngle", this, 50, 1, this);
+	return 1;
+}
+
+public func ControlUseStartAlt(object clonk, int ix, int iy)
+{
+	if(clonk->ContentsCount() < MaxContents()) Contents(0)->Enter(clonk);
+	else
+		Contents(0)->Exit();
+}
+
+private func Selected(object menu, object pObj, bool alt)
+{
+	ammo = pObj;
+}
+
+public func HoldingEnabled() { return true; }
+
+public func ControlUseHolding(object clonk, int ix, int iy)
+{
+	var r = ConvertAngle(Angle(0,0,ix,iy));
+
+	if(r - GetR() < 0 && GetDir() == 1) SetDir();
+	if(r - GetR() > 359 && GetDir() == 0) SetDir(1);
+	Message(Format("%d",Normalize(r - GetR(),-90)),this);
+
+	var iColor = RGB(255,255,255);
+	if(!Contents(0) || GetEffect("Cooldown",this)) iColor = RGB(255,0,0);
+	AddTrajectory(this, GetX() + 5, GetY() + 2, Cos(r-90, Fire_Velocity), Sin(r-90, Fire_Velocity), iColor, 20);
+
+	SetAnimationPosition(aim_anim, Anim_Const(AnimAngle(r)*3954444/100000)); //conversion. Apparently 90 blender frames is 3559 ogre frames.
+	return 1;
+}
+
+private func AnimAngle(int angle)
+{
+	//Conversion for animation
+	var r = Normalize(angle,-180);
+	r = r - GetR();
+	r = Abs(r);
+	r = 180-r-90;
+	r = BoundBy(r,0,90);
+	return r;
+}
+
+private func ConvertAngle(int angle)
+{
+	//More confusing conversion ;)
+	var r = angle;
+	if(r > 90 + GetR() && GetDir() == 1) r = 90 + GetR();
+	if(r < 270 + GetR() && r != 0 && GetDir() == 0) r = 270 + GetR();
+	if(r == 360 + GetR() && GetDir() == 0) r = 0 + GetR();
+	return r;
+}
+
+public func ControlUseStop(object clonk, int ix, int iy)
+{
+	RemoveTrajectory(this);
+	if(!Contents(0))//needs a shot
 	{
 		PlayerMessage(clonk->GetOwner(),"$TxtNeedsammo$");
 		return 1;
@@ -66,42 +116,30 @@ public func ControlUseStart(object clonk, int ix, int iy)
 	//Can't fire if cannon is cooling down
 	if(GetEffect("Cooldown",this))	return 1;
 	
-	if(FindObject(Find_Container(this),Find_ID(Blackpowder)) &&
-		FindObject(Find_Container(this),Find_Category(C4D_Object), Find_Not(Find_ID(Blackpowder))))
+	if(Contents(0))
 	{
-		DoFire(FindObject(Find_Container(this),Find_Category(C4D_Object), Find_Not(Find_ID(Blackpowder))),clonk);
-		FindContents(Blackpowder)->RemoveObject();
+		if(ammo == nil) ammo = Contents(0);
+		DoFire(ammo, clonk, Angle(0,0,ix,iy));
+		ammo = nil;
 		AddEffect("Cooldown",this,1,1,this);
 	}
 	return 1;
 }
 
+public func ControlUseCancel(object clonk, int ix, int iy)
+{
+	RemoveTrajectory(this);
+	return 1;
+}
+
+public func Grabbed(object clonk, bool grabbed)
+{
+	if(!grabbed) ControlUseCancel();
+}
+
 public func FxCooldownTimer(object target, int num, int timer)
 {
 	if(timer > 72) return -1;
-}
-
-//Sends variables to Timer() for press & release control
-func ControlUp(object clonk)
-{
-	if(aimangle < 90)
-	{
-		aimdir = 1;
-	}
-}
-
-func ControlDown(object clonk)
-{
-	if(aimangle > 0)
-	{
-		aimdir = -1;
-	}
-}
-
-//Player has released button? Stop rotating barrel.
-func ControlStop(object clonk, int control)
-{
-	aimdir = 0;
 }
 
 //Activate turn animations
@@ -115,7 +153,7 @@ func ControlRight(object clonk)
 	SetDir(1);
 }
 
-protected func DoFire(object iammo, object clonk)
+protected func DoFire(object iammo, object clonk, int angle)
 {
 	if(iammo == nil) iammo = FindContents(Rock); //debug only!
 	iammo->Exit();
@@ -125,13 +163,15 @@ protected func DoFire(object iammo, object clonk)
 		iammo->Fuse();
 	}
 
-	var r = ConvertAngle();
+	//Don't excede possible trajectory
+	var r = Normalize(angle,-180);
+	if(r > 90) r = 90;
+	if(r < -90) r = -90;
 
 	//Send ammo flying
-	var spread = -1 + Random(3);
 	iammo->SetR(r);
 	iammo->SetRDir(-4 + Random(9));
-	iammo->LaunchProjectile(r + spread, 17, Fire_Velocity);
+	iammo->LaunchProjectile(r, 17, Fire_Velocity);
 
 	//Particles
 	var dist = 25;
@@ -147,51 +187,10 @@ protected func DoFire(object iammo, object clonk)
 
 	//sound
 	Sound("Blast3");
-
-	//Recoil
-	if(aimangle < 88) aimangle = aimangle + Random(3);
-	PlayAnimation("Fire", 5, Anim_Linear(0,0, GetAnimationLength("Fire"), 30, ANIM_Remove), Anim_Const(500));
 }
-
-public func Grabbed(object clonk, bool grabbed)
-{
-	if(grabbed)
-	{
-		if(!grabber) grabber = clonk;
-		if(clonk->GetOwner() != GetOwner()) SetOwner(clonk->GetOwner());
-		AddEffect("TrajAngle", this, 50, 1, this);
-		return 1;
-	}
-
-	if(!grabbed)
-	{
-		grabber = nil;
-		RemoveEffect("TrajAngle",this);	
-		RemoveTrajectory(this);
-	return 1;
-	}
-}
-
-public func FxTrajAngleTimer(object target, int num, int timer)
-{
-	if(grabber->GetProcedure() != "PUSH")
-	{
-		RemoveTrajectory(this);
-		grabber = nil;
-		return -1;
-	}
-	
-	var r = ConvertAngle();
-
-	var iColor = RGB(255,255,255);
-	if(!FindContents(Blackpowder) || FindObject(Find_Category(C4D_Object),Find_Not(Find_ID(Blackpowder))) == nil ) iColor = RGB(255,0,0);
-	target->AddTrajectory(target, target->GetX(), target->GetY()-3, Cos(r-90, Fire_Velocity), Sin(r-90, Fire_Velocity), iColor, 20);
-}
-
 
 func Timer()
 {	
-	// Does a whole bunch of good stuff. Updates animation and handles press & release control method.
 	//Turning
 	if(olddir != GetDir())
 	{
@@ -206,26 +205,6 @@ func Timer()
 		}
 	}
 	olddir = GetDir();
-
-	//Aiming animation
-	if(aimangle >= 0 && aimangle <= 90)
-	{
-		//stop player from going over or under 0-90 degrees.
-		if(aimangle == 0 && aimdir == -1) return 1;
-		if(aimangle == 90 && aimdir == +1) return 1;
-		aimangle = aimangle + aimdir;
-	}
-	SetAnimationPosition(aim_anim, Anim_Const(aimangle*3954444/100000)); //conversion. Apparently 90 blender frames is 3559 ogre frames.
-}
-
-private func ConvertAngle()
-{
-	//Finds the angle based on chassis rotation and direction.
-	var iangle = (aimangle - GetR()); //modifies angle when the cannon's chassis rotates
-	if(GetDir() == 0) iangle = (aimangle + GetR());
-	var angle = (90 - iangle);//angle modifier which acknowledges direction
-	if(GetDir() == 0) angle = (270 + iangle);
-	return angle;
 }
 
 func Definition(def) {
