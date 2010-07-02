ede96f3747fb33ae6537d9e280743a6b00f6b566
Charles Spurrill <carlonfire@gmail.com>
2010-07-02 16:42:43 -0700

    CastObjects fix

--- planet/System.c4g/Commits.c
+++ planet/System.c4g/Commits.c
@@ -199,7 +199,9 @@ global func CastObjects(id def, int am, int lev, int x, int y, int angs, int ang
 		obj->SetR(Random(360));
 		obj->SetXDir(xdir);
 		obj->SetYDir(Sin(ang, lev) + RandomX(-3, 3));
-		obj->SetRDir((10 + Random(21)) * xdir / Abs(xdir));
+		if(xdir != 0) obj->SetRDir((10 + Random(21)) * (xdir / Abs(xdir)));
+		else
+			obj->SetRDir(-10 + Random(21));
 	}
 	return;
 }
