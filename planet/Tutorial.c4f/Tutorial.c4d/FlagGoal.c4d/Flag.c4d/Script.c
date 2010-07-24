5da85861a415882194b369c794494c0208a7a516
Charles Spurrill <carlonfire@gmail.com>
2010-07-24 12:46:37 -0700

    3d tut flag graphics. Stupid animation refuses to play!

--- planet/Tutorial.c4f/Tutorial.c4d/FlagGoal.c4d/Flag.c4d/Script.c
+++ planet/Tutorial.c4f/Tutorial.c4d/FlagGoal.c4d/Flag.c4d/Script.c
@@ -3,11 +3,33 @@
 	
 --*/
 
-
+protected func Initialize()
+{
+	SetAction("Wave");
+	PlayAnimation("Wave", 5, Anim_Linear(0, 0, GetAnimationLength("Wave"), 78, ANIM_Loop), Anim_Const(1000)); //Stupid ActMap!!!
+	//The animation seems to not want to play, no matter the method used. >:(
+}
 
 /*-- Proplist --*/
 
 func Definition(def)
 {
 	SetProperty("Name", "$Name$", def);
-}
+	SetProperty("MeshTransformation", Trans_Rotate(90,0,1,0),def);
+	SetProperty("ActMap", {
+Wave = {
+	Prototype = Action,
+	Name = "Wave",
+	Procedure = DFA_NONE,
+	Directions = 2,
+	FlipDir = 0,
+	Length = 78,
+	Delay = 1,
+	X = 0,
+	Y = 0,
+	Wdt = 30,
+	Hgt = 40,
+	NextAction = "Wave",
+	Animation = "Wave",
+},
+}, def);}
