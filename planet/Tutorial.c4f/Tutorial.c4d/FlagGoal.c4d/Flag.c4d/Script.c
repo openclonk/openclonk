ebed4aaf18144b4b894805d0f2030b5aa93e794f
Charles Spurrill <carlonfire@gmail.com>
2010-08-01 17:20:25 -0700

    flag animation works

--- planet/Tutorial.c4f/Tutorial.c4d/FlagGoal.c4d/Flag.c4d/Script.c
+++ planet/Tutorial.c4f/Tutorial.c4d/FlagGoal.c4d/Flag.c4d/Script.c
@@ -5,9 +5,7 @@
 
 protected func Initialize()
 {
-	SetAction("Wave");
-	PlayAnimation("Wave", 5, Anim_Linear(0, 0, GetAnimationLength("Wave"), 78, ANIM_Loop), Anim_Const(1000)); //Stupid ActMap!!!
-	//The animation seems to not want to play, no matter the method used. >:(
+	PlayAnimation("Wave", 1, Anim_Linear(0, 0, GetAnimationLength("Wave"), 78, ANIM_Loop), Anim_Const(1000));
 }
 
 /*-- Proplist --*/
@@ -15,21 +13,5 @@ protected func Initialize()
 func Definition(def)
 {
 	SetProperty("Name", "$Name$", def);
-	SetProperty("MeshTransformation", Trans_Rotate(90,0,1,0),def);
-	SetProperty("ActMap", {
-Wave = {
-	Prototype = Action,
-	Name = "Wave",
-	Procedure = DFA_NONE,
-	Directions = 2,
-	FlipDir = 0,
-	Length = 78,
-	Delay = 1,
-	X = 0,
-	Y = 0,
-	Wdt = 30,
-	Hgt = 40,
-	NextAction = "Wave",
-	Animation = "Wave",
-},
-}, def);}
+	SetProperty("MeshTransformation", Trans_Rotate(60,0,1,0),def);
+}
