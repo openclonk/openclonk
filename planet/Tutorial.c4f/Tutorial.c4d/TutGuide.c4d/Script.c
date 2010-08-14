ae7df11fa3c60f07a17ab45919144ac5034182fb
Maikel de Vries <maikeldevries@hotmail.com>
2010-08-14 21:31:58 +0200

    Added notification for new messages to the tutorial guide (#331)

--- planet/Tutorial.c4f/Tutorial.c4d/TutGuide.c4d/Script.c
+++ planet/Tutorial.c4f/Tutorial.c4d/TutGuide.c4d/Script.c
@@ -17,8 +17,10 @@ protected func Construction()
 	this["Visibility"] = VIS_Owner;
 	messages = [];
 	index = 0;
+	return;
 }
 
+// Creates the tutorial guide in the upper hud, returns the guide as a pointer.
 global func CreateTutorialGuide(int plr)
 {
 	var guide = CreateObject(TutorialGuide, 0, 0 , plr);
@@ -26,6 +28,7 @@ global func CreateTutorialGuide(int plr)
 	return guide;
 }
 
+// Get-Setters for the message index.
 public func GetGuideIndex() { return index; }
 public func SetGuideIndex(int to_index)
 {
@@ -33,27 +36,33 @@ public func SetGuideIndex(int to_index)
 	return;
 } 
 
+// Add a message to the guide, the index is set to this message.
 public func AddGuideMessage(string msg)
 {
 	// Automatically set index to current.
 	index = GetLength(messages);
 	// Add message to list.
 	messages[index] = msg;
+	// Make visible that there is a new message.
+	AddEffect("NotifyPlayer", this, 100, 72, this);
 	return;
 }
 
+// Shows a guide message to the player, also resets the index to that point.
 public func ShowGuideMessage(int show_index)
 {
 	index = Max(0, show_index);
 	if (!messages[index]) 
-		return;	
+		return;
+	if (GetEffect("NotifyPlayer", this))
+		RemoveEffect("NotifyPlayer", this);
 	GuideMessage(messages[index]);
-	AddEffect("MessageShown", this, 100, 2 * GetLength(messages[index]), this);
 	if (messages[index + 1]) 
 		index++;
 	return;
 }
 
+// Removes the current guide message.
 public func ClearGuideMessage()
 {
 	if (GetEffect("MessageShown", this))
@@ -62,22 +71,28 @@ public func ClearGuideMessage()
 	return;
 }
 
+// Callback: the player has clicked on the guide.
 public func MouseSelection(int plr)
 {
 	if (plr != GetOwner())
 		return;
-	if (GetEffect("MessageShown", this))
+	if (GetEffect("NotifyPlayer", this))
+		RemoveEffect("NotifyPlayer", this);
+	// Clear guide message if the latest is already shown.
+	var effect = GetEffect("MessageShown", this, nil, 0);
+	if (effect && EffectVar(0, this, effect) == messages[index])
 		return ClearGuideMessage();
+	// Show guide message if there is a new one, and increase index if possible.
 	if (!messages[index]) 
 		return;
 	GuideMessage(messages[index]);
-	AddEffect("MessageShown", this, 100, 2 * GetLength(messages[index]), this);
 	if (messages[index + 1]) 
 		index++;
 	return;
 }
 
-public func GuideMessage(string message)
+// Shows a message at the right place in message box.
+private func GuideMessage(string message)
 {
 	if (GetOwner() == NO_OWNER)
 		return false;
@@ -87,18 +102,47 @@ public func GuideMessage(string message)
 	var portrait_def = "Portrait:TutorialGuide::00ff00::1";
 	// Message as regular one, don't stop the player.
 	CustomMessage(message, nil, GetOwner(), 0, 16 + TutorialGuide->GetDefHeight(), 0xffffff, GuideMenuDeco, portrait_def, MSG_HCenter);
+	var effect = AddEffect("MessageShown", this, 100, 2 * GetLength(message), this);
+	EffectVar(0, this, effect) = message;
 	return true;
 }
 
+// Effect exists as long as the message is shown.
+// Message string is stored in EffectVar 0.
+// TODO account for infinite messages, those with @ in front.
 protected func FxMessageShownTimer(object target, int num, int time)
 {
-	// Delete effect if message has disappeared.
+	// Delete effect if time has passed, i.e. message has disappeared.
 	if (time)
 		return -1;
 	return 1;
 }
 
+// Effect to display the player notification for some time.
+protected func FxNotifyPlayerStart(object target, int num, int temporary)
+{
+	// Display notifier.
+	SetGraphics("0", Icon_Number, 1, GFXOV_MODE_Base); //TODO get rid of this placeholder image.
+	SetObjDrawTransform(500, 0, 10000, 0, 500, -10000, 1);
+	return 1;
+}
+
+protected func FxNotifyPlayerTimer(object target, int num, int time)
+{
+	// Delete effect if time has passed.
+	if (time)
+		return -1;	
+	return 1;
+}
+
+protected func FxNotifyPlayerStop(object target, int num, int reason, bool temporary)
+{
+	// Remove notifier.
+	SetGraphics(nil, nil, 1);
+	return 1;
+}
+
 protected func Definition(def)
 {
-	def["Name"] = "Prof. clonkine";
+	def["Name"] = "$Name$";
 }
