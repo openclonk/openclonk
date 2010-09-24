d11a4f92ec8d0375a772369f7a34d2ddc5618e77
Tobias Zwick <newton@westnordost.de>
2010-09-24 20:23:27 +0200

    fix msvc build

--- src/platform/C4Video.cpp
+++ src/platform/C4Video.cpp
@@ -214,9 +214,9 @@ bool C4Video::AdjustPosition()
 	C4Player *pPlr = ::Players.Get(pViewport->GetPlayer());
 	if (!pPlr) return false;
 	// Set camera position
-	X = int32_t(pPlr->ViewX - pViewport->ViewX + pViewport->DrawX - Width/2);
+	X = int32_t(fixtof(pPlr->ViewX) - pViewport->ViewX + pViewport->DrawX - Width/2);
 	X = BoundBy( X, 0, pViewport->ViewWdt - Width );
-	Y = int32_t(pPlr->ViewY - pViewport->ViewY + pViewport->DrawY - Height/2);
+	Y = int32_t(fixtof(pPlr->ViewY) - pViewport->ViewY + pViewport->DrawY - Height/2);
 	Y = BoundBy( Y, 0, pViewport->ViewHgt - Height );
 	// Success
 	return true;
