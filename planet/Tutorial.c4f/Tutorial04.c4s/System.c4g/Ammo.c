d92364e86cd05ba9fabf74f1fa94705e60de0657
Maikel de Vries <maikeldevries@hotmail.com>
2010-10-03 12:44:17 +0200

    Fixed fading out for ammo in tutorial 4

--- planet/Tutorial.c4f/Tutorial04.c4s/System.c4g/Ammo.c
+++ planet/Tutorial.c4f/Tutorial04.c4s/System.c4g/Ammo.c
@@ -31,32 +31,28 @@ public func HitObject()
 
 protected func FxIntFadeOutStart(object target, int num) 
 {
-	EffectVar(0, target, num) = target->GetX();
-	EffectVar(1, target, num) = target->GetY();
-	EffectVar(2, target, num) = target->GetClrModulation() & 0x00ffffff;
-	EffectVar(3, target, num) = target->GetClrModulation() >> 24 & 255;
+	EffectVar(0, target, num) = target->GetClrModulation() & 0x00ffffff;
+	EffectVar(1, target, num) = target->GetClrModulation() >> 24 & 255;
 }
 
 protected func FxIntFadeOutTimer(object target, int num, int time)
 {
-	if (time < 180 * 2/3) 
-		return;
-	if (!target->Contained() && EffectVar(0, target, num) == target->GetX() && EffectVar(1, target, num) == target->GetY()) 
+	if (time < 144 * 2/3) 
+		return 1;
+	if (!target->Contained()) 
 	{
-		if (time >= 180) 
+		if (time >= 144)
 		{
 			target->RemoveObject();
 			return -1;
 		}
-	} 
+	}
 	else
 	{
-		target->SetClrModulation(EffectVar(4, target, num) << 24 | EffectVar(3, target, num));
+		target->SetClrModulation(EffectVar(1, target, num) << 24 | EffectVar(0, target, num));
 		return -1;
 	}
-	EffectVar(0, target, num) = target->GetX();
-	EffectVar(1, target, num) = target->GetY();
-	target->SetClrModulation(((180 - time) * EffectVar(4, target, num) / (180/3)) << 24 | EffectVar(3, target, num));
+	target->SetClrModulation(((144 - time) * EffectVar(1, target, num) / (144/3)) << 24 | EffectVar(0, target, num));
 	return 1;
 }
 
