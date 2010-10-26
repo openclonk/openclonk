4c697647ae6c558654061af3e8592f2b1968f6d4
Charles Spurrill <carlonfire@gmail.com>
2010-10-26 11:34:35 -0700

    Shiver Peak polished, moved to BttR

---
+++ planet/BackToTheRocks.c4f/ShiverPeak.c4s/Script.c
@@ -0,0 +1,73 @@
+/* Mountain parkour */
+
+func Initialize()
+{
+	var pGoal = CreateObject(Goal_Parkour, 0, 0, NO_OWNER);
+	var x, y;
+	y=LandscapeHeight()-120;
+	x=LandscapeWidth()/2;
+	pGoal->SetStartpoint(x, y);
+	var ix,iy;
+	y=LandscapeHeight()/7*6;
+	for(var i=1; i<7;i++)
+	{
+		iy=y-50+Random(100); ix=x-125+Random(250);
+		var l=0,u=125;
+		while(GBackSolid(ix,iy)) {++l;u+=5;iy=y-50+Random(100); ix=x-u+Random(2*u);if(l>50){break;};}
+		var mode = PARKOUR_CP_Check | PARKOUR_CP_Respawn;
+		pGoal->AddCheckpoint(ix, iy, mode);
+		y-=LandscapeHeight()/7;
+	}
+	x=LandscapeWidth()/2;;
+	y=35;
+	pGoal->SetFinishpoint(x, y);
+	
+	//Environmental Effects
+	var time = CreateObject(Environment_Time);
+	time->SetCycleSpeed();
+	time->SetTime(900);
+
+	//Clouds
+	for(var i; i < 30; i++)
+		CreateObject(CloudEffect,Random(LandscapeWidth()),Random(LandscapeHeight()))->Show(nil,nil,5000,true);
+	//Snow
+	AddEffect("Snowfall",0,1,2);
+
+	MapBottomFix();
+
+	//Place powderkegs and dynamite boxes
+	var i = 0;
+	while(i < 25)
+	{
+		var pos = FindPosInMat("Tunnel",0,0,LandscapeWidth(),LandscapeHeight());
+		if(Random(2)) CreateObject(PowderKeg,pos[0],pos[1],NO_OWNER);
+		else CreateObject(DynamiteBox,pos[0],pos[1],NO_OWNER);
+		i++;
+	}
+}
+
+protected func PlrHasRespawned(int iPlr, object cp)
+{
+	var clonk = GetCrew(iPlr);
+	clonk->CreateContents(Shovel);
+	if (!Random(2))
+		clonk->CreateContents(Loam);
+	else
+		clonk->CreateContents(Dynamite);
+	return;
+}
+
+global func FxSnowfallTimer(object target, int num, int timer)
+{
+	CastPXS("Snow",5,1,RandomX(0,LandscapeWidth()),1);
+}
+
+global func MapBottomFix()
+{
+	for(var i=1; i < LandscapeWidth(); i++)
+	{
+		var sway = Sin(i,10);
+		if(GetMaterial(i,LandscapeHeight()-1) == Material("Tunnel"))
+			DrawMaterialQuad("Granite",i-1,LandscapeHeight() - 13 + sway,i+1,LandscapeHeight() - 13 + sway,i+1,LandscapeHeight(),i-1,LandscapeHeight());
+	}
+}
