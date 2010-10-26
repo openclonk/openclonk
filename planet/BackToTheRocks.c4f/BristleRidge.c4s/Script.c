a8208f6a048f496ac80d065ee6cee7fa3b781d54
MimmoO <mimmo-o@hg.openclonk.org>
2010-10-26 16:51:41 +0200

    BristelRidge to BTTR.c4f (Canyon Parcour)

--- planet/BackToTheRocks.c4f/BristleRidge.c4s/Script.c
+++ planet/BackToTheRocks.c4f/BristleRidge.c4s/Script.c
@@ -13,22 +13,26 @@ func Initialize()
 		iy=y;ix=x;
 		var l=0,u=20;
 		while(GBackSolid(ix,iy) | GBackSky(ix,iy)) {++l;u+=3;iy=y-u+Random(2*u); ix=x-u+Random(2*u);if(l>100){break;};}
-		var mode = PARKOUR_CP_Check | PARKOUR_CP_Respawn;
+		var mode = PARKOUR_CP_Check | PARKOUR_CP_Respawn | PARKOUR_CP_Ordered;
 		pGoal->AddCheckpoint(ix, iy, mode);
 		y-=LandscapeHeight()/10;
 		x+=LandscapeWidth()/6;
 	}
-	x=LandscapeWidth()-35;
-	y=35;
+	x=LandscapeWidth()-130;
+	y=50;
 	pGoal->SetFinishpoint(x, y);
 	SetFoW(0);
+	Sound("BirdsLoop.ogg",true,100,nil,+1);
+	CreateObject(Environment_Clouds);
+	PlaceGrass(200);
 }
 
 protected func PlrHasRespawned(int iPlr, object cp)
 {
 	var clonk = GetCrew(iPlr);
-	clonk->CreateContents(Shovel);
-	clonk->CreateContents(Loam);
+	clonk->CreateContents(GrappleBow);
 	clonk->CreateContents(JarOfWinds);
+	clonk->CreateContents(Loam);
+	clonk->CreateContents(Shovel);
 	return;
 }
