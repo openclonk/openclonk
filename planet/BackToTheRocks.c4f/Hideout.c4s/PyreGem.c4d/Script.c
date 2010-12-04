ce5e983ee1cdc05ed2906726b3ad0a1a0b898452
MimmoO <mimmo-o@hg.openclonk.org>
2010-12-04 20:46:39 +0100

    PyreGem: added even more feedback

--- planet/BackToTheRocks.c4f/Hideout.c4s/PyreGem.c4d/Script.c
+++ planet/BackToTheRocks.c4f/Hideout.c4s/PyreGem.c4d/Script.c
@@ -77,9 +77,10 @@ global func FxGemPyreTimer(object target, int num, int time)
 		if(end) continue;
 		if(PathFree(x,y,obj->GetX(),obj->GetY()))
 		{
-			obj->DoEnergy(-BoundBy((30-time),1,25));
+			obj->DoEnergy((-BoundBy((30-time),1,25)*3)/4);
 			obj->CastParticles("MagicFire",20 + (BoundBy((30-time),1,25)*2),(BoundBy((30-time),5,25)*2),0,0,25,40,clr,clr);
 			obj->CastParticles("Air",10 + BoundBy((30-time),1,25),10,0,0,15,30,clr,clr);
+			obj->Fling(RandomX(-2,2),-2-(BoundBy((30-time),10,30)/10));
 			EffectVar(5+EffectVar(4, target, num), target, num) = obj;
 			EffectVar(4, target, num)++;
 		}	
