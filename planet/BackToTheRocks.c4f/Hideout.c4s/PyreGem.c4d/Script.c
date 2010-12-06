611b6309ba1997376e6cbee51972474ab3d1e2fb
MimmoO <mimmo-o@hg.openclonk.org>
2010-12-06 18:46:20 +0100

    Hideout: PyreGem: Transfer thrower for kill-tracing from LifeGem

--- planet/BackToTheRocks.c4f/Hideout.c4s/PyreGem.c4d/Script.c
+++ planet/BackToTheRocks.c4f/Hideout.c4s/PyreGem.c4d/Script.c
@@ -1,6 +1,7 @@
 /*--- Pyre Gem ---*/
 
 local e;
+local thrower;
 
 protected func Initialize()
 {
@@ -29,26 +30,28 @@ protected func Initialize()
 	SetR(Random(360));
 }
 
-protected func Departure()
+protected func Departure( from)
 {
 	SetRDir(RandomX(-15,15));
+	thrower=from;
 }
 
 func Hit()
 {
 
-	AddEffect("GemPyre",nil,100,1,nil,nil,GetX(),GetY(),e,this->GetOwner());
+	AddEffect("GemPyre",nil,100,1,nil,nil,GetX(),GetY(),e,this->GetOwner(),thrower);
 	RemoveObject();
 }
-global func FxGemPyreStart(object target, int num, int temporary, x, y, e, owner)
+global func FxGemPyreStart(object target, int num, int temporary, x, y, e, owner,thrower)
 {
 	if (temporary) 
 		return 1;
 	EffectVar(0, target, num)=x;
 	EffectVar(1, target, num)=y;
 	EffectVar(2, target, num)=e;
-	EffectVar(3, target, num)=owner;
-	EffectVar(4, target, num)=0;
+	EffectVar(3, target, num)=thrower;
+	EffectVar(4, target, num)=owner;
+	EffectVar(5, target, num)=0;
 }
 global func FxGemPyreTimer(object target, int num, int time)
 {
@@ -72,17 +75,17 @@ global func FxGemPyreTimer(object target, int num, int time)
 	for(var obj in FindObjects(Find_NoContainer(), Find_OCF(OCF_Alive), Find_Distance(((time/2)+1)*6,x,y),Find_Not(Find_Distance((time/2)*4,x,y)),Find_ID(Clonk)))
 	{
 		var end=false;	
-		for(var i = 0; i < EffectVar(4, target, num) ; i++)
-			if(obj == EffectVar(5+i, target, num)) end=true;
+		for(var i = 0; i < EffectVar(5, target, num) ; i++)
+			if(obj == EffectVar(6+i, target, num)) end=true;
 		if(end) continue;
 		if(PathFree(x,y,obj->GetX(),obj->GetY()))
 		{
-			obj->DoEnergy((-BoundBy((30-time),1,25)*3)/4);
-			obj->CastParticles("MagicFire",20 + (BoundBy((30-time),1,25)*2),(BoundBy((30-time),5,25)*2),0,0,25,40,clr,clr);
-			obj->CastParticles("Air",10 + BoundBy((30-time),1,25),10,0,0,15,30,clr,clr);
+			obj->DoEnergy((-BoundBy((30-time),1,26)*3)/5,0,0,EffectVar(3,target,num));
+			obj->CastParticles("MagicFire",20 + (BoundBy((30-time),1,26)*2),(BoundBy((30-time),6,26)*2),0,0,26,50,clr,clr);
+			obj->CastParticles("Air",10 + BoundBy((30-time),1,26),10,0,0,16,30,clr,clr);
 			obj->Fling(RandomX(-2,2),-2-(BoundBy((30-time),10,30)/10));
-			EffectVar(5+EffectVar(4, target, num), target, num) = obj;
-			EffectVar(4, target, num)++;
+			EffectVar(6+EffectVar(5, target, num), target, num) = obj;
+			EffectVar(5, target, num)++;
 		}	
 	}
 	return 1;
