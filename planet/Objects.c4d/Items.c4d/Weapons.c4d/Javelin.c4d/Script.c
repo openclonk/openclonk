87faf5c0b54695fc2d15c5522c5fc8566095f0a6
Tobias Zwick <newton@westnordost.de>
2010-12-08 16:34:53 +0100

    Javelin exits the clonk on throw in the right height now

--- planet/Objects.c4d/Items.c4d/Weapons.c4d/Javelin.c4d/Script.c
+++ planet/Objects.c4d/Items.c4d/Weapons.c4d/Javelin.c4d/Script.c
@@ -97,12 +97,6 @@ public func DoThrow(object clonk, int angle)
 {
 	var javelin=TakeObject();
 	
-	// how fast the javelin is thrown depends very much on
-	// the speed of the clonk
-	//var speed = 1200 * clonk->GetPhysical("Throw") / 12000 + 150 * Abs(clonk->GetXDir());
-	//var xdir = Sin(angle,+speed);
-	//var ydir = Cos(angle,-speed);
-	// The clonk can convert some, indicated by div in percentages, of its own kinetic energy to change the momenta of the javelin, a miracle.
 	var div = 60; // 40% is converted to the direction of the throwing angle.
 	var xdir = clonk->GetXDir(1000);
 	var ydir = clonk->GetYDir(1000);
@@ -112,6 +106,7 @@ public func DoThrow(object clonk, int angle)
 		
 	javelin->SetXDir(jav_x, 1000);
 	javelin->SetYDir(jav_y, 1000);
+	javelin->SetPosition(javelin->GetX(),javelin->GetY()+6);
 	
 	SetController(clonk->GetController());
 	javelin->AddEffect("Flight",javelin,1,1,javelin,nil);
