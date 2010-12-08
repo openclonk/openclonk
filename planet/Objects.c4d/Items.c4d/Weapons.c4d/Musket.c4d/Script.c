191921520d3edeb527307a69ece96c66a902de18
Tobias Zwick <newton@westnordost.de>
2010-12-08 01:24:53 +0100

    resolved #508: Continue to aim with musket after musket reloaded Before, the musket would always stop using the musket after reloading even if the mouse button was held down the whole time during reload.

--- planet/Objects.c4d/Items.c4d/Weapons.c4d/Musket.c4d/Script.c
+++ planet/Objects.c4d/Items.c4d/Weapons.c4d/Musket.c4d/Script.c
@@ -49,6 +49,8 @@ local reload;
 local yOffset;
 local iBarrel;
 
+local holding;
+
 local MuskUp; local MuskFront; local MuskDown; local MuskOffset;
 
 protected func HoldingEnabled() { return true; }
@@ -79,6 +81,8 @@ func ControlUseStart(object clonk, int x, int y)
 
 	fAiming = 1;
 
+	holding = true;
+	
 	// reload weapon if not loaded yet
 	if(!loaded)
 		clonk->StartLoad(this);
@@ -95,7 +99,8 @@ public func FinishedLoading(object clonk)
 {
 	SetProperty("PictureTransformation",Trans_Mul(Trans_Translate(500,1000,-000),Trans_Rotate(130,0,1,0),Trans_Rotate(20,0,0,1)));
 	loaded = true;
-	return false; // false means stop here and reset the clonk
+	clonk->StartAim(this);
+	return holding; // false means stop here and reset the clonk
 }
 
 func ControlUseHolding(object clonk, ix, iy)
@@ -110,6 +115,7 @@ func ControlUseHolding(object clonk, ix, iy)
 
 protected func ControlUseStop(object clonk, ix, iy)
 {
+	holding = false;
 	clonk->StopAim();
 	return true;
 }
