bb59adbe1d224d0abb87d73436e40147aaac1908
MimmoO <mimmo-o@hg.openclonk.org>
2010-12-17 16:19:20 +0100

    Thunderours Skies: ThunderScroll: now has a one second delay instead of instant strike.

--- planet/BackToTheRocks.c4f/FreeSkies.c4s/Scrolls.c4d/ThunderScroll.c4d/Script.c
+++ planet/BackToTheRocks.c4f/FreeSkies.c4s/Scrolls.c4d/ThunderScroll.c4d/Script.c
@@ -9,32 +9,52 @@
 public func ControlUse(object pClonk)
 {
 	Sound("Blast3");
-	Exit(-3,-GetY());
-	var x;
-	var wdt = 15;
+	Exit(0,-GetY());
+	AddEffect("ThunderStrike",0,100,1,0,this->GetID(),pClonk->GetOwner(),this->GetX()-5);
+	RemoveObject();
+	return 1;
+}
+global func FxThunderStrikeStart(pTarget, iEffectNumber, iTemp, owner, x)
+{
+	if(iTemp) return;
+	EffectVar(0, pTarget, iEffectNumber)=owner;
+	EffectVar(1, pTarget, iEffectNumber)=x;
+}
+global func FxThunderStrikeTimer(pTarget, iEffectNumber, iEffectTime)
+{
+	var move = EffectVar(1, pTarget, iEffectNumber);
+
+	if(iEffectTime>36)
+	{
+
+	var owner = EffectVar(0, pTarget, iEffectNumber);
+	var x=0;
+	var wdt = 18;
 	var y = [];
 	var targets=[];
 	for(var i = (x-wdt); i < (wdt*2); i++ )
 	{
 		
-		while(!GBackSolid(i,y[i+wdt]) && y[i+wdt] < LandscapeHeight())
+		while(!GBackSolid(i+move,y[i+wdt]) && y[i+wdt] < LandscapeHeight())
 			y[i+wdt]++;
+
 	}
 	
 	for(var i = (x-wdt); i < (wdt*2); i++ )
 	{
 		if(!(i%3))
 			for(var k=0; k<y[i+wdt]; k+=12+Random(6))
-			{
-				CreateParticle("Air",i,k,RandomX(-1,1),RandomX(-1,1),20+Random(41),RGB(255-Random(100),255-Random(50),255-Random(10)),nil,Random(2));
+			{	
+	
+				CreateParticle("Air",i+move,k,RandomX(-1,1),RandomX(-1,1),20+Random(41),RGB(255-Random(100),255-Random(50),255-Random(10)),nil,Random(2));
 				if(!Random(4)) 
-					CreateParticle("AirIntake",i,k,RandomX(-1,1),RandomX(-5,1),40+Random(21),RGB(255-Random(100),255-Random(50),255-Random(10)),nil,Random(2));
+					CreateParticle("AirIntake",i+move,k,RandomX(-1,1),RandomX(-5,1),40+Random(21),RGB(255-Random(100),255-Random(50),255-Random(10)),nil,Random(2));
 			}
 		
 		for(var l=0; l<3; l++)
-			CreateParticle("AirIntake",i,y[i+wdt]-l-2,i+RandomX(-10,10),-10+Random(6),30+Random(30),RGB(255-Random(50),255-Random(30),255-Random(5)),nil,Random(2));
+			CreateParticle("AirIntake",i+move,y[i+wdt]-l-2,i+RandomX(-10,10),-10+Random(6),30+Random(30),RGB(255-Random(50),255-Random(30),255-Random(5)),nil,Random(2));
 		
-		for(var t in FindObjects(Find_Or(Find_And(Find_ID(Clonk),Find_OCF(OCF_Alive)), Find_ID(TargetBalloon)),Find_OnLine(i,-0,i,y[i+wdt])))
+		for(var t in FindObjects(Find_Or(Find_And(Find_ID(Clonk),Find_OCF(OCF_Alive)), Find_ID(TargetBalloon)),Find_OnLine(i+move,-0,i+move,y[i+wdt])))
 		{
 			var add=true;
 			for(var j=0; j<GetLength(targets); j++)
@@ -47,24 +67,35 @@ public func ControlUse(object pClonk)
 	{
 		if(t->GetID() == TargetBalloon)
 		{
-			var arw=CreateObject(Arrow,0,0,pClonk->GetOwner());
+			var arw=CreateObject(Arrow,0,0,owner);
 			t->OnProjectileHit(arw);
 			arw->RemoveObject();
 		}
 		else
 		{
-			if(t!=pClonk)
+			if(t->GetOwner() != owner)
 			{
-				t->DoEnergy(-10,0,0,pClonk->GetOwner());
-				t->Fling((t->GetX()-pClonk->GetX())/5,-4);
+				t->DoEnergy(-10,0,0,owner);
+				t->Fling((t->GetX()-(move))/5,-4);
 			}
 		}
 	}
-	
-	RemoveObject();
-	return 1;
+	return -1;
+	}
+	else if(iEffectTime<4)
+	{
+		if(iEffectTime%3)
+		{
+		
+			while(!GBackSolid(x+move+5,y) && y < LandscapeHeight())
+			{
+				var add=Random((iEffectTime*5))*((Random(2)*2) -1);
+				CreateParticle("Air",x+move+5+add,y,0,RandomX(-2,1),10+(iEffectTime*5),RGB(255-Random(50),255-Random(30),255-Random(5)));
+				y+=Random(4)+3;
+			}
+		}
+	}
 }
-
 
 
 
