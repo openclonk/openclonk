a8abb445fd96625b27ea19b348bff13a00fa8077
Günther Brammer <gbrammer@gmx.de>
2010-12-22 01:59:27 +0100

    Renumber Commands after Energy removal to avoid crashing
    The table used by CommandByName got out of sync with the command numbers.

--- src/game/object/C4Command.h
+++ src/game/object/C4Command.h
@@ -28,43 +28,43 @@
 #include "C4Value.h"
 
 const int32_t C4CMD_None      = 0,
-                                C4CMD_Follow    = 1,
-                                                  C4CMD_MoveTo    = 2,
-                                                                    C4CMD_Enter     = 3,
-                                                                                      C4CMD_Exit      = 4,
-                                                                                                        C4CMD_Grab      = 5,
-                                                                                                                          C4CMD_Build     = 6,
-                                                                                                                                            C4CMD_Throw     = 7,
-                                                                                                                                                              C4CMD_Chop      = 8,
-                                                                                                                                                                                C4CMD_UnGrab    = 9,
-                                                                                                                                                                                                  C4CMD_Jump      = 10,
-                                                                                                                                                                                                                    C4CMD_Wait      = 11,
-                                                                                                                                                                                                                                      C4CMD_Get       = 12,
-                                                                                                                                                                                                                                                        C4CMD_Put       = 13,
-                                                                                                                                                                                                                                                                          C4CMD_Drop      = 14,
-                                                                                                                                                                                                                                                                                            C4CMD_Dig       = 15,
-                                                                                                                                                                                                                                                                                                              C4CMD_Activate  = 16,
-                                                                                                                                                                                                                                                                                                                                C4CMD_PushTo    = 17,
-                                                                                                                                                                                                                                                                                                                                                  C4CMD_Construct = 18,
-                                                                                                                                                                                                                                                                                                                                                                    C4CMD_Transfer  = 19,
-                                                                                                                                                                                                                                                                                                                                                                                      C4CMD_Attack    = 20,
-                                                                                                                                                                                                                                                                                                                                                                                                        C4CMD_Context   = 21,
-                                                                                                                                                                                                                                                                                                                                                                                                                          C4CMD_Buy       = 22,
-                                                                                                                                                                                                                                                                                                                                                                                                                                            C4CMD_Sell      = 23,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                              C4CMD_Acquire   = 24,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  C4CMD_Retry     = 26,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    C4CMD_Home      = 27,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      C4CMD_Call      = 28,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        C4CMD_Take      = 29, // carlo
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          C4CMD_Take2     = 30; // carlo
+              C4CMD_Follow    = 1,
+              C4CMD_MoveTo    = 2,
+              C4CMD_Enter     = 3,
+              C4CMD_Exit      = 4,
+              C4CMD_Grab      = 5,
+              C4CMD_Build     = 6,
+              C4CMD_Throw     = 7,
+              C4CMD_Chop      = 8,
+              C4CMD_UnGrab    = 9,
+              C4CMD_Jump      = 10,
+              C4CMD_Wait      = 11,
+              C4CMD_Get       = 12,
+              C4CMD_Put       = 13,
+              C4CMD_Drop      = 14,
+              C4CMD_Dig       = 15,
+              C4CMD_Activate  = 16,
+              C4CMD_PushTo    = 17,
+              C4CMD_Construct = 18,
+              C4CMD_Transfer  = 19,
+              C4CMD_Attack    = 20,
+              C4CMD_Context   = 21,
+              C4CMD_Buy       = 22,
+              C4CMD_Sell      = 23,
+              C4CMD_Acquire   = 24,
+              C4CMD_Retry     = 25,
+              C4CMD_Home      = 26,
+              C4CMD_Call      = 27,
+              C4CMD_Take      = 28, // carlo
+              C4CMD_Take2     = 29; // carlo
 
 const int32_t C4CMD_First     = C4CMD_Follow,
-                                C4CMD_Last      = C4CMD_Take2; // carlo
+              C4CMD_Last      = C4CMD_Take2; // carlo
 
 const int32_t C4CMD_Mode_SilentSub  = 0, // subcommand; failure will cause base to fail (no message in case of failure)
-                                      C4CMD_Mode_Base       = 1, // regular base command
-                                                              C4CMD_Mode_SilentBase = 2, // silent base command (no message in case of failure)
-                                                                                      C4CMD_Mode_Sub    = 3; // subcommand; failure will cause base to fail
+              C4CMD_Mode_Base       = 1, // regular base command
+              C4CMD_Mode_SilentBase = 2, // silent base command (no message in case of failure)
+              C4CMD_Mode_Sub    = 3; // subcommand; failure will cause base to fail
 
 // MoveTo and Enter command options: Include push target
 const int32_t C4CMD_MoveTo_NoPosAdjust = 1,
