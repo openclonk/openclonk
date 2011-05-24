53cf29ce5e497ab951e17cbb4b594f8eda4a5915
MimmoO <mimmo-o@hg.openclonk.org>
2011-05-24 17:55:43 +0200

    Added test scenario for Moss

---
+++ planet/Tests.ocf/Moss_Test.ocs/Script.c
@@ -0,0 +1,43 @@
+/* MOOSSSSSS */
+
+func Initialize()
+{
+
+
+	CreateObject(Moss,280,380);
+	CreateObject(Moss,166,166);
+	Log("Try /fast 10 for faster growing!");
+	
+}
+
+ func InitializePlayer(int iPlr, int iX, int iY, object pBase, int iTeam)
+{
+	SetFoW(0,iPlr);
+	JoinPlayer(iPlr);
+	return;
+}
+
+ func RelaunchPlayer(int iPlr)
+{
+	var clonk = CreateObject(Clonk, 0, 0, iPlr);
+	clonk->MakeCrewMember(iPlr);
+	SetCursor(iPlr,clonk);
+	JoinPlayer(iPlr);
+	return;
+}
+
+ func JoinPlayer(int iPlr)
+{
+	var clonk = GetCrew(iPlr);
+	clonk->DoEnergy(100000);
+	clonk->SetPosition(150, 100);
+//	clonk->CreateContents(Javelin);
+//		clonk->CreateContents(DynamiteBox);
+	clonk->CreateContents(Bow);
+	clonk->Collect(CreateObject(Arrow));
+	clonk->CreateContents(Firestone);
+		clonk->CreateContents(Shovel);
+	clonk->CreateContents(GrappleBow);
+	return;
+	
+}
