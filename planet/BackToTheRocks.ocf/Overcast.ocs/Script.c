693c1f68aafd49d97302663339129a1de6e4bd81
Maikel de Vries <maikeldevries@hotmail.com>
2011-09-05 17:07:00 +0200

    Overcast: Completely redesigned
    The old map had at least the flaw that there was room for campers.

--- planet/BackToTheRocks.ocf/Overcast.ocs/Script.c
+++ planet/BackToTheRocks.ocf/Overcast.ocs/Script.c
@@ -11,91 +11,290 @@ protected func Initialize()
 {
 
 	// Goal.
-	CreateObject(Goal_LastManStanding, 0, 0, NO_OWNER);
+	CreateObject(Goal_LastManStanding);
 	CreateObject(Rule_KillLogs);
 	
 	//Enviroment.
 	CreateObject(Environment_Clouds);
 	CreateObject(Environment_Clouds); //Double the clouds.
 	SetSkyAdjust(RGBa(250,250,255,128),RGB(200,200,220));
-	Sound("BirdsLoop.ogg",true,100,nil,+1);
+	Sound("BirdsLoop.ogg", true, 100, nil, 1);
 	
-	// Chests with weapons.
-	CreateObject(Chest, 200, 150, NO_OWNER);
-	CreateObject(Chest, 400, 460, NO_OWNER);
-	CreateObject(Chest, 380, 220, NO_OWNER);
-	CreateObject(Chest, 625, 300, NO_OWNER);
-	CreateObject(Chest, 100, 480, NO_OWNER);
-	CreateObject(Chest, 65, 660, NO_OWNER)->SetClrModulation(RGBa(100,180,255,220));
-
-	AddEffect("IntFillChests", nil, 100, 2 * 36, this);
 	// Smooth brick edges.
-	AddEffect("ChanneledWind", nil, 100, 1, this);
-	PlaceEdges();
-	MovingBricks();
-	return;
-}
-
-global func MovingBricks()
-{
-	CreateObject(MovingBrick,265,180)->Room(0,85,0,0,8);
-	CreateObject(MovingBrick,265,500)->Room(0,40,2,0,7);
-	CreateObject(MovingBrick,320,500)->Room(0,50,3,0,6);
-	CreateObject(MovingBrick,660,430)->Room(0,75,3,0,9);
-	CreateObject(MovingBrick,140,480)->Room(0,180,1,0,17);
-	DrawMap(119,490,10,70,"map plainbrick{mat=Tunnel; tex=brickback;};");
-	DrawMap(121,490,10,70,"map plainbrick{mat=Tunnel; tex=brickback;};");
-}
-
-global func FxChanneledWindTimer()
-{
-	for(var obj in FindObjects(Find_InRect(110,150,30,280)))
-	{
-		obj->SetYDir(Min(obj->GetYDir()-3,-14));
-	}
-	for(var obj in FindObjects(Find_InRect(110,130,70,40)))
-	{
-		obj->SetXDir(obj->GetXDir()+2);
-	}
-	for(var obj in FindObjects(Find_InRect(110,0,100,100)))
-	{
-		obj->SetXDir(obj->GetXDir()+2);
-	}
-	CreateParticle("AirIntake",110+Random(30),230+Random(185),RandomX(-1,1),-30,60+Random(10),RGB(100+Random(25),128+Random(20),255));
-	CreateParticle("AirIntake",110+Random(30),230+Random(180),RandomX(-1,1),-30,60+Random(10),RGB(100+Random(25),128+Random(20),255));
-	CreateParticle("AirIntake",110+Random(70),130+Random(30) , 30,Random(3),60+Random(10),RGB(100+Random(25),128+Random(20),255));
-	CreateParticle("AirIntake",110+Random(80),Random(100) , 30,Random(3),60+Random(10),RGB(100+Random(25),128+Random(20),255));
-}
-
-global func PlaceEdges()
-{
-	var x=[545, 535, 555, 565, 195, 185, 165, 155, 455, 445, 315, 325, 535, 525, 525, 525, 525, 545, 545, 545, 525, 535, 525, 525, 525, 685, 675, 635, 625, 155, 145, 225, 205, 85, 375, 435, 445, 595, 585, 575, 545, 535, 525, 515, 115, 835, 815, 795, 785, 855, 855, 1315, 1165, 1205, 1415, 1425, 1405, 875, 875, 775, 775, 185, 185, 175, 305, 305, 465, 465, 145, 105, 155, 105, 75, 85, 755, 775, 785, 475, 475, 75, 55, 55, 215, 215, 205, 365, 365, 375, 455, 445, 455, 455, 455, 365, 365, 605, 605, 585, 595, 1195, 1195, 1205, 1165, 1155, 1155, 1425, 1435, 1435, 65, 75, 85, 95, 65, 136, 114, 136, 114];
-	var y=[515, 515, 315, 315, 495, 495, 165, 165, 245, 245, 245, 245, 565, 555, 535, 545, 565, 555, 565, 535, 535, 565, 545, 555, 525, 515, 505, 505, 515, 405, 175, 175, 505, 495, 495, 495, 505, 385, 355, 325, 325, 335, 345, 355, 135, 315, 215, 505, 515, 515, 545, 45, 415, 115, 415, 425, 435, 245, 255, 285, 275, 125, 115, 115, 255, 265, 265, 255, 415, 315, 415, 445, 455, 445, 335, 495, 495, 365, 375, 485, 485, 475, 525, 515, 525, 525, 515, 525, 515, 535, 525, 525, 535, 515, 505, 425, 415, 415, 425, 145, 135, 145, 435, 425, 435, 445, 435, 445, 495, 475, 465, 455, 465, 545, 555, 495, 495];
-	var d=[0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 2, 1, 1, 3, 3, 0, 2, 2, 3, 3, 1, 3, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 1, 2, 0, 3, 0, 2, 3, 1, 1, 0, 0, 3, 0, 2, 3, 1, 2, 0, 1, 1, 3, 2, 0, 3, 0, 2, 2, 1, 1, 2, 3, 0, 1, 3, 0, 3, 1, 2, 0, 3, 3, 1, 2, 0, 3, 0, 2, 2, 3, 1, 2, 0, 3, 3, 3, 1, 2, 2, 1, 3, 3, 0, 2, 3, 2, 2, 2, 1, 2, 3, 0, 1];
+	var x=[557, 565, 124, 213, 213, 172, 149, 909, 804, 909, 828, 692, 804, 812, 820, 828, 836, 844, 852, 860, 901, 893, 885, 877, 829, 837, 724, 581, 573, 364, 372, 372, 380, 589, 508, 461, 412, 508, 452, 517, 461, 781, 773, 829, 837, 549, 325, 292, 221, 180, 44, 109, 117, 125, 133, 261, 269, 277, 285, 293, 236, 228, 220, 212, 309, 197, 205, 213, 221, 229, 124, 100, 92, 380, 573, 573, 716, 789, 797, 805, 813, 821, 829, 909, 852, 716, 812, 812, 868, 525, 436, 436, 525];
+	var y=[517, 509, 676, 692, 677, 525, 524, 445, 277, 396, 396, 340, 260, 252, 244, 236, 228, 220, 212, 204, 228, 220, 212, 204, 165, 157, 132, 45, 53, 109, 117, 45, 53, 109, 100, 100, 293, 284, 237, 237, 284, 348, 340, 132, 140, 284, 148, 148, 148, 148, 212, 212, 220, 228, 236, 236, 244, 252, 260, 268, 236, 244, 252, 260, 285, 396, 404, 412, 420, 428, 396, 404, 412, 484, 485, 500, 485, 484, 492, 500, 508, 516, 524, 524, 524, 500, 668, 653, 644, 677, 692, 677, 692];
+	var d=[2, 2, 1, 2, 0, 1, 0, 2, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 1, 2, 2, 3, 3, 3, 3, 2, 1, 0, 3, 1, 3, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 3, 3, 1, 1, 0, 3, 1, 2];
 	for (var i = 0; i < GetLength(x); i++)
 	{
-		var edge=CreateObject(BrickEdge, x[i], y[i] + 5, NO_OWNER);
+		var edge=CreateObject(BrickEdge, x[i], y[i], NO_OWNER);
 		edge->Initialize();
 		edge->SetP(d[i]);
 		edge->SetPosition(x[i],y[i]);
 		edge->PermaEdge();
 	}
-	CreateObject(BrickEdge,90,690)->PermaEdge();	
 	
-	var x=[679, 685, 691, 608, 598, 587, 579, 572, 566, 560, 553, 546, 542, 537, 532, 526, 521, 514, 506, 497, 489, 481, 474, 470, 462, 456, 451, 444, 426, 418, 408, 398, 388, 379, 372, 362, 352, 342, 333, 325, 320, 313, 308, 301, 173, 181, 190, 199, 209, 223, 230, 161, 155, 185, 62, 70, 52, 63, 52, 72, 81, 218, 87, 48, 170, 178, 159, 198, 204, 211, 220, 148, 140, 134, 115, 105, 89, 82, 76, 363, 371, 377, 386, 396, 407, 417, 426, 435, 442, 448, 455, 523, 529, 536, 539, 546, 553, 565, 576, 587, 597, 608, 617, 625, 630, 637, 672, 189];
-	var y=[504, 511, 517, 413, 384, 353, 324, 319, 313, 309, 314, 319, 324, 330, 335, 342, 346, 352, 357, 358, 358, 357, 363, 256, 251, 244, 237, 245, 249, 248, 248, 248, 248, 247, 247, 247, 247, 247, 248, 243, 238, 244, 248, 254, 166, 167, 166, 168, 168, 171, 177, 406, 401, 493, 423, 396, 476, 677, 677, 676, 676, 166, 683, 689, 498, 497, 497, 494, 501, 509, 517, 498, 496, 490, 490, 497, 495, 487, 483, 504, 497, 491, 487, 487, 487, 487, 487, 490, 497, 505, 511, 526, 519, 514, 510, 514, 518, 517, 518, 517, 517, 517, 517, 512, 507, 500, 499, 488];
-	for (var i = 0; i < GetLength(x); i++)
-	{
-		var gras=CreateObject(Grass, x[i], y[i], NO_OWNER);
-		//while(!GBackSolid(x[i],gras->GetY())) gras->SetPosition(x[i],gras->GetY()+1);
-		gras->SetPosition(x[i],gras->GetY()-2+Random(2));
-		gras->SetCategory(C4D_StaticBack);
-		gras->DoCon(Random(25));
-	}
+	// Vertically moving bricks
+	var brick = CreateObject(MovingBrick, 660, 90);
+	brick->SetSize(3);
+	brick->MoveVertical(80, 144);
+	
+	var brick = CreateObject(MovingBrick, 592, 270);
+	brick->SetSize(2);
+	brick->MoveVertical(264, 344);
+	var brick = CreateObject(MovingBrick, 632, 285);
+	brick->SetSize(2);
+	brick->MoveVertical(272, 352);
+	var brick = CreateObject(MovingBrick, 672, 300);
+	brick->SetSize(2);
+	brick->MoveVertical(280, 360);
+	
+	var brick = CreateObject(MovingBrick, 270, 430);
+	brick->SetSize(2);
+	brick->MoveVertical(416, 488);
+	var brick = CreateObject(MovingBrick, 310, 440);
+	brick->SetSize(2);
+	brick->MoveVertical(424, 496);
+	var brick = CreateObject(MovingBrick, 350, 450);
+	brick->SetSize(2);
+	brick->MoveVertical(432, 504);
+	
+	var brick = CreateObject(MovingBrick, 932, 240);
+	brick->SetSize(4);
+	brick->MoveVertical(232, 536);
+	
+	var brick = CreateObject(MovingBrick, 368, 200);
+	brick->SetSize(3);
+	brick->MoveVertical(152, 288);
+	
+	var brick = CreateObject(MovingBrick, 184, 320);
+	brick->SetSize(2);
+	brick->MoveVertical(240, 368);
+	
+	var brick = CreateObject(MovingBrick, 498, 560);
+	brick->SetSize(2);
+	brick->MoveVertical(480, 680);
+	DrawMaterialQuad("Tunnel-Brickback", 478, 480, 497, 480, 497, 552, 478, 552);
+	DrawMaterialQuad("Tunnel-Brickback", 478, 672, 497, 672, 497, 680, 478, 680);
+	
+	var brick = CreateObject(MovingBrick, 170, 600);
+	brick->SetSize(2);
+	brick->MoveVertical(528, 680);
+	DrawMaterialQuad("Tunnel-Brickback", 150, 524, 169, 524, 169, 576, 150, 576);
+	DrawMaterialQuad("Tunnel-Brickback", 150, 672, 169, 672, 169, 680, 150, 680);
+	
+	// Horizontally moving bricks.
+	var brick = CreateObject(MovingBrick, 600, 496);
+	brick->SetSize(4);
+	brick->MoveHorizontal(552, 736);
+	
+	var brick = CreateObject(MovingBrick, 656, 688);
+	brick->SetSize(3);
+	brick->MoveHorizontal(504, 668);
+	var brick = CreateObject(MovingBrick, 688, 664);
+	brick->SetSize(3);
+	brick->MoveHorizontal(668, 832);
+	
+	var brick = CreateObject(MovingBrick, 320, 688);
+	brick->SetSize(3);
+	brick->MoveHorizontal(192, 456);
+	
+	// Chests with weapons.
+	var chest;
+	chest = CreateObject(Chest, 764, 128, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 732, 336, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 116, 400, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 272, 152, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 424, 480, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 872, 520, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	chest = CreateObject(Chest, 72, 208, NO_OWNER);
+	AddEffect("FillChest", chest, 100, 72);
+	
+	// Blue chest with jar of winds.
+	var chest_blue = CreateObject(Chest, 850, 648, NO_OWNER);
+	chest_blue->SetClrModulation(RGB(100,180,255));
+	AddEffect("FillBlueChest", chest_blue, 100, 72);
+	
+	// Red chest with club.
+	var chest_red = CreateObject(Chest, 124, 520, NO_OWNER);
+	chest_red->SetClrModulation(RGB(255, 100, 100));
+	AddEffect("FillRedChest", chest_red, 100, 72);
+		
+	// Wind channel.
+	AddEffect("WindChannel", nil, 100, 1);
+	return;
+}
+
+/*-- Wind channel --*/
+
+global func FxWindChannelStart(object target, proplist effect, int temporary)
+{
+	if (temporary == 0)
+		effect.Divider = 1; 
 	return 1;
 }
-	
 
+global func FxWindChannelTimer(object target, proplist effect)
+{
+	// Channel divided into three parts.
+	
+	// First shaft with upward wind.
+	for (var obj in FindObjects(Find_InRect(464, 288, 40, 152)))
+	{
+		var speed = obj->GetYDir(100);
+		if (speed < -360)
+			continue;
+		obj->SetYDir(speed - 32, 100);
+	}
+	CreateParticle("AirIntake", 464+Random(40), 344+Random(112), RandomX(-1,1), -30, 60+Random(10), RGB(100+Random(25),128+Random(20),255));
+	
+	// Divider with random wind.
+	if (!Random(100))
+		effect.Divider = Random(3);
+	for (var obj in FindObjects(Find_InRect(464, 240, 40, 48)))
+	{
+		if (effect.Divider == 1)
+		{
+			var speed = obj->GetYDir(100);
+			if (speed < -360)
+				continue;
+			obj->SetYDir(speed - 32, 100);
+		}
+		if (effect.Divider == 0)
+		{
+			var speed = obj->GetXDir(100);
+			if (speed > -360)
+				obj->SetXDir(speed - 20, 100);			
+		}
+		if (effect.Divider == 2)
+		{
+			var speed = obj->GetXDir(100);
+			if (speed < 360)
+				obj->SetXDir(speed + 20, 100);			
+		}
+	}
+	if (effect.Divider == 1)
+		CreateParticle("AirIntake", 464+Random(40), 280+Random(10), RandomX(-1,1), -30, 60+Random(10), RGB(100+Random(25),128+Random(20),255));
+	if (effect.Divider == 0)
+		CreateParticle("AirIntake", 464+Random(40), 280+Random(10), RandomX(-20,-10), -20, 60+Random(10), RGB(100+Random(25),128+Random(20),255));
+	if (effect.Divider == 2)
+		CreateParticle("AirIntake", 464+Random(40), 280+Random(10), RandomX(10,20), -20, 60+Random(10), RGB(100+Random(25),128+Random(20),255));
+		
+	// Second shaft with upward wind.
+	for (var obj in FindObjects(Find_InRect(464, 104, 40, 136)))
+	{
+		var speed = obj->GetYDir(100);
+		if (speed < -360)
+			continue;
+		obj->SetYDir(speed - 32, 100);
+	}
+	CreateParticle("AirIntake", 464+Random(40), 160+Random(96), RandomX(-1,1), -30, 60+Random(10), RGB(100+Random(25),128+Random(20),255));
+	
+	return 1;
+}
+
+/*-- Chest contents --*/
+
+global func FxFillChestStart(object target, proplist effect, int temporary)
+{
+	if (temporary) 
+		return 1;
+	var w_list = [Shield, Javelin, FireballScroll, Bow, Musket, WindScroll, TeleportScroll];
+	for (var i = 0; i < 4; i++)
+		target->CreateChestContents(w_list[Random(GetLength(w_list))]);
+	return 1;
+}
+
+global func FxFillChestTimer(object target, proplist effect)
+{
+	if (Random(5))
+		return 1;
+	var w_list = [Boompack, Dynamite, Shield, Javelin, Bow, Musket, Boompack, Dynamite, Shield, Javelin, Bow, Musket, TeleportScroll, WindScroll, FireballScroll];
+
+	if (target->ContentsCount() < 6)
+		target->CreateChestContents(w_list[Random(GetLength(w_list))]);
+	return 1;
+}
+
+global func FxFillBlueChestStart(object target, proplist effect, int temporary)
+{
+	if (temporary) 
+		return 1;		
+	target->CreateContents(JarOfWinds);
+	var w_list = [Firestone, Boompack, FireballScroll, WindScroll];
+	for (var i = 0; i < 4; i++)
+		target->CreateContents(w_list[Random(GetLength(w_list))]);	
+	return 1;
+}
+
+global func FxFillBlueChestTimer(object target, proplist effect)
+{
+	if (Random(5))
+		return 1;
+	var w_list = [Firestone, Boompack, FireballScroll, WindScroll];
+
+	if (target->ContentsCount() < 6)
+		target->CreateContents(w_list[Random(GetLength(w_list))]);
+	return 1;
+}
+
+global func FxFillRedChestStart(object target, proplist effect, int temporary)
+{
+	if (temporary) 
+		return 1;
+	target->CreateContents(Club);
+	var w_list = [Firestone, Boompack, FireballScroll, WindScroll];
+	for (var i = 0; i < 4; i++)
+		target->CreateContents(w_list[Random(GetLength(w_list))]);	
+	return 1;
+}
+
+global func FxFillRedChestTimer(object target, proplist effect)
+{
+	if (Random(5))
+		return 1;
+	var w_list = [Firestone, Boompack, FireballScroll, WindScroll];
+
+	if (target->ContentsCount() < 6)
+		target->CreateContents(w_list[Random(GetLength(w_list))]);
+	return 1;
+}
+
+global func CreateChestContents(id obj_id)
+{
+	if (!this)
+		return;
+	var obj = CreateObject(obj_id);
+	if (obj_id == Bow)
+		obj->CreateContents(Arrow);
+	if (obj_id == Musket)
+		obj->CreateContents(LeadShot);
+	obj->Enter(this);
+	return;
+}
+
+/*-- Player control --*/
+
+// GameCall from RelaunchContainer.
+func OnClonkLeftRelaunch(object clonk)
+{
+	var pos = GetRandomSpawn();
+	clonk->SetPosition(pos[0],pos[1]);
+	CastParticles("Magic",36,12,pos[0],pos[1],30,60,clonk->GetColor(),clonk->GetColor(),clonk);
+	return;
+}
+
+global func GetRandomSpawn()
+{
+	var spawns = [[432,270],[136,382],[200,134],[864,190],[856,382],[840,518],[408,86],[536,470]];
+	var rand = Random(GetLength(spawns));
+	return spawns[rand];
+}
 
 
 // Gamecall from LastManStanding goal, on respawning.
@@ -107,63 +306,5 @@ protected func OnPlayerRelaunch(int plr)
 	return;
 }
 
-
-// Refill/fill chests.
-global func FxIntFillChestsStart(object target, effect, int temporary)
-{
-	if(temporary) return 1;
-	var chests = FindObjects(Find_ID(Chest),Find_InRect(0,0,LandscapeWidth(),610));
-	var w_list = [Shield, Javelin, FireballScroll, Bow, Musket, WindScroll, TeleportScroll];
-	
-	for(var chest in chests)
-		for(var i=0; i<4; ++i)
-			chest->CreateChestContents(w_list[Random(GetLength(w_list))]);
-	return 1;
-}
-
-global func FxIntFillChestsTimer()
-{
-	SetTemperature(100);
-	var chest = FindObjects(Find_ID(Chest), Sort_Random())[0];
-	var w_list = [Boompack, Dynamite, Shield,Javelin, Bow, Musket, Boompack, Dynamite, Shield,Javelin, Bow, Musket, TeleportScroll, WindScroll, FireballScroll];
-	if(chest->GetY()>600)
-	{
-		w_list = [FireballScroll, FireballScroll, TeleportScroll, WindScroll, WindScroll];
-		if(!FindObject(Find_ID(JarOfWinds))) w_list[GetLength(w_list)]=JarOfWinds;
-	}
-	if (chest->ContentsCount() < 5)
-		chest->CreateChestContents(w_list[Random(GetLength(w_list))]);
-	return 1;
-}
-
-global func CreateChestContents(id obj_id)
-{
-if (!this)
-		return;
-	var obj = CreateObject(obj_id);
-	if (obj_id == Bow)
-		obj->CreateContents(Arrow);
-	if (obj_id == Musket)
-		obj->CreateContents(LeadShot);
-	obj->Enter(this);
-	return;
-}
-
-// GameCall from RelaunchContainer.
-func OnClonkLeftRelaunch(object clonk)
-{
-	var r=Random(5);
-	var x,y;
-	if(r==0) { x=370; y=220; }
-	if(r==1) { x=600; y=500; }
-	if(r==2) { x=100; y=470; }
-	if(r==3) { x=500; y=330; }
-	if(r==4) { x=200; y=150; }
-	if(r==5) { x=400; y=470; }
-	clonk->SetPosition(x,y);
-	CastParticles("Magic",36,12,x,y,30,60,clonk->GetColor(),clonk->GetColor(),clonk);
-	clonk->SetYDir(-5);
-}
-
 func KillsToRelaunch() { return 0; }
-func RelaunchWeaponList() { return [Bow,  Javelin, Musket, FireballScroll, WindScroll, TeleportScroll]; }
+func RelaunchWeaponList() { return [Bow, Javelin, Musket, FireballScroll, WindScroll, TeleportScroll]; }
