06104a908d2edd2753fb6bdbd5fc6fb4f8dc477f
Maikel de Vries <maikeldevries@hotmail.com>
2011-09-06 18:34:26 +0200

    Boomshire: Fixed doors and brickedges
    Did it the easy way, let's hope Guenthers landscape patch will make brickedges obsolete soon.

---
+++ planet/BackToTheRocks.ocf/Boomshire.ocs/StoneDoor.ocd/Script.c
@@ -0,0 +1,67 @@
+/*-- Stone Door --*/
+
+protected func Initialize()
+{
+	SetAction("Door");
+	SetComDir(COMD_None);
+	return;
+}
+
+public func OpenGateDoor()
+{
+	AddEffect("IntMoveGateUp", this, 100, 1, this);
+	Sound("GateMove");
+	return;
+}
+
+public func CloseGateDoor()
+{
+	AddEffect("IntMoveGateDown", this, 100, 1, this);
+	Sound("GateMove");
+	return;
+}
+
+protected func FxIntMoveGateUpTimer(object target)
+{
+	if (GBackSolid(0, -20))
+	{
+		Sound("GateHit.ogg");
+		SetYDir(0);
+		return -1;
+	}
+	
+	SetYDir(-5);
+	return 1;
+}
+
+protected func FxIntMoveGateDownTimer(object target)
+{
+	if (GBackSolid(0, 19))
+	{
+		Sound("GateHit.ogg");
+		SetYDir(0);
+		return -1;
+	}
+	
+	SetYDir(5);
+	return 1;
+}	
+
+func Definition(def) 
+{
+	SetProperty("ActMap", {
+		Door = {
+			Prototype = Action,
+			Name = "Door",
+			Procedure = DFA_FLOAT,
+			Length = 1,
+			Delay = 1,
+			X = 0,
+			Y = 0,
+			Wdt = 10,
+			Hgt = 40,
+			NextAction = "Door",
+		},
+	}, def);
+	SetProperty("Name", "$Name$", def);
+}
