fc69b49f5090ee3cdbd68f9cc4aa00fdbfea1bea
David Dormagen <czapper@gmx.de>
2011-09-18 10:59:38 +0200

    simple flame object added

---
+++ planet/Objects.ocd/Effects.ocd/Flame.ocd/Script.c
@@ -0,0 +1,23 @@
+local Name = "$Name$";
+local Description = "$Description$";
+
+public func Initialize()
+{
+	Incinerate();
+	return true;
+}
+
+func Burning()
+{
+	if(GetCon() > 50)
+	if(!Random(3))
+	{
+		var x = Random(15);
+		var o = CreateObject(GetID(), 0, 0, GetOwner());
+		o->SetSpeed(x, -7);
+		o->SetCon(GetCon()/2);
+		SetSpeed(-x, -7);
+		SetCon(GetCon()/2);
+		
+	}
+}
