4920f8b6931cf359cb9cf7412e625566d1a88b72
Charles Spurrill <carlonfire@gmail.com>
2011-10-12 17:27:58 -0700

    Added functions to tree for axe interaction

--- planet/Objects.ocd/Environment.ocd/Vegetation.ocd/Trees.ocd/Coniferous.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Vegetation.ocd/Trees.ocd/Coniferous.ocd/Script.c
@@ -15,4 +15,7 @@ private func Initialize()
 	SetProperty("MeshTransformation", Trans_Rotate(RandomX(0,359),0,1,0));
 }
 
+public func IsTree() { return true; }
+public func IsStanding() { return true; }
+
 local Name = "$Name$";
