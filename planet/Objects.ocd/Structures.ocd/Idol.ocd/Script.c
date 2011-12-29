35acbb0764e20225f739473a5f121be9226ab074
David Dormagen <czapper@gmx.de>
2011-12-29 20:21:52 +0100

    Gold Idol added (temporary graphics)

---
+++ planet/Objects.ocd/Structures.ocd/Idol.ocd/Script.c
@@ -0,0 +1,12 @@
+protected func Construction()
+{
+	SetCategory(C4D_Vehicle);
+	return _inherited(...);
+}
+
+private func MenuOnInteraction() { return false; }
+private func MenuOnControlUse() { return false; }
+private func MenuOnControlUseAlt() { return false; }
+
+local Name = "$Name$";
+local Touchable = 1;
