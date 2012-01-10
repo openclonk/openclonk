feafa4da2fdef1a70432ae7c27e0b551771529b3
Maikel de Vries <maikeldevries@hotmail.com>
2012-01-10 21:01:53 +0100

    Fixed team account

--- planet/Objects.ocd/Rules.ocd/TeamAccount.ocd/Script.c
+++ planet/Objects.ocd/Rules.ocd/TeamAccount.ocd/Script.c
@@ -6,36 +6,40 @@
 */
 
 
-protected func Initialize()
+// Only SetWealth needs to be overloaded, DoWealth just uses that.
+global func SetWealth(int plr, int wealth)
 {
-	// Give all existing players an account.
+	// Only if team account rule is activated.
+	if (!FindObject(Find_ID(Rule_TeamAccount)))
+		return _inherited(plr, wealth, ...);
+		
+	// Only for valid players.
+	if (plr == NO_OWNER || !GetPlayerName(plr))
+		return _inherited(plr, wealth, ...);
+		
+	// Also set wealth of all allies.
 	for (var i = 0; i < GetPlayerCount(); i++)
 	{
-		var plr = GetPlayerByIndex(i);
-		AddEffect("IntTeamAccount", nil, 100, 2, this, nil, plr);		
+		var to_plr = GetPlayerByIndex(i);
+		if (to_plr != plr && !Hostile(to_plr, plr))
+			_inherited(to_plr, wealth, ...);
 	}
-	return;
+
+	return _inherited(plr, wealth, ...);
 }
 
 protected func InitializePlayer(int plr)
 {
-	// Give new player an account.
-	var account = AddEffect("IntTeamAccount", nil, 100, 2, this, nil, plr);
-	// Store wealth.
-	var wealth = GetWealth(plr);
-	// Add wealth of allies to player's account.
+	// Find an ally and add this wealth.
 	for (var i = 0; i < GetPlayerCount(); i++)
 	{
-		var allied_plr = GetPlayerByIndex(i);
-		EffectCall(nil, account, "Change", allied_plr, GetWealth(allied_plr));
-	}
-	// Add original wealth to all allies.
-	var i = 0, effect;
-	while (effect = GetEffect("IntTeamAccount", nil, i, 0))
-	{
-		EffectCall(nil, effect, "Change", plr, wealth);
-		i++;
-	}
+		var to_plr = GetPlayerByIndex(i);
+		if (to_plr != plr && !Hostile(to_plr, plr))
+		{
+			DoWealth(to_plr, GetWealth(plr));
+			break;
+		}
+	}	
 	return;
 }
 
@@ -57,7 +61,7 @@ protected func OnTeamSwitch(int player, int new_team, int old_team)
 	var share = GetWealth(player) / count;
 
 	// Add player to new team, i.e. add his wealth.
-
+	// TODO Implement
 	return;
 }
 
@@ -71,57 +75,6 @@ protected func Activate(int byplr)
 {
 	MessageWindow("$Description$", byplr);
 	return;
-}
-
-
-/*-- Team Account --*/
-// Every player gets an effect which stores the current wealth.
-
-protected func FxIntTeamAccountStart(object target, proplist effect, int temporary, int plr)
-{
-	// Effect start: store player number and player's wealth.
-	effect.Plr = plr;
-	effect.Wealth = GetWealth(plr);
-	return 1;
-}
-
-protected func FxIntTeamAccountTimer(object target, proplist effect, int time)
-{
-	// Remove effect if player is non-existant.
-	if (!GetPlayerName(effect.Plr))
-		return -1;
-	// Register wealth change.
-	var change = GetWealth(effect.Plr) - effect.Wealth;
-	if (change != 0)
-	{
-		// Reset effect Wealth.
-		effect.Wealth = GetWealth(effect.Plr);
-		// Apply wealth change to allied accounts.
-		var i = 0, account;
-		while (account = GetEffect("IntTeamAccount", target, i, 0))
-		{
-			EffectCall(nil, account, "Change", effect.Plr, change);
-			i++;
-		}			
-	}
-	return 1;
-}
-
-protected func FxIntTeamAccountStop(object target, proplist effect, int reason, bool temporary)
-{
-
-	return 1;
-}
-
-protected func FxIntTeamAccountChange(object target, proplist effect, int from_plr, int change)
-{
-	// Only accept changes from allied players.
-	if (effect.Plr == from_plr || Hostile(effect.Plr, from_plr))
-		return 1;
-	// Change both wealth and effect var.
-	DoWealth(effect.Plr, change);
-	effect.Wealth += change;	
-	return 1;
 }
 
 /*-- Proplist --*/
