139184c540dbc94dde6fe0e5cd35f15a61256067
Armin Burgmeier <armin@arbur.net>
2012-02-03 23:43:18 +0100

    Set system data path to exepath for binaries with automatic updates
    Updates work only when the game data is at the same location as the binary.
    If this is not the case then the game was probably installed differently,
    for example via the distribution or with make install. In this case we
    cannot do automatic updates but we also want to use a different system path.
    
    This fixes the linux development snapshots and release tarballs. They were
    broken in the sense that they didn't find their game data.

--- src/config/C4Config.cpp
+++ src/config/C4Config.cpp
@@ -460,10 +460,16 @@ void C4ConfigGeneral::DeterminePaths()
 #elif defined(__linux__)
 
 #ifdef OC_SYSTEM_DATA_DIR
+#ifdef WITH_AUTOMATIC_UPDATE
+	// WITH_AUTOMATIC_UPDATE builds are our tarball releases and
+	// development snapshots, i.e. where the game data is at the
+	// same location as the executable.
+	SCopy(ExePath.getMData(),SystemDataPath);
+#else
 	SCopy(OC_SYSTEM_DATA_DIR, SystemDataPath);
+#endif
 #else
 #error Please define OC_SYSTEM_DATA_DIR!
-	//SCopy(ExePath,SystemDataPath);
 #endif
 #endif
 	AppendBackslash(SystemDataPath);
