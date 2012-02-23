d458eca6c445a4fb8881af547900a3361a696595
David Dormagen <czapper@gmx.de>
2012-02-23 21:42:15 +0100

    ownable objects now have the right owner after construction

--- planet/Objects.ocd/Libraries.ocd/Ownable.ocd/Script.c
+++ planet/Objects.ocd/Libraries.ocd/Ownable.ocd/Script.c
@@ -13,3 +13,9 @@ public func IsInteractable(object clonk)
 	if(Hostile(GetOwner(), clonk->GetOwner())) return false;
 	return _inherited(clonk, ...);
 }
+
+func Initialize()
+{
+	// set right owner
+	SetOwner(GetOwnerOfPosition(0, 0));
+}
