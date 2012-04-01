0a6c9df7f36328db69431989842d9047ee082327
Günther Brammer <gbrammer@gmx.de>
2012-04-01 22:30:55 +0200

    gtk: Move the FindInfo functions to C4WindowGtk.cpp

--- src/platform/C4WindowGTK.cpp
+++ src/platform/C4WindowGTK.cpp
@@ -29,13 +29,173 @@
 #include <C4Console.h>
 #include <C4ViewportWindow.h>
 #include "C4MouseControl.h"
-#include <X11/Xlib.h>
+
 #include <gdk/gdk.h>
-#include <gdk/gdkx.h>
-#include <GL/glx.h>
 #include <gdk/gdkkeysyms.h>
 #include <gtk/gtk.h>
 
+#ifdef USE_X11
+#include <gdk/gdkx.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/xf86vmode.h>
+#include <GL/glx.h>
+#endif
+
+// Some helper functions for choosing a proper visual
+
+#ifdef USE_GL
+// Returns which XVisual attribute for two given attributes is greater.
+static int CompareVisualAttribute(Display* dpy, XVisualInfo* first, XVisualInfo* second, int attrib)
+{
+	int first_value, second_value;
+	glXGetConfig(dpy, first, attrib, &first_value);
+	glXGetConfig(dpy, second, attrib, &second_value);
+	if(first_value != second_value) return first_value > second_value ? 1 : -1;
+	return 0;
+}
+
+// Given two X visuals, check which one is superior, according to
+// the following rule: Double buffering is preferred over single
+// buffering, then highest color buffer is preferred. If both are equal
+// then the buffers are considered equal (return value 0).
+static int CompareVisual(Display* dpy, XVisualInfo* first, XVisualInfo* second)
+{
+	int result = CompareVisualAttribute(dpy, first, second, GLX_DOUBLEBUFFER);
+	if(result != 0) return result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_BUFFER_SIZE);
+	return result;
+}
+
+// Compare otherwise equivalent visuals. If the function above
+// considered two visuals to be equivalent then this function can
+// be used to decide which one to use. We prefer visuals with high depth
+// beffer size and low accumulation and stencil buffer sizes since the latter
+// two are not used in Clonk.
+static int CompareEquivalentVisual(Display* dpy, XVisualInfo* first, XVisualInfo* second)
+{
+	int result = CompareVisualAttribute(dpy, first, second, GLX_DEPTH_SIZE);
+	if(result != 0) return result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_STENCIL_SIZE);
+	if(result != 0) return -result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_ACCUM_RED_SIZE);
+	if(result != 0) return -result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_ACCUM_GREEN_SIZE);
+	if(result != 0) return -result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_ACCUM_BLUE_SIZE);
+	if(result != 0) return -result;
+
+	result = CompareVisualAttribute(dpy, first, second, GLX_ACCUM_ALPHA_SIZE);
+	return -result;
+}
+
+// This function generates a list of acceptable visuals. The most
+// superiour visual as defined by CompareVisual is chosen. If there
+// are two or more visuals which compare equal with CompareVisual then
+// we add all of them to the output list as long as their multi
+// sampling properties differ. If they do not differ then we use
+// CompareEquivalentVisual to decide which one to put into the output
+// list.
+static std::vector<XVisualInfo> EnumerateVisuals(Display* dpy)
+{
+	XVisualInfo templateInfo;
+	templateInfo.screen = DefaultScreen(dpy);
+	long vinfo_mask = VisualScreenMask;
+	int nitems;
+	XVisualInfo* infos = XGetVisualInfo(dpy, vinfo_mask, &templateInfo, &nitems);
+
+	std::vector<XVisualInfo> selected_infos;
+	for(int i = 0; i < nitems; ++i)
+	{
+		// Require minimum depth and color buffer
+		if(infos[i].depth < 8 || infos[i].bits_per_rgb < 4) continue;
+
+		// Require it to be an RGBA visual
+		int value;
+		glXGetConfig(dpy, &infos[i], GLX_RGBA, &value);
+		if(!value) continue;
+
+		// Require GL rendering to be supported (probably always true...)
+		glXGetConfig(dpy, &infos[i], GLX_USE_GL, &value);
+		if(!value) continue;
+
+		// Multisampling with only 1 sample gives the same result as
+		// no multisampling at all, so simply ignore these visuals.
+		int second_value;
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLE_BUFFERS_ARB, &value);
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLES_ARB, &second_value);
+		if(value == 1 && second_value == 1) continue;
+
+		// This visual is acceptable in principle. Use it if
+		// we don't have any other.
+		if(selected_infos.empty())
+		{
+			selected_infos.push_back(infos[i]);
+		}
+		// Otherwise, check which one is superior. Note that all selected
+		// visuals have same buffering and RGBA sizes.
+		else
+		{
+			unsigned int j;
+			switch(CompareVisual(dpy, &infos[i], &selected_infos[0]))
+			{
+			case 1:
+				// The new visual is superior.
+				selected_infos.clear();
+				selected_infos.push_back(infos[i]);
+				break;
+			case -1:
+				// The old visual is superior.
+				break;
+			case 0:
+				// The visuals are equal. OK, so check whether there is an otherwise equivalent
+				// visual (read: same multisampling properties) but with different depth, stencil or
+				// auxiliary buffer sizes. If so, replace it, otherwise add the new one.
+				for(j = 0; j < selected_infos.size(); ++j)
+				{
+					if(CompareVisualAttribute(dpy, &infos[i], &selected_infos[j], GLX_SAMPLE_BUFFERS_ARB) != 0) continue;
+					if(CompareVisualAttribute(dpy, &infos[i], &selected_infos[j], GLX_SAMPLES_ARB) != 0) continue;
+
+					// The new visual has the same multi sampling properties then the current one.
+					// Use CompareEquivalentVisual() to decide
+					switch(CompareEquivalentVisual(dpy, &infos[i], &selected_infos[j]))
+					{
+					case 1:
+						// The current info is more suitable
+						selected_infos[j] = infos[i];
+						break;
+					case -1:
+						// The existing info is more suitable;
+						break;
+					case 0:
+						// No decision. Keep the existing one, but we could as well take
+						// the new one since we don't know what the difference between the two is.
+						break;
+					}
+
+					// Break the for loop. There is only one visual
+					// with the same multi sampling properties.
+					break;
+				}
+
+				// If we did not find a visual with the same multisampling in the for loop
+				// then add this visual to the result list
+				if(j == selected_infos.size())
+					selected_infos.push_back(infos[i]);
+
+				break;
+			}
+		}
+	}
+
+	XFree(infos);
+	return selected_infos;
+}
+#endif // USE_GL
 static void OnDestroyStatic(GtkWidget* widget, gpointer data)
 {
 	C4Window* wnd = static_cast<C4Window*>(data);
@@ -480,6 +640,47 @@ C4Window::C4Window ():
 C4Window::~C4Window ()
 {
 	Clear();
+}
+
+bool C4Window::FindInfo(int samples, void** info)
+{
+#ifdef USE_GL
+	Display * const dpy = gdk_x11_display_get_xdisplay(gdk_display_get_default());
+	std::vector<XVisualInfo> infos = EnumerateVisuals(dpy);
+	for(unsigned int i = 0; i < infos.size(); ++i)
+	{
+		int v_buffers, v_samples;
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLE_BUFFERS_ARB, &v_buffers);
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLES_ARB, &v_samples);
+
+		if((samples == 0 && v_buffers == 0) ||
+		   (samples > 0 && v_buffers == 1 && v_samples == samples))
+		{
+			*info = new XVisualInfo(infos[i]);
+			return true;
+		}
+	}
+#else
+	// TODO: Do we need to handle this case?
+#endif // USE_GL
+
+	return false;
+}
+
+void C4Window::EnumerateMultiSamples(std::vector<int>& samples) const
+{
+#ifdef USE_GL
+	Display * const dpy = gdk_x11_display_get_xdisplay(gdk_display_get_default());
+	std::vector<XVisualInfo> infos = EnumerateVisuals(dpy);
+	for(unsigned int i = 0; i < infos.size(); ++i)
+	{
+		int v_buffers, v_samples;
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLE_BUFFERS_ARB, &v_buffers);
+		glXGetConfig(dpy, &infos[i], GLX_SAMPLES_ARB, &v_samples);
+
+		if(v_buffers == 1) samples.push_back(v_samples);
+	}
+#endif
 }
 
 C4Window* C4Window::Init(WindowKind windowKind, C4AbstractApp * pApp, const char * Title, C4Window * pParent, bool HideCursor)
