acff7e76beb9ce4f82799a237d3109f780b4bb73
Armin Burgmeier <armin@arbur.net>
2012-04-28 17:24:34 +0200

    Rope.ocs: Add a rope between the player's clonk and a fixpoint

--- planet/Tests.ocf/Rope.ocs/Script.c
+++ planet/Tests.ocf/Rope.ocs/Script.c
@@ -1,6 +1,19 @@
+
+
 func Initialize()
 {
 	var  a = CreateObject(Rock, 100, 100, NO_OWNER);
 	var  b = CreateObject(Rock, 200, 100, NO_OWNER);
+	a->SetCategory(C4D_StaticBack);
+	b->SetCategory(C4D_StaticBack);
 	a->CreateRope2(a, b, 20);
+	//a->CreateRope2(a, b, 1);
+}
+
+func InitializePlayer(int plr)
+{
+	var clonk = GetCrew(plr, 0);
+	var  a = CreateObject(Rock, clonk->GetX(), clonk->GetY()-100, NO_OWNER);
+	a->SetCategory(C4D_StaticBack);
+	a->CreateRope2(a, clonk, 20);
 }
