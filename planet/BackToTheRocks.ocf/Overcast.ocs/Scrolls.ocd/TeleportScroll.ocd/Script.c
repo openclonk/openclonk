60c3b5bf44320560373fa3da70cebe4da34a7a6d
Maikel de Vries <maikeldevries@hotmail.com>
2012-05-13 02:21:42 +0200

    Fixed teleportation scroll for attach procedure(#779)

--- planet/BackToTheRocks.ocf/Overcast.ocs/Scrolls.ocd/TeleportScroll.ocd/Script.c
+++ planet/BackToTheRocks.ocf/Overcast.ocs/Scrolls.ocd/TeleportScroll.ocd/Script.c
@@ -14,6 +14,10 @@ public func ControlUse(object clonk, int x, int y)
 	
 	DrawParticleLine("Magic",0,0,-GetX()+x,-GetY()+y,3,64,RGB(0,128,255),RGB(0,200,255),-1);
 	DrawParticleLine("MagicFire",0,0,-GetX()+x,-GetY()+y,4,64,RGB(0,255,128),RGB(0,255,20),-1);
+	// Make sure the clonk loses the attach procedure.
+	var action = clonk->GetAction();
+	if (action && clonk.ActMap[action].Procedure == DFA_ATTACH)
+		clonk->SetAction("Jump");
 	clonk->SetPosition(x, y);
 	clonk->SetXDir(0);
 	clonk->SetYDir(-5);
