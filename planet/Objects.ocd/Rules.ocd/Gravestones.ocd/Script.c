6f74d0430f9e472b064755cdd0e3dd12e7f4ed68
Charles Spurrill <carlonfire@gmail.com>
2012-08-02 12:00:11 -0700

    #796: Added missing gravestone rule

---
+++ planet/Objects.ocd/Rules.ocd/Gravestones.ocd/Script.c
@@ -0,0 +1,10 @@
+/*-- Gravestones --*/
+
+protected func Activate(int iByPlayer)
+{
+	MessageWindow(GetProperty("Description"), iByPlayer);
+	return true;
+}
+
+local Name = "$Name$";
+local Description = "$Description$";
