095645a7bb08ba2c9b8e3e249b0b27a285d81bad
Tobias Zwick <newton@westnordost.de>
2012-10-30 00:48:50 +0100

    dont draw stars and moon at all during the day, only fade all celestial objects and cloud on dawn and dusk (performance optimization)

--- planet/Objects.ocd/Environment.ocd/Time.ocd/Stars.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Time.ocd/Stars.ocd/Script.c
@@ -8,7 +8,7 @@ protected func Initialize()
 	if(g > 1) SetGraphics(Format("%d",g));
 	SetClrModulation(RGBa(255,255,255,alpha));
 	SetObjectBlitMode(GFX_BLIT_Additive);
-	var parallax = RandomX(5,8);
+	var parallax = RandomX(8,12);
 	this["Parallaxity"] = [parallax,parallax];
 }
 
