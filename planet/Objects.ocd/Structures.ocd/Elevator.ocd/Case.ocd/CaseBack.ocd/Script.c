f509d052e3e158ff6faf483cf999a61cf06ce88b
David Dormagen <czapper@gmx.de>
2012-11-03 16:17:30 +0100

    rewritten elevator case to be less hacky and adjusted its power-consumption-behavior

--- planet/Objects.ocd/Structures.ocd/Elevator.ocd/Case.ocd/CaseBack.ocd/Script.c
+++ planet/Objects.ocd/Structures.ocd/Elevator.ocd/Case.ocd/CaseBack.ocd/Script.c
@@ -4,3 +4,19 @@ func Initialize()
 {
 	//SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(800), Trans_Translate(0, -2000)));
 }
+
+func AttachTargetLost()
+{
+	RemoveObject();
+}
+
+local ActMap = {
+	Attach = {
+		Prototype = Action,
+		Name = "Attach",
+		Procedure = DFA_ATTACH,
+		Directions = 1,
+		FacetBase = 1,
+		NextAction = "Attach"
+	}
+};
--- planet/Objects.ocd/Structures.ocd/Elevator.ocd/Case.ocd/CaseFront.ocd/Script.c
+++ planet/Objects.ocd/Structures.ocd/Elevator.ocd/Case.ocd/CaseFront.ocd/Script.c
@@ -6,3 +6,19 @@ func Initialize()
 {
 	//SetProperty("MeshTransformation", Trans_Scale(800));
 }
+
+func AttachTargetLost()
+{
+	RemoveObject();
+}
+
+local ActMap = {
+	Attach = {
+		Prototype = Action,
+		Name = "Attach",
+		Procedure = DFA_ATTACH,
+		Directions = 1,
+		FacetBase = 1,
+		NextAction = "Attach"
+	}
+};
