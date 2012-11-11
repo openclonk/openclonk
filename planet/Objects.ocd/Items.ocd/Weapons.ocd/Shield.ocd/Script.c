e42161b8a7ef8a1ed9a3a7912aad589933ca0b26
Charles Spurrill <carlonfire@gmail.com>
2012-11-11 11:15:01 -0800

    Fixed shield clipping through clonk's backpack

--- planet/Objects.ocd/Items.ocd/Weapons.ocd/Shield.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Weapons.ocd/Shield.ocd/Script.c
@@ -286,10 +286,11 @@ public func GetCarryTransform(clonk, sec, back)
 	if(mTrans != nil) return mTrans;
 	if(!sec)
 	{
-		if(back) return Trans_Mul(Trans_Rotate(-90, 0, 0, 1),Trans_Translate(0,0,-400));
+		if(back) return Trans_Mul(Trans_Rotate(-90, 0, 0, 1),Trans_Translate(0,0,400));
 		return nil;
 	}
-	if(back) return Trans_Mul(Trans_Mul(Trans_Rotate(90, 0, 0, 1),Trans_Rotate(180, 0, 1)),Trans_Translate(0,0,-400));
+	
+	if(back) return Trans_Mul(Trans_Mul(Trans_Rotate(90, 0, 0, 1),Trans_Rotate(180, 0, 1)),Trans_Translate(0,0,400));
 	return Trans_Rotate(180,1,0,0);
 }
 
