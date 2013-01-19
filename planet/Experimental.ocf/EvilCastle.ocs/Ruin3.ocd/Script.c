18c7525420b86e4e6ad69fb746a729ae6205accc
Sven Eberhardt <sven2@goldwipf.de>
2013-01-19 17:49:08 +0100

    Experimental: EvilCastle

---
+++ planet/Experimental.ocf/EvilCastle.ocs/Ruin3.ocd/Script.c
@@ -0,0 +1,6 @@
+/*-- Ruin --*/
+
+local Name = "$Name$";
+local Description ="$Description$";
+local BlastIncinerate = 100;
+local HitPoints = 70;
---
+++ planet/Experimental.ocf/EvilCastle.ocs/Script.c
@@ -0,0 +1,44 @@
+/**
+	Evil Castle
+	Desc
+	
+	@authors Sven2
+*/
+
+static const S2DD_InitialRelaunchs = 0;
+
+static g_is_initialized;
+
+func DoInit(int first_player)
+{
+	// Goal
+	//CreateObject(Goal_Plane);
+	// Elevators
+	// Top
+	Object(332)->SetNoPowerNeed(true);
+	Object(331)->CreateShaft(500);
+	// Left
+	Object(420)->CreateShaft(100);
+	// Rules
+	CreateObject(Rule_TeamAccount);
+	CreateObject(Rule_NoPowerNeed);
+	return true;
+}
+
+func InitializePlayer(int plr)
+{
+	// Players only
+	if (GetPlayerType(plr)!=C4PT_User) return;
+	// Scenario init
+	if (!g_is_initialized) g_is_initialized = DoInit(plr);
+	// Start intro if not yet started
+	//IntroStart();
+	// Place in village
+	var crew;
+	for(var index = 0; crew = GetCrew(plr, index); ++index)
+	{
+		var x = 50 + Random(50);
+		var y = 850;
+		crew->SetPosition(x , y);
+	}
+}
