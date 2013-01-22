50fd0ebc39a3866d751c58bb55451e8dd9b8fcf5
David Dormagen <czapper@gmx.de>
2013-01-22 18:17:01 +0100

    added Rule_EnergyBarsAboveStructures and adjusted Krakatoa to use that

--- planet/Objects.ocd/Libraries.ocd/Structure.ocd/Script.c
+++ planet/Objects.ocd/Libraries.ocd/Structure.ocd/Script.c
@@ -2,9 +2,20 @@
 	Structure Library
 	Basic library for structures, handles:
 	* Damage
+	* Energy bar if rule active
 	
 	@author Maikel
 */
+
+func Initialize()
+{
+	if (ObjectCount(Find_ID(Rule_EnergyBarsAboveStructures)) > 0)
+	{
+		if (this.HitPoints != nil)
+			AddEnergyBar();
+	}
+	return _inherited(...);
+}
 
 public func Damage(int change, int cause, int cause_plr)
 {
