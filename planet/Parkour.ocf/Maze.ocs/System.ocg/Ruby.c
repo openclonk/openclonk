408f9fca791c2bec79244687ddaa905449e1c1bb
Tobias Zwick <newton@openclonk.org>
2014-01-25 00:54:44 +0700

    maze.ocs: make rubies sparkle even when collected

---
+++ planet/Parkour.ocf/Maze.ocs/System.ocg/Ruby.c
@@ -0,0 +1,9 @@
+#appendto Ruby
+
+// also flash when contained so noone can smuggle his ruby to the goal
+func FxSparkleTimer(target, effect, effect_time)
+{
+	if(!Random(2)) return FX_OK;
+	CreateParticle("MagicRing", 0, 0, 0, 0, effect.Interval, effect.particles, 1);
+	return FX_OK;
+}
