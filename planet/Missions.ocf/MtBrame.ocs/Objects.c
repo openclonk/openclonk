4713437503c660f43eada29eb07073a8d57db0f6
Tobias Zwick <newton@openclonk.org>
2014-01-26 18:31:21 +0700

    MtBrame: use Objects.c

---
+++ planet/Missions.ocf/MtBrame.ocs/Objects.c
@@ -0,0 +1,411 @@
+/* Automatically created objects file */
+
+func InitializeObjects()
+{
+	var Grass0199 = CreateObject(Grass, 792, 656);
+	Grass0199.Plane = -500;
+	var Grass0200 = CreateObject(Grass, 647, 614);
+	Grass0200.Plane = -500;
+	var Grass0204 = CreateObject(Grass, 656, 618);
+	Grass0204.Plane = -500;
+	var Grass0250 = CreateObject(Grass, 176, 533);
+	Grass0250.Plane = -500;
+	var Grass0254 = CreateObject(Grass, 86, 502);
+	Grass0254.Plane = -500;
+	var Grass0255 = CreateObject(Grass, 164, 529);
+	Grass0255.Plane = -500;
+	var Grass0256 = CreateObject(Grass, 248, 571);
+	Grass0256.Plane = -500;
+	var Grass0257 = CreateObject(Grass, 254, 575);
+	Grass0257.Plane = -500;
+	var Grass0258 = CreateObject(Grass, 207, 548);
+	Grass0258.Plane = -500;
+	var Grass0259 = CreateObject(Grass, 241, 568);
+	Grass0259.Plane = -500;
+	var Grass0260 = CreateObject(Grass, 224, 558);
+	Grass0260.Plane = -500;
+	var Grass0261 = CreateObject(Grass, 169, 533);
+	Grass0261.Plane = -500;
+	var Grass0262 = CreateObject(Grass, 199, 541);
+	Grass0262.Plane = -500;
+	var Grass0266 = CreateObject(Grass, 215, 550);
+	Grass0266.Plane = -500;
+	var Grass0270 = CreateObject(Grass, 222, 554);
+	Grass0270.Plane = -500;
+	var Grass0274 = CreateObject(Grass, 10, 498);
+	Grass0274.Plane = -500;
+	var Grass0278 = CreateObject(Grass, 3, 501);
+	Grass0278.Plane = -500;
+
+	CreateObject(Trunk, 1117, 546);
+	var Trunk0052 = CreateObject(Trunk, 1100, 555);
+	Trunk0052->SetCon(80);
+	var Trunk0053 = CreateObject(Trunk, 1363, 543);
+	Trunk0053->SetCon(65);
+	var Trunk0054 = CreateObject(Trunk, 1236, 561);
+	Trunk0054->SetCon(40);
+	CreateObject(Trunk, 983, 593);
+	CreateObject(Trunk, 1050, 574);
+
+	var Rank0069 = CreateObject(Rank, 1105, 503);
+	Rank0069->SetR(-140);
+	Rank0069->SetPosition(1105, 503);
+	var Rank0071 = CreateObject(Rank, 1415, 280);
+	Rank0071->SetR(176);
+	Rank0071->SetPosition(1415, 280);
+	var Rank0072 = CreateObject(Rank, 1509, 289);
+	Rank0072->SetR(-165);
+	Rank0072->SetPosition(1509, 289);
+	var Rank0073 = CreateObject(Rank, 1557, 314);
+	Rank0073->SetR(-159);
+	Rank0073->SetPosition(1557, 314);
+	var Rank0074 = CreateObject(Rank, 1315, 293);
+	Rank0074->SetR(-173);
+	Rank0074->SetPosition(1315, 293);
+	var Rank0075 = CreateObject(Rank, 1583, 452);
+	Rank0075->SetR(-8);
+	Rank0075->SetPosition(1583, 449);
+	var Rank0076 = CreateObject(Rank, 1491, 479);
+	Rank0076->SetR(-26);
+	Rank0076->SetPosition(1491, 476);
+
+	var Trunk0081 = CreateObject(Trunk, 1388, 315);
+	Trunk0081->SetCon(55);
+	Trunk0081->SetR(180);
+	Trunk0081->SetPosition(1388, 290);
+	var Trunk0082 = CreateObject(Trunk, 1316, 218);
+	Trunk0082->SetCon(66);
+
+	CreateObject(Tree_Coniferous, 531, 574);
+
+	CreateObject(Fern, 596, 595);
+
+	CreateObject(Tree_Coniferous, 633, 607);
+	CreateObject(Tree_Coniferous, 716, 632);
+	CreateObject(Tree_Coniferous, 764, 631);
+	CreateObject(Tree_Coniferous, 581, 590);
+	CreateObject(Tree_Coniferous, 413, 702);
+	CreateObject(Tree_Coniferous, 789, 628);
+	CreateObject(Tree_Coniferous, 934, 615);
+	CreateObject(Tree_Coniferous, 1134, 533);
+	CreateObject(Tree_Coniferous, 122, 507);
+	CreateObject(Tree_Coniferous, 137, 518);
+	CreateObject(Tree_Coniferous, 131, 515);
+	CreateObject(Tree_Coniferous, 152, 525);
+	CreateObject(Tree_Coniferous, 189, 543);
+	CreateObject(Tree_Coniferous, 231, 565);
+	CreateObject(Tree_Coniferous, 1290, 227);
+
+	CreateObject(Fern, 241, 1104);
+	CreateObject(Fern, 493, 1160);
+	CreateObject(Fern, 1008, 736);
+	CreateObject(Fern, 1530, 464);
+	CreateObject(Fern, 820, 1248);
+	CreateObject(Fern, 1125, 1207);
+
+	CreateObject(Tree_Coniferous, 264, 1111);
+	CreateObject(Tree_Coniferous, 691, 626);
+
+	CreateObject(Fern, 1100, 1353);
+	CreateObject(Fern, 1074, 1354);
+
+	var Trunk0392 = CreateObject(Trunk, 211, 1090);
+	Trunk0392->SetCon(80);
+	var Trunk0393 = CreateObject(Trunk, 438, 1155);
+	Trunk0393->SetCon(70);
+	var Trunk0394 = CreateObject(Trunk, 538, 1105);
+	Trunk0394->SetR(-165);
+	Trunk0394->SetPosition(538, 1080);
+
+	var Tree_Coniferous0430 = CreateObject(Tree_Coniferous, 624, 612);
+	Tree_Coniferous0430->SetCon(24);
+
+	var Rank0436 = CreateObject(Rank, 1634, 363);
+	Rank0436->SetR(-158);
+	Rank0436->SetPosition(1634, 363);
+
+	var Trunk0437 = CreateObject(Trunk, 1457, 528);
+	Trunk0437->SetCon(30);
+
+	CreateObject(Fern, 75, 1102);
+
+	CreateObject(Rule_NoPowerNeed, 0, 0);
+
+	var WoodenCabin0019 = CreateObject(WoodenCabin, 56, 505);
+	WoodenCabin0019->SetCategory(C4D_Structure|C4D_Background);
+
+	var Chest0022 = CreateObject(Chest, 55, 1308);
+	CreateObject(Chest, 1381, 534);
+	var Chest0083 = CreateObject(Chest, 620, 879);
+	var Chest0445 = CreateObject(Chest, 625, 748);
+	var Chest0472 = CreateObject(Chest, 1553, 279);
+	CreateObject(Chest, 1647, 1380);
+
+	var Elevator0039 = CreateObject(Elevator, 1170, 544);
+	Elevator0039->SetCon(40);
+	Elevator0039.Plane = 200;
+	Elevator0039->SetClrModulation(0xff58362c);
+
+	var WindGenerator0048 = CreateObject(WindGenerator, 1262, 537);
+	WindGenerator0048->SetCon(48);
+	WindGenerator0048->SetClrModulation(0xff7a6e65);
+
+	var Flagpole0494 = CreateObject(Flagpole, 120, 71);
+	Flagpole0494->SetColor(0xffc85a12);
+
+	CreateObject(Armory, 1326, 523);
+
+	var Clonk0027 = CreateObject(Clonk, 155, 997);
+	Clonk0027->Kill(Clonk0027, true);
+	Clonk0027->SetYDir(16);
+	Clonk0027->SetColor(0xffdd3420);
+	Clonk0027->SetName("Hans-Georg");
+	Clonk0027->SetDir(DIR_Left);
+
+	var Loam0084 = Chest0083->CreateContents(Loam);
+	Loam0084.Plane = 500;
+	var Loam0345 = CreateObject(Loam, 28, 587);
+	Loam0345.Plane = 500;
+	var Loam0477 = Chest0472->CreateContents(Loam);
+	Loam0477.Plane = 500;
+	var Loam0491 = CreateObject(Loam, 154, 183);
+	Loam0491.Plane = 500;
+
+	var Firestone0025 = CreateObject(Firestone, 84, 1018);
+	Firestone0025.Plane = 500;
+	var Firestone0343 = CreateObject(Firestone, 95, 909);
+	Firestone0343.Plane = 500;
+	var Firestone0356 = CreateObject(Firestone, 695, 1002);
+	Firestone0356.Plane = 500;
+	var Firestone0357 = CreateObject(Firestone, 864, 1279);
+	Firestone0357.Plane = 500;
+	var Firestone0358 = CreateObject(Firestone, 901, 710);
+	Firestone0358.Plane = 500;
+	var Firestone0359 = CreateObject(Firestone, 1298, 951);
+	Firestone0359.Plane = 500;
+	var Firestone0360 = CreateObject(Firestone, 1448, 742);
+	Firestone0360.Plane = 500;
+	var Firestone0444 = CreateObject(Firestone, 1644, 318);
+	Firestone0444.Plane = 500;
+	var Firestone0446 = Chest0445->CreateContents(Firestone);
+	Firestone0446.Plane = 500;
+	var Firestone0447 = Chest0445->CreateContents(Firestone);
+	Firestone0447.Plane = 500;
+	var Firestone0464 = CreateObject(Firestone, 193, 1172);
+	Firestone0464.Plane = 500;
+	var Firestone0467 = Chest0022->CreateContents(Firestone);
+	Firestone0467.Plane = 500;
+	var Firestone0468 = CreateObject(Firestone, 573, 616);
+	Firestone0468.Plane = 500;
+	var Firestone0469 = CreateObject(Firestone, 1154, 674);
+	Firestone0469.Plane = 500;
+	var Firestone0471 = CreateObject(Firestone, 534, 698);
+	Firestone0471.Plane = 500;
+	var Firestone0581 = CreateObject(Firestone, 1520, 953);
+	Firestone0581.Plane = 500;
+
+	var Axe0026 = CreateObject(Axe, 1537, 1292);
+	Axe0026->SetR(99);
+	Axe0026->SetPosition(1537, 1290);
+
+	var Shovel0037 = CreateObject(Shovel, 138, 982);
+	Shovel0037->SetR(123);
+	Shovel0037->SetPosition(138, 978);
+
+	Chest0445->CreateContents(Ropeladder);
+
+	CreateObject(Mushroom, 1503, 472);
+	CreateObject(Mushroom, 979, 590);
+	CreateObject(Mushroom, 1225, 544);
+	CreateObject(Mushroom, 1082, 551);
+	CreateObject(Mushroom, 1438, 529);
+	CreateObject(Mushroom, 601, 880);
+	CreateObject(Mushroom, 199, 1071);
+	CreateObject(Mushroom, 180, 1047);
+	CreateObject(Mushroom, 116, 941);
+	CreateObject(Mushroom, 1113, 726);
+	CreateObject(Mushroom, 1076, 726);
+	CreateObject(Mushroom, 1532, 825);
+	CreateObject(Mushroom, 1552, 1309);
+	CreateObject(Mushroom, 1577, 1317);
+	CreateObject(Mushroom, 674, 897);
+	CreateObject(Mushroom, 662, 1161);
+	CreateObject(Mushroom, 799, 1240);
+	CreateObject(Mushroom, 1616, 463);
+	CreateObject(Mushroom, 1659, 507);
+	Chest0445->CreateContents(Mushroom);
+	CreateObject(Mushroom, 659, 757);
+	CreateObject(Mushroom, 1615, 1389);
+
+	Chest0083->CreateContents(Dynamite);
+	Chest0083->CreateContents(Dynamite);
+	Chest0472->CreateContents(Dynamite);
+	Chest0472->CreateContents(Dynamite);
+
+	var Nugget0136 = CreateObject(Nugget, 679, 894);
+	Nugget0136.Plane = 500;
+	var Nugget0137 = CreateObject(Nugget, 660, 887);
+	Nugget0137.Plane = 500;
+	var Nugget0138 = CreateObject(Nugget, 637, 878);
+	Nugget0138.Plane = 500;
+	var Nugget0139 = CreateObject(Nugget, 683, 845);
+	Nugget0139.Plane = 500;
+	var Nugget0344 = CreateObject(Nugget, 91, 584);
+	Nugget0344.Plane = 500;
+	var Nugget0353 = CreateObject(Nugget, 718, 856);
+	Nugget0353.Plane = 500;
+	var Nugget0354 = CreateObject(Nugget, 802, 1101);
+	Nugget0354.Plane = 500;
+	var Nugget0355 = CreateObject(Nugget, 677, 1225);
+	Nugget0355.Plane = 500;
+	var Nugget0485 = Chest0083->CreateContents(Nugget);
+	Nugget0485.Plane = 500;
+	var Nugget0486 = Chest0083->CreateContents(Nugget);
+	Nugget0486.Plane = 500;
+	var Nugget0487 = CreateObject(Nugget, 1100, 1229);
+	Nugget0487.Plane = 500;
+	var Nugget0488 = CreateObject(Nugget, 1114, 974);
+	Nugget0488.Plane = 500;
+	var Nugget0489 = CreateObject(Nugget, 1484, 586);
+	Nugget0489.Plane = 500;
+	var Nugget0490 = CreateObject(Nugget, 358, 407);
+	Nugget0490.Plane = 500;
+
+	var Metal0465 = Chest0022->CreateContents(Metal);
+	Metal0465.Plane = 500;
+
+	var Rock0285 = CreateObject(Rock, 109, 659);
+	Rock0285.Plane = 500;
+	var Rock0286 = CreateObject(Rock, 86, 860);
+	Rock0286.Plane = 500;
+	var Rock0287 = CreateObject(Rock, 31, 883);
+	Rock0287.Plane = 500;
+	var Rock0288 = CreateObject(Rock, 172, 601);
+	Rock0288.Plane = 500;
+	var Rock0289 = CreateObject(Rock, 128, 1045);
+	Rock0289.Plane = 500;
+	var Rock0290 = CreateObject(Rock, 37, 1031);
+	Rock0290.Plane = 500;
+	var Rock0291 = CreateObject(Rock, 240, 1204);
+	Rock0291.Plane = 500;
+	var Rock0292 = CreateObject(Rock, 453, 1176);
+	Rock0292.Plane = 500;
+	var Rock0293 = CreateObject(Rock, 514, 1233);
+	Rock0293.Plane = 500;
+	var Rock0294 = CreateObject(Rock, 493, 1292);
+	Rock0294.Plane = 500;
+	var Rock0295 = CreateObject(Rock, 669, 1190);
+	Rock0295.Plane = 500;
+	var Rock0296 = CreateObject(Rock, 794, 1262);
+	Rock0296.Plane = 500;
+	var Rock0303 = CreateObject(Rock, 983, 1371);
+	Rock0303.Plane = 500;
+	var Rock0304 = CreateObject(Rock, 1135, 1283);
+	Rock0304.Plane = 500;
+	var Rock0311 = CreateObject(Rock, 1193, 1261);
+	Rock0311.Plane = 500;
+	var Rock0312 = CreateObject(Rock, 1036, 1128);
+	Rock0312.Plane = 500;
+	var Rock0313 = CreateObject(Rock, 876, 1126);
+	Rock0313.Plane = 500;
+	var Rock0314 = CreateObject(Rock, 940, 976);
+	Rock0314.Plane = 500;
+	var Rock0315 = CreateObject(Rock, 668, 926);
+	Rock0315.Plane = 500;
+	var Rock0316 = CreateObject(Rock, 1152, 1064);
+	Rock0316.Plane = 500;
+	var Rock0320 = CreateObject(Rock, 1044, 809);
+	Rock0320.Plane = 500;
+	var Rock0321 = CreateObject(Rock, 1199, 740);
+	Rock0321.Plane = 500;
+	var Rock0325 = CreateObject(Rock, 1305, 605);
+	Rock0325.Plane = 500;
+	var Rock0326 = CreateObject(Rock, 1315, 837);
+	Rock0326.Plane = 500;
+	var Rock0327 = CreateObject(Rock, 1448, 637);
+	Rock0327.Plane = 500;
+	var Rock0328 = CreateObject(Rock, 1579, 854);
+	Rock0328.Plane = 500;
+	var Rock0329 = CreateObject(Rock, 1455, 244);
+	Rock0329.Plane = 500;
+	var Rock0434 = CreateObject(Rock, 667, 711);
+	Rock0434.Plane = 500;
+	var Rock0435 = CreateObject(Rock, 475, 634);
+	Rock0435.Plane = 500;
+	var Rock0448 = Chest0445->CreateContents(Rock);
+	Rock0448.Plane = 500;
+	var Rock0470 = CreateObject(Rock, 948, 721);
+	Rock0470.Plane = 500;
+	var Rock0484 = Chest0022->CreateContents(Rock);
+	Rock0484.Plane = 500;
+	var Rock0492 = CreateObject(Rock, 380, 332);
+	Rock0492.Plane = 500;
+	var Rock0493 = CreateObject(Rock, 302, 300);
+	Rock0493.Plane = 500;
+	var Rock0580 = CreateObject(Rock, 1519, 1011);
+	Rock0580.Plane = 500;
+
+	var Coal0331 = CreateObject(Coal, 160, 666);
+	Coal0331.Plane = 500;
+	var Coal0332 = CreateObject(Coal, 133, 1112);
+	Coal0332.Plane = 500;
+	var Coal0336 = CreateObject(Coal, 741, 708);
+	Coal0336.Plane = 500;
+	var Coal0337 = CreateObject(Coal, 1081, 642);
+	Coal0337.Plane = 500;
+	var Coal0338 = CreateObject(Coal, 1482, 1211);
+	Coal0338->SetCon(98);
+	Coal0338.Plane = 500;
+	var Coal0483 = CreateObject(Coal, 609, 1179);
+	Coal0483.Plane = 500;
+
+	var Sulphur0346 = CreateObject(Sulphur, 663, 947);
+	Sulphur0346.Plane = 500;
+	var Sulphur0347 = CreateObject(Sulphur, 897, 958);
+	Sulphur0347.Plane = 500;
+	var Sulphur0348 = CreateObject(Sulphur, 973, 784);
+	Sulphur0348.Plane = 500;
+	var Sulphur0349 = CreateObject(Sulphur, 993, 673);
+	Sulphur0349.Plane = 500;
+	var Sulphur0350 = CreateObject(Sulphur, 1324, 692);
+	Sulphur0350.Plane = 500;
+	var Sulphur0351 = CreateObject(Sulphur, 1554, 749);
+	Sulphur0351.Plane = 500;
+	var Sulphur0352 = CreateObject(Sulphur, 1393, 640);
+	Sulphur0352.Plane = 500;
+
+	var Seaweed0455 = CreateObject(Seaweed, 189, 1167);
+	Seaweed0455->SetYDir(16);
+	Seaweed0455->SetPhase(11);
+	var Seaweed0456 = CreateObject(Seaweed, 142, 1169);
+	Seaweed0456->SetYDir(16);
+	Seaweed0456->SetPhase(11);
+	var Seaweed0457 = CreateObject(Seaweed, 522, 658);
+	Seaweed0457->SetYDir(16);
+	Seaweed0457->SetPhase(11);
+	var Seaweed0458 = CreateObject(Seaweed, 610, 705);
+	Seaweed0458->SetYDir(16);
+	Seaweed0458->SetPhase(11);
+	var Seaweed0459 = CreateObject(Seaweed, 697, 682);
+	Seaweed0459->SetYDir(16);
+	Seaweed0459->SetPhase(11);
+	var Seaweed0460 = CreateObject(Seaweed, 766, 697);
+	Seaweed0460->SetYDir(16);
+	Seaweed0460->SetPhase(11);
+
+	CreateObject(GrappleBow, 1601, 326);
+
+	var Wood0481 = CreateObject(Wood, 643, 656);
+	Wood0481.Plane = 500;
+	var Wood0482 = CreateObject(Wood, 672, 759);
+	Wood0482->SetR(22);
+	Wood0482->SetRDir(-3);
+	Wood0482.Plane = 500;
+	Wood0482->SetPosition(672, 758);
+
+	Chest0472->CreateContents(Bow);
+
+	var Arrow0575 = Chest0472->CreateContents(Arrow);
+	Arrow0575->SetR(90);
+	return true;
+}
