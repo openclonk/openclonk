647003cf3c4861471761b324e22c3f6f36e6c6bd
Maikel de Vries <mdt.maikel@gmail.com>
2014-02-17 20:39:47 +0100

    Added a nice GUI menu for displaying multiple goals

--- planet/Objects.ocd/HUD.ocd/Elements.ocd/Goal.ocd/Script.c
+++ planet/Objects.ocd/HUD.ocd/Elements.ocd/Goal.ocd/Script.c
@@ -6,6 +6,8 @@
 */
 
 local goal;
+local id_menu;
+local prop_menu;
 
 protected func Initialize()
 {
@@ -38,13 +40,128 @@ public func Update()
 	return;
 }
 
+/* Goal display using GUI menus */
+
 public func MouseSelection(int plr)
 {
 	if (plr != GetOwner())
 		return;
 	if (!goal)
 		return;
-	// Let the goal decide on how to inform the player.
-	goal->Activate(plr);
+	// Open the goal menu if not already open.
+	if (!prop_menu)
+		OpenGoalWindow();
 	return;
+}
+
+private func OpenGoalWindow()
+{
+	var goals = FindObjects(Find_Category(C4D_Goal));
+	var nr_goals = GetLength(goals);
+	var menu_width = BoundBy(nr_goals * 4, 20, 40); // in em
+	
+	// Safety: there has to be at least one goal.
+	if (nr_goals <= 0)
+		return;
+	
+	// Main menu
+	prop_menu =
+	{
+		Target = this,
+		Style = GUI_Multiple,
+		Decoration = GUI_MenuDeco,
+		Left = Format("50%-%dem", menu_width),
+		Right = Format("50%+%dem", menu_width),
+		Top = "50%-8em",
+		Bottom = "50%+16em",
+		BackgroundColor = {Std = 0},
+	};
+	
+	// Close button
+	Gui_AddCloseButton(prop_menu, this, "OnCloseButtonClick");
+	
+	// Text submenu
+	var prop_text =
+	{
+		Target = this,
+		ID = 1,
+		Left = "0%",
+		Right = "100%",
+		Top = "0%+8em",
+		Bottom = "100%",
+		Text = "bla",
+		BackgroundColor = {Std = 0},	
+	};
+	prop_menu.TextMenu = prop_text;
+	
+	// Goal icons: maximum number of 10 goals for now
+	var prop_goals = [];
+	for (var i = 0; i < Min(10, nr_goals); i++)
+	{
+		var prop_goal = Format("GoalMenu%d", i + 2);
+		prop_goals[i] = GoalSubMenu(goals[i], i);	
+		prop_menu[prop_goal] = prop_goals[i];
+	}
+	
+	// Select first goal and its description.
+	OnGoalGUIHover(goals[0]);
+
+	id_menu = CustomGuiOpen(prop_menu);
+}
+
+private func CloseGoalWindow()
+{
+	CustomGuiClose(id_menu, nil, this);
+	id_menu = nil;
+	prop_menu = nil;
+}
+
+private func GoalSubMenu(object g, int nr, int size)
+{
+	if (size == nil)
+		size = 8;
+	
+	// Create the goal submenu with id counting upwards from 2.
+	var prop_goal = 
+	{
+		Target = this,
+		ID = nr + 2,
+		Left = Format("0%+%dem", nr * size),
+		Right = Format("0%+%dem", (nr + 1) * size),
+		Top = "0%",
+		Bottom = Format("0%+%dem", size),
+		Symbol = g->GetID(),
+		BackgroundColor = {Std = 0, Hover = 0x50ffffff},
+		OnMouseIn = [GuiAction_SetTag("Hover"), GuiAction_Call(this, "OnGoalGUIHover", g)],
+		OnMouseOut = GuiAction_SetTag("Std"),
+	};
+	// Indicate whether the goal is already fulfilled with a star.
+	var prop_star = 
+	{
+		Target = this,
+		Left = "100%-2em",
+		Right = "100%",
+		Top = "0%",
+		Bottom = "0%+2em",
+		Symbol = Icon_Ok,
+		BackgroundColor = {Std = 0},	
+	};
+	if (g->~IsFulfilled())
+		prop_goal.Star = prop_star;
+	
+	return prop_goal;
+}
+
+public func OnCloseButtonClick()
+{
+	CloseGoalWindow();
+}
+
+public func OnGoalGUIHover(object g)
+{
+	// change text to the current goal.
+	var prop_text = prop_menu.TextMenu;
+	prop_text.Text = g->~GetDescription(GetOwner());
+	var id_text = prop_text.ID;
+	CustomGuiUpdate(prop_text, id_menu, id_text, this);
 }
