c167e990a51f2766567afa773abe2a289ea4f137
Günther Brammer <gbrammer@gmx.de>
2014-04-20 22:06:47 +0200

    Add GetName function to Actions

--- planet/System.ocg/Action.c
+++ planet/System.ocg/Action.c
@@ -22,6 +22,7 @@ static const DFA_ATTACH  =  "ATTACH";
 static const DFA_CONNECT =  "CONNECT";
 static const DFA_PULL    =  "PULL";
 static const Action = {
+	GetName = Global.GetName,
 	Length = 1,
 	Directions = 1,
 	Step = 1,
