49327c40a78e64f15d944f2dbae2a4f955ca95ed
Sven Eberhardt <sven2@goldwipf.de>
2014-06-29 00:08:51 +0200

    Construction site: Added function to disallow site cancellation. Added callback to structure for custom build preview.

--- planet/Objects.ocd/Libraries.ocd/Constructor.ocd/ConstructionSite.ocd/Script.c
+++ planet/Objects.ocd/Libraries.ocd/Constructor.ocd/ConstructionSite.ocd/Script.c
@@ -9,19 +9,22 @@ local definition;
 local direction;
 local stick_to;
 local full_material; // true when all needed material is in the site
+local no_cancel; // if true, site cannot be cancelled
 
 public func IsContainer()		{ return !full_material; }
 // disallow taking stuff out
 public func RefuseTransfer(object toMove) { return true; }
+// disallow site cancellation. Useful e.g. for sites that are pre-placed for a game goal
+public func MakeUncancellable() { no_cancel = true; return true; }
 
 // we have 2 interaction modes
 public func IsInteractable(object obj)	{ return definition != nil && !full_material; }
-public func GetInteractionCount() { return 2; }
+public func GetInteractionCount() { return 1 + !no_cancel; }
 public func GetInteractionMetaInfo(object obj, int num)
 {
 	if(num == 0)
 		return {IconName=nil, IconID=Hammer, Description="$TxtTransfer$"};
-	if(num == 1)
+	if(num == 1 && !no_cancel)
 		return {IconName=nil, IconID=Icon_Cancel, Description="$TxtAbort$"};
 }
 
@@ -44,11 +47,14 @@ public func Set(id def, int dir, object stick)
 	var w, h;
 	w = def->GetDefWidth();
 	h = def->GetDefHeight();
-	// Draw the building with a wired frame and large alpha.
-	SetGraphics(nil, def, 1, GFXOV_MODE_Base);
-	SetClrModulation(RGBa(255,255,255,50), 1);
+	// Draw the building with a wired frame and large alpha unless site graphics is overloaded by definition
+	if (!def->~SetConstructionSiteOverlay(this, dir, stick))
+	{
+		SetGraphics(nil, def, 1, GFXOV_MODE_Base);
+		SetClrModulation(RGBa(255,255,255,50), 1);
+		SetGraphics(nil, def, 2, GFXOV_MODE_Base, nil, GFX_BLIT_Wireframe);
+	}
 	SetObjDrawTransform(xw,0,0,0,1000, -h*500,1);
-	SetGraphics(nil, def, 2, GFXOV_MODE_Base, nil, GFX_BLIT_Wireframe);
 	SetObjDrawTransform(xw,0,0,0,1000, -h*500,2);
 	// Height of construction site needs to exceed 12 pixels for the clonk to be able to add materials.
 	h = Max(12, h);
@@ -107,7 +113,7 @@ public func Interact(object clonk, int num)
 		clonk->CreateContentsMenus();
 	}
 	// Remove Site
-	if(num == 1)
+	if(num == 1 && !no_cancel)
 	{
 		// test
 		for(var obj in FindObjects(Find_Container(this)))
@@ -182,8 +188,9 @@ private func StartConstructing()
 	var lying_around = FindObjects(Find_Or(Find_Category(C4D_Vehicle), Find_Category(C4D_Object), Find_Category(C4D_Living)),Find_InRect(-wdt/2 - 2, -hgt, wdt + 2, hgt + 12), Find_Not(Find_OCF(OCF_InFree)));
 	
 	// create the construction, below surface constructions don't perform any checks.
+	// uncancellable sites (for special game goals) are forced and don't do checks either
 	var site;
-	var checks = !definition->~IsBelowSurfaceConstruction();
+	var checks = !definition->~IsBelowSurfaceConstruction() && !no_cancel;
 	if(!(site = CreateConstruction(definition, 0, 0, GetOwner(), 1, checks, checks)))
 	{
 		// spit out error message. This could happen if the landscape changed in the meantime
