e395e0741d117526d46a885c9ac7b196dababa7d
Martin Plicht <mardmol@gmx.de>
2014-09-28 18:56:34 +0200

    C4Network2IRC: Remove call to boost::cref which seems to be unnecessary since C4GUI::Ico_Error is just an integer

--- src/network/C4Network2IRC.cpp
+++ src/network/C4Network2IRC.cpp
@@ -399,7 +399,7 @@ bool C4Network2IRCClient::Join(const char *szChannel)
 			const char* message = LoadResStr("IDS_ERR_CHANNELNOTALLOWED");
 			PushMessage(MSG_Status, "", "", message);
 			SetError("Joining this channel not allowed");
-			Application.InteractiveThread.ThreadPostAsync(boost::bind(&C4GUI::Screen::ShowMessage, ::pGUI, message, LoadResStr("IDS_DLG_CHAT"), boost::cref(C4GUI::Ico_Error), static_cast<int32_t* const &>(0)));
+			Application.InteractiveThread.ThreadPostAsync(boost::bind(&C4GUI::Screen::ShowMessage, ::pGUI, message, LoadResStr("IDS_DLG_CHAT"), C4GUI::Ico_Error, static_cast<int32_t* const &>(0)));
 			return false;
 		}
 	return Send("JOIN", szChannel);
