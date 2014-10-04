8d5d4715fe487e87eb0b0d5df387154d53a9a5b4
Maikel de Vries <mdt.maikel@gmail.com>
2014-10-04 09:21:58 +0200

    remove physical documentation

--- planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/King_Javelin.c
+++ planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/King_Javelin.c
@@ -19,12 +19,7 @@ public func DoThrow(object clonk, int angle)
 	var javelin=TakeObject();
 	if (king_size)
 		javelin->MakeKingSize();
-	
-	// how fast the javelin is thrown depends very much on
-	// the speed of the clonk
-	//var speed = 1200 * clonk->GetPhysical("Throw") / 12000 + 150 * Abs(clonk->GetXDir());
-	//var xdir = Sin(angle,+speed);
-	//var ydir = Cos(angle,-speed);
+
 	// The clonk can convert some, indicated by div in percentages, of its own kinetic energy to change the momenta of the javelin, a miracle.
 	var div = 60; // 40% is converted to the direction of the throwing angle.
 	var xdir = clonk->GetXDir(1000);
