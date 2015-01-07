804bf41c92bcffceb1b71a989eb6a2001c055cf1
Nicolas Hake <isilkor@openclonk.org>
2015-01-07 20:32:49 +0100

    Fix UPnP auto-detection
    Auto-detecting UPnP might fail when CMake was invoked multiple times.
    This would result in a broken build because the compiler would not find
    <natupnp.h>.

--- cmake/FindUpnp.cmake
+++ cmake/FindUpnp.cmake
@@ -1,6 +1,6 @@
 # OpenClonk, http://www.openclonk.org
 #
-# Copyright (c) 2012-2013, The OpenClonk Team and contributors
+# Copyright (c) 2012-2015, The OpenClonk Team and contributors
 #
 # Distributed under the terms of the ISC license; see accompanying file
 # "COPYING" for details.
@@ -20,20 +20,33 @@
 
 # TODO: Use pkg-config if available
 
-find_path(UPNP_INCLUDE_DIR NAMES upnp.h PATH_SUFFIXES upnp)
-set(UPNP_NAMES ${UPNP_NAMES} upnp)
-set(THREADUTIL_NAMES ${THREADUTIL_NAMES} threadutil)
-set(IXML_NAMES ${IXML_NAMES} ixml)
-find_library(UPNP_LIBRARY NAMES ${UPNP_NAMES})
-find_library(THREADUTIL_LIBRARY NAMES ${THREADUTIL_NAMES})
-find_library(IXML_LIBRARY NAMES ${IXML_NAMES})
+if(WIN32)
+	CHECK_INCLUDE_FILE_CXX(natupnp.h HAVE_NATIVE_NATUPNP)
+	if(NOT HAVE_NATIVE_NATUPNP)
+		SET(UPNP_INCLUDE_DIR "thirdparty/natupnp")
+	else()
+		SET(UPNP_INCLUDE_DIR)
+	endif()
+	SET(UPNP_LIBRARIES)
+	SET(UPNP_FOUND TRUE)
+	SET(UPNP_STYLE "Win32")
+else()
+	find_path(UPNP_INCLUDE_DIR NAMES upnp.h PATH_SUFFIXES upnp)
+	set(UPNP_NAMES ${UPNP_NAMES} upnp)
+	set(THREADUTIL_NAMES ${THREADUTIL_NAMES} threadutil)
+	set(IXML_NAMES ${IXML_NAMES} ixml)
+	find_library(UPNP_LIBRARY NAMES ${UPNP_NAMES})
+	find_library(THREADUTIL_LIBRARY NAMES ${THREADUTIL_NAMES})
+	find_library(IXML_LIBRARY NAMES ${IXML_NAMES})
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(UPNP DEFAULT_MSG UPNP_LIBRARY THREADUTIL_LIBRARY IXML_LIBRARY UPNP_INCLUDE_DIR)
+	include(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(UPNP DEFAULT_MSG UPNP_LIBRARY THREADUTIL_LIBRARY IXML_LIBRARY UPNP_INCLUDE_DIR)
 
-if(UPNP_FOUND)
-	set(UPNP_LIBRARIES ${UPNP_LIBRARY} ${THREADUTIL_LIBRARY} ${IXML_LIBRARY})
-	set(UPNP_INCLUDE_DIR ${UPNP_INCLUDE_DIR})
+	if(UPNP_FOUND)
+		set(UPNP_LIBRARIES ${UPNP_LIBRARY} ${THREADUTIL_LIBRARY} ${IXML_LIBRARY})
+		set(UPNP_INCLUDE_DIR ${UPNP_INCLUDE_DIR})
+		set(UPNP_STYLE "libupnp")
+	endif()
 endif()
 
 mark_as_advanced(UPNP_LIBRARY IXML_LIBRARY UPNP_INCLUDE_DIR)
