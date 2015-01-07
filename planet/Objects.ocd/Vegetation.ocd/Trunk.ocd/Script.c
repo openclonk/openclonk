872a64ff890da8329b68e991877668e65aee48e9
Maikel de Vries <mdt.maikel@gmail.com>
2015-01-07 21:43:59 +0100

    add Place() functionality to Trunk

--- planet/Objects.ocd/Vegetation.ocd/Trunk.ocd/Script.c
+++ planet/Objects.ocd/Vegetation.ocd/Trunk.ocd/Script.c
@@ -1,8 +1,15 @@
-/*-- Trunk --*/
+/** 
+	Trunk 
+	Dead trunk which was once part of a tree.
+	
+	@author Maikel, Randrian
+*/
 
-private func Initialize()
+
+protected func Initialize()
 {
-	SetProperty("MeshTransformation", Trans_Rotate(RandomX(0,359),0,1,0));
+	SetProperty("MeshTransformation", Trans_Rotate(RandomX(0, 359), 0, 1, 0));
+	return;
 }
 
 public func IsPlant() { return true; }
@@ -74,6 +81,92 @@ func BurstIntoAshes()
 	}
 	RemoveObject();
 }
+
+
+/*-- Placement --*/
+
+public func Place(int amount, proplist rectangle, proplist settings)
+{
+	// Only allow definition call.
+	if (this != Trunk) 
+		return;
+	// Default parameters.
+	if (!settings) 
+		settings = { size = [80, 100] };
+	if (!settings.size) 
+		settings.size = [80, 100];
+	var loc_area = nil;
+	if (rectangle) 
+		loc_area = Loc_InRect(rectangle);
+		
+	var trunks = [];	
+	for (var i = 0; i < amount; i++)
+	{
+		var size = RandomX(settings.size[0], settings.size[1]);
+		var loc = FindLocation(Loc_Or(Loc_Sky(), Loc_Tunnel()), Loc_Wall(CNAT_Left | CNAT_Right | CNAT_Top), loc_area);
+		if (!loc)
+			continue;
+		var trunk = CreateObject(Trunk);
+		trunk->SetPosition(loc.x, loc.y);
+		trunk->SetCon(size);
+		if (!Random(3))
+			trunk.Plane = 510; 
+		// Adjust orientation and position with respect to landscape.
+		trunk->AdjustOrientation();
+		trunk->AdjustPosition();
+		// Retry if the center is add a solid location.
+		if (trunk->GBackSolid())
+		{
+			trunk->RemoveObject();
+			i--;
+			continue;		
+		}		
+		PushBack(trunks, trunk);	
+	}
+	return trunks;
+}
+
+// Adjust the orientation of the trunk with respect to material.
+public func AdjustOrientation()
+{
+	// Make sure it is not stuck in solid with its center.
+	var dx = 0;
+	if (GBackSolid(-2, 0) && !GBackSolid(2, 0))
+		dx = 3;
+	if (GBackSolid(2, 0) && !GBackSolid(-2, 0))	
+		dx = -3;
+	var dy = 0;
+	if (GBackSolid(0, -2) && !GBackSolid(0, 2))
+		dy = 3;
+	if (GBackSolid(0, 2) && !GBackSolid(0, -2))	
+		dy = -3;
+	SetPosition(GetX() + dx, GetY() + dy);
+	
+	// Check for increasing radius.
+	for (var d = 0; d < 30 * GetCon() / 100; d++)
+		// Check 8 directions.
+		for (var angle = 0; angle < 360; angle += 45)
+			if (GBackSolid(-Sin(angle, d), Cos(angle, d)))
+				return SetR(RandomX(angle - 10, angle + 10));
+	return;
+}
+
+// Adjust position with respect to material.
+public func AdjustPosition()
+{
+	var angle = GetR();
+	// Find distance to material.
+	var d = 0;
+	while (!GBackSolid(-Sin(angle, d), Cos(angle, d)) && d < 24 * GetCon() / 100)
+		d++;
+	// Adjust position.
+	var size = 15 * GetCon() / 100;
+	SetPosition(GetX() - Sin(angle, d - size), GetY() + Cos(angle, d - size));
+	return;
+}
+
+
+/*-- Properties --*/
 
 local Name = "$Name$";
 local BlastIncinerate = 1;
