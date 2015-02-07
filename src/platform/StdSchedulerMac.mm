1a1cecadd46489ec25dd18bdedb693e0cc0021b7
Nicolas Hake <isilkor@openclonk.org>
2015-02-07 15:55:05 +0100

    Update license texts
    Some files, most notably ObjC and test cases, hadn't been updated to use
    the new license header. Fix this.

--- src/platform/StdSchedulerMac.mm
+++ src/platform/StdSchedulerMac.mm
@@ -1,3 +1,18 @@
+/*
+ * OpenClonk, http://www.openclonk.org
+ *
+ * Copyright (c) 2009-2015, The OpenClonk Team and contributors
+ *
+ * Distributed under the terms of the ISC license; see accompanying file
+ * "COPYING" for details.
+ *
+ * "Clonk" is a registered trademark of Matthes Bender, used with permission.
+ * See accompanying file "TRADEMARK" for details.
+ *
+ * To redistribute this file separately, substitute the full license texts
+ * for the above references.
+ */
+
 #include <C4Include.h>
 #include <StdScheduler.h>
 #ifdef USE_COCOA
