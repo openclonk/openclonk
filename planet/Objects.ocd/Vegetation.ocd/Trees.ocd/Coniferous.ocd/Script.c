97ee9b6fab31e44d3b08c2127e10415b96c3af93
Maikel de Vries <mdt.maikel@gmail.com>
2015-03-16 21:19:56 +0100

    trees shake a little when hit by an axe

--- planet/Objects.ocd/Vegetation.ocd/Trees.ocd/Coniferous.ocd/Script.c
+++ planet/Objects.ocd/Vegetation.ocd/Trees.ocd/Coniferous.ocd/Script.c
@@ -38,9 +38,9 @@ public func ChopDown()
 	_inherited(...);
 }
 
-func Damage()
+protected func Damage(int change, int cause)
 {
-	_inherited();
+	_inherited(change, cause, ...);
 
 	if (GetDamage() > MaxDamage() && OnFire())
 	{
@@ -58,6 +58,27 @@ func Damage()
 		RemoveObject();
 		return;
 	}
+	
+	if (cause == FX_Call_DmgChop && IsStanding())
+		ShakeTree();
+	return;
+}
+
+private func ShakeTree()
+{
+	var effect = AddEffect("IntShakeTree", this, 100, 1, this);
+	effect.current_trans = this.MeshTransformation;
+	return;
+}
+
+protected func FxIntShakeTreeTimer(object target, proplist effect, int time)
+{
+	if (time > 24)
+		return FX_Execute_Kill;
+	var angle = Sin(time * 45, 2);
+	var r = Trans_Rotate(angle, 0, 0, 1);
+	target.MeshTransformation = Trans_Mul(r, effect.current_trans);
+	return FX_OK;
 }
 
 /*-- Properties --*/
