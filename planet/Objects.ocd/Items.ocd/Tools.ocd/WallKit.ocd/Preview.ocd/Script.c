147624f9f5716795e5289eb76021187266d1ef82
Maikel de Vries <mdt.maikel@gmail.com>
2015-04-02 18:42:42 +0200

    increase wallkit thickness to make clonks not go stuck

--- planet/Objects.ocd/Items.ocd/Tools.ocd/WallKit.ocd/Preview.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Tools.ocd/WallKit.ocd/Preview.ocd/Script.c
@@ -1,17 +1,17 @@
 /*-- WallKit preview --*/
 
 // definition call: create line
-func Create(int x1,int y1,int x2,int y2,int clr)
+func Create(int x1, int y1, int x2, int y2, int clr)
 {
 	var obj = CreateObjectAbove(WallKit_Preview);
-	obj->Set(x1,y1,x2,y2,clr);
+	obj->Set(x1, y1, x2, y2, clr);
 	return obj;
 }
 
 func Set(int x1,int y1,int x2,int y2,int clr)
 {
-	SetVertexXY(0, x1,y1);
-	SetVertexXY(1, x2,y2);
+	SetVertexXY(0, x1, y1);
+	SetVertexXY(1, x2, y2);
 	this.LineColors = [clr, clr];
 	return;
 }
