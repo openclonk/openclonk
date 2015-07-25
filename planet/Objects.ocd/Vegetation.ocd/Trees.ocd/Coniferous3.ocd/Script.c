5d3fc9683f607c1adf29139b0280f3730e6c4195
Clonkonaut <Armag3ddon@web.de>
2015-07-25 12:59:22 +0200

    Another tree by Nachtfalter: Tree_Coniferous3

---
+++ planet/Objects.ocd/Vegetation.ocd/Trees.ocd/Coniferous3.ocd/Script.c
@@ -0,0 +1,16 @@
+/*-- Yet Another Coniferous Tree --*/
+
+#include Library_Plant
+#include Library_Tree
+
+private func SeedChance() { return 500; }
+private func SeedArea() { return 400; }
+private func SeedAmount() { return 10; }
+
+local lib_tree_burned = Tree_Coniferous3_Burned;
+
+local Name = "$Name$";
+local Touchable = 0;
+local BlastIncinerate = 2;
+local ContactIncinerate = 6;
+local NoBurnDecay = 1;
