a45c5afe60470105b83a1f96bebbe013f2d6637d
Sven Eberhardt <sven2@goldwipf.de>
2015-08-09 19:41:16 -0400

    Fix mape build.

--- src/mape/cpp-handles/stub-handle.cpp
+++ src/mape/cpp-handles/stub-handle.cpp
@@ -54,7 +54,7 @@ class C4Draw *pDraw = NULL;
 
 bool EraseItemSafe(const char *szFilename) {return false;}
 void Smoke(int32_t tx, int32_t ty, int32_t level, DWORD dwClr) {}
-class C4SoundInstance *StartSoundEffectAt(const char *, int32_t, int32_t, int32_t, int32_t) { return NULL; }
+class C4SoundInstance *StartSoundEffectAt(const char *, int32_t, int32_t, int32_t, int32_t, int32_t) { return NULL; }
 
 C4Config::C4Config() {}
 C4Config::~C4Config() {}
