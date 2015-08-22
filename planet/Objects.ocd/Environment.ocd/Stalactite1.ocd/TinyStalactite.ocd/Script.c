07cc668f46421324d4d83da691277958793cae14
Armin <arminnessie@gmail.com>
2015-08-22 15:33:19 +0200

    stalactites: dynamic texture color & fix water sources being out of solid material

--- planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/TinyStalactite.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/TinyStalactite.ocd/Script.c
@@ -2,24 +2,49 @@
 	TinyStalactite
 	Covers up the base of stalactites
 
-	@author 
+	@author Armin, Win
 */
+
+private func Initialize()
+{
+}
+
+private func SetChild(object target)
+{
+	child = target;
+	AddEffect("Parent", this, 1, 10, this);
+}
+
+private func FxParentTimer(object target, effect, int time)
+{
+	if (!child)
+	{
+		child = CreateObject(Stalactite1);
+	}
+	//SetPosition(parent->GetX(), parent->GetY());
+}
+
+private func DrawWaterSource()
+{
+	var xold = GetX();
+	var yold = GetY()-8;
+	var xnew = GetX();
+	var ynew = GetY()-8;
+	for (var i = 0; i < RandomX(30, 140); i++)
+	{
+		ynew--;
+		xnew = xold + RandomX(-1, 1);
+		if (GBackSemiSolid(xnew-GetX(), ynew-GetY()))
+			DrawMaterialQuad("Water", xold,yold, xold+1,yold, xold+1,yold+1, xold,yold+1);	
+		else
+			return;
+		xold = xnew;
+		yold = ynew;
+	}
+}
 
 local Name = "$Name$";
 local Description = "$Description$";
-local parent;
-
-func Initialize()
-{
-}
-
-func SetParent(object target)
-{
-	parent = target;
-	AddEffect("Parent",this,1,1,this);
-}
-
-func FxParentTimer(object target, effect, int time)
-{
-	SetPosition(parent->GetX(), parent->GetY());
-}
+local Plane = 500;
+local child;
+local mat;
