cc876e01ceaf041c964d7b69c330bff22e7eeb60
David Dormagen <czapper@gmx.de>
2015-09-01 22:54:31 +0200

    moved LaunchMeteor into more general Objects.ocd/System.ocg folder
    This is to prevent errors on including objects that contain global functions. I believe this will be needed more in the future, thus there is now a new System.ocg folder.

--- planet/Objects.ocd/Environment.ocd/Disasters.ocd/Meteor.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Disasters.ocd/Meteor.ocd/Script.c
@@ -82,19 +82,6 @@ public func FxIntMeteorControlSaveScen(obj, fx, props)
 	return true;
 }
 
-/**
-	Launches a meteor.
-	The meteor can spawn objects via spawn_id.
-*/
-global func LaunchMeteor(int x, int y, int size, int xdir, int ydir, id spawn_id, int spawn_amount)
-{
-	var meteor_skin = Meteor;
-	if (spawn_id) meteor_skin = spawn_id->~GetMeteorSkin() ?? meteor_skin;
-
-	var meteor = CreateObject(meteor_skin);
-	return meteor->Launch(x, y, size, xdir, ydir, spawn_id, spawn_amount);
-}
-
 /*-- Meteor --*/
 
 public func Launch(int x, int y, int size, int xdir, int ydir, id spawn_id, int spawn_amount)
