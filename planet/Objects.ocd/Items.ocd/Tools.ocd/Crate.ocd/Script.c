6b1bf6c4c7afe15085ea4cd5822647b95bf1143f
Sven Eberhardt <sven2@goldwipf.de>
2015-09-06 21:28:28 -0400

    Fix script error in crate.
    Although the evidence is overwhelming, this was actually not intentional :(

--- planet/Objects.ocd/Items.ocd/Tools.ocd/Crate.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Tools.ocd/Crate.ocd/Script.c
@@ -71,7 +71,7 @@ func Hit()
 }
 
 local Name = "$Name$";
-local Description = "$Description$"
+local Description = "$Description$";
 local Collectible = false;
 local ContainBlast = true;
 local Touchable = 2;
