0751a552189d6f8d673a71da204385a1f7d832a8
Sven Eberhardt <sven2@goldwipf.de>
2015-09-09 21:09:36 -0400

    Fix flower skin (the yellow flower skin was never set) and improve flower scenario saving.
    Randomized flower colors are not saved in the scenario. Explicit skins (via SetSkin) and custom skins (via SetMeshMaterial) are saved.

--- planet/Objects.ocd/Vegetation.ocd/Flower.ocd/Script.c
+++ planet/Objects.ocd/Vegetation.ocd/Flower.ocd/Script.c
@@ -7,25 +7,61 @@
 
 #include Library_Plant
 
+local is_explicit_skin = false;
+
 private func SeedChance() { return 300; }
 private func SeedArea() { return 120; }
 private func SeedAmount() { return 6; }
 
-func Construction()
+public func Construction()
 {
 	StartGrowth(1);
-	var skin = 1 + Random(3);
-	if (skin != 1)
-		SetMeshMaterial(Format("flower%d", skin));
+	SetSkin(Random(4));
+	is_explicit_skin = false;
 	SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(RandomX(850,1200)), Trans_Rotate(RandomX(0,359),0,1,0)));
 	
 	inherited(...);
+}
+
+// Set one of four possible flower skins (skin from 0 to 3)
+// Also makes the skin explicit for saved scenarios
+public func SetSkin(int skin)
+{
+	var skin_name = "flower";
+	if (skin) skin_name = Format("flower%d", skin);
+	SetMeshMaterial(skin_name);
+}
+
+public func SetMeshMaterial(...)
+{
+	is_explicit_skin = true;
+	return inherited(...);
 }
 
 public func Incineration()
 {
 	CreateParticle("Grass", 0, 0, PV_Random(-20, 20), PV_Random(-20, 10), PV_Random(30, 100), Particles_Straw(), 10);
 	RemoveObject();
+}
+
+public func SaveScenarioObject(proplist props, ...)
+{
+	if (!inherited(props, ...)) return false;
+	// Do not set automatic skin, but set it if made explicit (or e.g. an external flower skin was used)
+	if (!is_explicit_skin)
+		props->Remove("MeshMaterial");
+	else
+	{
+		var mat = GetMeshMaterial();
+		var flower_skins = ["flower", "flower1", "flower2", "flower3"];
+		var skin = GetIndexOf(flower_skins, mat);
+		if (skin >= 0)
+		{
+			props->Remove("MeshMaterial");
+			props->AddCall("MeshMaterial", this, "SetSkin", skin);
+		}
+	}
+	return true;
 }
 
 local Name = "$Name$";
