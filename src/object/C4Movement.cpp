4020769e2324f1becd0401777670861e040e31b4
Sven Eberhardt <sven2@goldwipf.de>
2015-09-19 17:22:20 -0400

    Don't delete objects with partial shape still in viewport (#1404).
    This will allow better graphics for the clouds movement when wrapping the landscape.
    
    There is some potential for weird behavior; e.g. objects hidden just out of view outside the landscape. But we don't have any goals like Clonk Rage gold mine where you would have to find that nugget lying hidden in that place. Besides, the current behavior of just removing stuff even though it was just halfway out is worse.

--- src/object/C4Movement.cpp
+++ src/object/C4Movement.cpp
@@ -584,7 +584,7 @@ bool C4Object::ExecMovement() // Every Tick1 by Execute
 	if (!Def->Rotateable) fix_r=Fix0;
 
 	// Out of bounds check
-	if ((!Inside<int32_t>(GetX(),0,GBackWdt) && !(Def->BorderBound & C4D_Border_Sides)) || (GetY()>GBackHgt && !(Def->BorderBound & C4D_Border_Bottom)))
+	if ((!Inside<int32_t>(GetX(),-Shape.Wdt/2,GBackWdt+Shape.Wdt/2) && !(Def->BorderBound & C4D_Border_Sides)) || (GetY()+Shape.Hgt/2>GBackHgt && !(Def->BorderBound & C4D_Border_Bottom)))
 	{
 		C4PropList* pActionDef = GetAction();
 		// Never remove attached objects: If they are truly outside landscape, their target will be removed,
