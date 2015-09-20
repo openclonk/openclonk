dcc5d0fe0742550d85a06e47b32c1478c6cd4a40
David Dormagen <czapper@gmx.de>
2015-09-20 18:32:58 +0200

    bucket: removed ability to place earth chunk
    It was rather random whether you would place a chunk of earth or just spill the bucket. And because the spilling yielded so much more earth, placing a chunk was a waste most of the time. If we want placeable earth (which might be a good thing), we should add it to the hammer.

--- planet/Objects.ocd/Items.ocd/Tools.ocd/Bucket.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Tools.ocd/Bucket.ocd/Script.c
@@ -12,10 +12,6 @@
 // Maximum distance at which material is collected / spilled
 local maxreach = 15;
 
-// This is only temporary during drawing. The contents define the actual material & amount;
-local material_amount;
-local material_name;
-
 public func GetCarryMode() { return CARRY_HandBack; }
 public func GetCarryBone() { return "main"; }
 public func GetCarryTransform()
@@ -30,15 +26,12 @@ public func RejectUse(object clonk)
 
 public func ControlUse(object clonk, int iX, int iY)
 {
-	var angle = Angle(0,0,iX,iY);
-	var distance = GetBucketReachDistance(angle);
-	var x2 = Sin(180-angle,distance);
-	var y2 = Cos(180-angle,distance);
+	var angle = Angle(0, 0, iX, iY);
 
 	// spill bucket
 	if (IsBucketFilled())
 	{
-		Spill(x2, y2, distance >= maxreach);
+		Spill(angle);
 		EmptyBucket();
 		PlayAnimation(clonk);
 		return true;
@@ -81,17 +74,6 @@ public func IsBucketEmpty()
 	return !IsBucketFilled();
 }
 
-/** Creates an imaginary line which runs for 'maxreach' distance (units in pixels) or until it hits a solid wall */
-private func GetBucketReachDistance(int angle)
-{
-	var distance = 0;
-	while(!GBackSolid(Sin(180-angle,distance),Cos(180-angle,distance)) && distance < maxreach)
-	{
-		++distance;
-	}
-	return distance;
-}
-
 private func PlayAnimation(object clonk)
 {
 	// animation only available for jumping and walking
@@ -122,109 +104,23 @@ private func PlayAnimation(object clonk)
 	clonk->UpdateAttach();
 }
 
-private func Spill(int x, int y, bool soft_spill)
+private func Spill(int angle)
 {
 	var obj = Contents(0);
 	if (!obj) return;
-	material_name = obj->GetMaterialName();
-	material_amount = obj->GetMaterialAmount();
+	var material_name = obj->GetMaterialName();
+	var material_amount = obj->GetMaterialAmount();
 	var stack_count = obj->~GetStackCount();
 	if (stack_count > 1) material_amount *= stack_count;
 	
 	// This will only spray out the material because no solid base to stick it on was found
-	if (soft_spill)
-	{
-		var angle = Angle(0,0, x,y);
-		CastPXS(material_name, material_amount, 20, 0,0, angle, 15);
-		return;
-	}
-	
-	// Store as property. This is solely done so that we don't have to pass it as a parameter everywhere.
-	
-	for (var i = 0; i < 5; i++)
-	{
-		// Fix some holes
-		if (i == 1)
-		{
-			DrawPixel(GetX()+x-1, GetY()+y-1);
-			if (!material_amount) break;
-			DrawPixel(GetX()+x+1, GetY()+y-1);
-			if (!material_amount) break;
-			DrawPixel(GetX()+x-1, GetY()+y+1);
-			if (!material_amount) break;
-			DrawPixel(GetX()+x+1, GetY()+y+1);
-			if (!material_amount) break;
-		}
-		if (!DrawCircle(GetX()+x,GetY()+y,i))
-			break;
-	}
-}
-
-private func DrawCircle(int x0, int y0, int radius)
-{
-	// Midpoint circle algorithm
-
-	var f = 1 - radius;
-	var ddF_x = 1;
-	var ddF_y = -2 * radius;
-	var x = 0;
-	var y = radius;
-
-	DrawPixel(x0, y0 + radius);
-	if (!material_amount) return false;
-	DrawPixel(x0, y0 - radius);
-	if (!material_amount) return false;
-	DrawPixel(x0 + radius, y0);
-	if (!material_amount) return false;
-	DrawPixel(x0 - radius, y0);
-	if (!material_amount) return false;
-
-	while(x < y)
-	{
-		if (f >= 0)
-		{
-			y--;
-			ddF_y += 2;
-			f += ddF_y;
-		}
-		x++;
-		ddF_x += 2;
-		f += ddF_x;
-		DrawPixel(x0 + x, y0 + y);
-		if (!material_amount) return false;
-		DrawPixel(x0 - x, y0 + y);
-		if (!material_amount) return false;
-		DrawPixel(x0 + x, y0 - y);
-		if (!material_amount) return false;
-		DrawPixel(x0 - x, y0 - y);
-		if (!material_amount) return false;
-		DrawPixel(x0 + y, y0 + x);
-		if (!material_amount) return false;
-		DrawPixel(x0 - y, y0 + x);
-		if (!material_amount) return false;
-		DrawPixel(x0 + y, y0 - x);
-		if (!material_amount) return false;
-		DrawPixel(x0 - y, y0 - x);
-		if (!material_amount) return false;
-	}
-
-	return true;
-}
-
-private func DrawPixel(int x, int y)
-{
-	// Don't overwrite solid material
-	if (GBackSolid(AbsX(x), AbsY(y))) return;
-
-	DrawMaterialQuad(material_name, x,y, x+1,y, x+1,y+1, x,y+1, true);
-	material_amount--;
+	CastPXS(material_name, material_amount, 20, 0,0, angle, 15);
 }
 
 protected func Hit()
 {
 	Sound("DullWoodHit?");
 }
-
 
 // Can collect IsBucketMaterial?
 public func IsBucket() { return true; }
