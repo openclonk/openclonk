0afc12a3f07f123f7facbbc9568242e3cbdc6e74
Sven Eberhardt <sven2@goldwipf.de>
2015-09-26 20:36:35 -0400

    Fix treasure hunt goal messages.

--- planet/Missions.ocf/TreasureHunt.ocs/TreasureHunt.ocd/Script.c
+++ planet/Missions.ocf/TreasureHunt.ocs/TreasureHunt.ocd/Script.c
@@ -18,41 +18,19 @@ public func OnOilDelivered() { return is_fulfilled = true; }
 
 public func GetDescription(int plr)
 {
+	var desc;
 	if (is_fulfilled)
-		return "$MsgGoalFulfilled$";
+		desc = "$MsgGoalFulfilled$";
 	else if (got_oil)
-		return "$MsgGotOil$";
+		desc = "$MsgGotOil$";
 	else if (has_gem_task)
-		return "$MsgGoalUnFulfilled$";
+		desc = "$MsgGoalUnFulfilled$";
 	else
-		return "$MsgGoalOil$";
+		desc = "$MsgGoalOil$";
+	if (has_gem_task || g_num_goldbars) desc = Format("%s|%s", desc, Format("$MsgSideGoal$", g_num_goldbars, MAX_GOLD_BARS));
+	return desc;
 }
 
-public func Activate(int byplr)
-{
-	var desc = GetDescription(byplr);
-	if (has_gem_task) desc = Format("%s|%s", desc, Format("$MsgSideGoal$", g_num_goldbars, MAX_GOLD_BARS));
-	ToggleGoalMessage(desc, byplr);
-	return true;
-}
-
-// Shows or hides a message window with information.
-private func ToggleGoalMessage(string msg, int plr)
-{
-	// If goal message open -> hide it.
-	if (GetEffect("GoalMessage", this))
-	{
-		CustomMessage("", nil, plr, nil, nil, nil, nil, nil, MSG_HCenter);
-		RemoveEffect("GoalMessage", this);
-		return;
-	}
-	// Otherwise open a new message.
-	AddEffect("GoalMessage", this, 100, 0, this);
-	CustomMessage(Format("@%s", msg), nil, plr, 0, 16 + 64, 0xffffff, GUI_MenuDeco, this, MSG_HCenter);
-	return;
-}
-
-protected func FxGoalMessageStart() {}
 public func GetShortDescription(int plr) {}
 
 /*-- Proplist --*/
