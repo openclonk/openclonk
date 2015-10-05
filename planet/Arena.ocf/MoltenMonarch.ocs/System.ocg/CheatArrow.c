3300f6f80d474123dfb896fbfa34d17c4902ed29
Clonkonaut <Armag3ddon@web.de>
2015-10-05 02:03:16 +0200

    Do not redefine constant ProjectileHit_tumble.

--- planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/CheatArrow.c
+++ planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/CheatArrow.c
@@ -1,1 +1,17 @@
-static const ProjectileHit_tumble=0; // the target tumbles
+#appendto Arrow
+
+// Arrows do not tumble
+
+public func HitObject(object obj)
+{
+	// Determine damage to obj from speed and arrow strength.
+	var relx = GetXDir() - obj->GetXDir();
+	var rely = GetYDir() - obj->GetYDir();
+	var speed = Sqrt(relx * relx + rely * rely);
+	var dmg = ArrowStrength() * speed / 100;
+	ProjectileHit(obj, dmg, 0);
+	// Stick does something unwanted to controller.
+	if (this) 
+		Stick();
+	return;
+}
