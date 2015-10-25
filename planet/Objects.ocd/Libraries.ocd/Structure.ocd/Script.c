0f1b48d2c6e72dea4a7809136bd8eeb6f864ceb2
David Dormagen <czapper@gmx.de>
2015-10-25 15:57:20 +0100

    structures: update interaction menus when taking damage

--- planet/Objects.ocd/Libraries.ocd/Structure.ocd/Script.c
+++ planet/Objects.ocd/Libraries.ocd/Structure.ocd/Script.c
@@ -42,11 +42,16 @@ public func Damage(int change, int cause, int cause_plr)
 {
 	// Only do stuff if the object has the HitPoints property.
 	if (this && this.HitPoints != nil)
+	{
 		if (GetDamage() >= this.HitPoints)
 		{		
 			// Destruction callback is made by the engine.
 			return RemoveObject();
 		}
+		
+		// Update all interaction menus with the new hitpoints.
+		UpdateInteractionMenus(this.GetDamageMenuEntries);
+	}
 	return _inherited(change, cause, cause_plr, ...);
 }
 
