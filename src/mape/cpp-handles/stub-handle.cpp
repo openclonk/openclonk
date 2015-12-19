27cbdb2177ac222baa6e97c3467ef0dc2928e72b
Günther Brammer <gbrammer@gmx.de>
2015-12-20 00:46:03 +0100

    Remove superfluous mape stubs

--- src/mape/cpp-handles/stub-handle.cpp
+++ src/mape/cpp-handles/stub-handle.cpp
@@ -35,19 +35,11 @@
 
 C4Landscape Landscape;
 C4PXSSystem PXS;
-C4GameObjects Objects;
-class C4Draw *pDraw = NULL;
 
 class C4SoundInstance *StartSoundEffectAt(const char *, int32_t, int32_t, int32_t, int32_t, int32_t, class C4SoundModifier *) { return NULL; }
 
-const char* C4Config::AtTempPath(const char *) { return NULL; }
-
-bool C4Draw::TextOut(const char *, CStdFont &, float, C4Surface *, float, float, DWORD, BYTE, bool) { return false; }
-
 C4Facet::C4Facet() {}
 void C4Facet::Set(C4Surface*, float, float, float, float) {}
-int32_t C4Facet::GetSectionCount() { return 0; }
-C4Facet C4Facet::TruncateSection(int32_t) { return *this; }
 
 C4Surface::C4Surface() {}
 C4Surface::~C4Surface() {}
@@ -79,7 +71,6 @@ C4IDListChunk::~C4IDListChunk() {}
 C4DefGraphics::C4DefGraphics(C4Def*) {}
 void C4DefGraphics::Clear() {}
 
-void C4DefList::Draw(C4ID, C4Facet &, bool, int32_t) {}
 void C4DefList::CallEveryDefinition() {}
 void C4DefList::ResetIncludeDependencies() {}
 bool C4DefList::DrawFontImage(const char* szImageTag, C4Facet& rTarget, C4DrawTransform* pTransform) { return false; }
@@ -97,77 +88,11 @@ void C4Landscape::CheckInstabilityRange(int32_t, int32_t) {}
 void C4Sky::Default() {}
 C4Sky::~C4Sky() {}
 
-void C4LSector::Clear() {}
-
-C4ObjectList::C4ObjectList() {}
-C4ObjectList::~C4ObjectList() {}
-void C4ObjectList::Default() {}
-void C4ObjectList::Clear() {}
-void C4ObjectList::InsertLinkBefore(C4ObjectLink*, C4ObjectLink*) {}
-void C4ObjectList::InsertLink(C4ObjectLink*, C4ObjectLink*) {}
-void C4ObjectList::RemoveLink(C4ObjectLink*) {}
-bool C4ObjectList::Add(C4Object*, C4ObjectList::SortType, C4ObjectList*) {return 0;}
-bool C4ObjectList::Remove(C4Object*) {return 0;}
-bool C4ObjectList::AssignInfo() {return 0;}
-bool C4ObjectList::ValidateOwners() {return 0;}
-
-void C4NotifyingObjectList::InsertLinkBefore(C4ObjectLink *pLink, C4ObjectLink *pBefore) {}
-void C4NotifyingObjectList::InsertLink(C4ObjectLink*, C4ObjectLink*) {}
-void C4NotifyingObjectList::RemoveLink(C4ObjectLink*) {}
-
-C4GameObjects::C4GameObjects() {}
-C4GameObjects::~C4GameObjects() {}
-void C4GameObjects::Clear(bool) {}
-void C4GameObjects::Default() {}
-bool C4GameObjects::Remove(C4Object*) {return 0;}
-bool C4GameObjects::AssignInfo() {return 0;}
-bool C4GameObjects::ValidateOwners() {return 0;}
-C4Object * C4GameObjects::ObjectPointer(int) {return 0;}
-void C4GameObjects::UpdateScriptPointers() {}
-C4Value C4GameObjects::GRBroadcast(char const*, C4AulParSet*, bool, bool) {return C4Value();}
-
 C4PXSSystem::C4PXSSystem() {}
 C4PXSSystem::~C4PXSSystem() {}
 bool C4PXSSystem::Create(int, C4Real, C4Real, C4Real, C4Real) { return false; }
 
 bool C4TextureShape::Load(C4Group &group, const char *filename, int32_t base_tex_wdt, int32_t base_tex_hgt) { return true; }
-
-#if 0
-/* Pulled in by C4Game... */
-CStdFont::CStdFont() {}
-C4PathFinder::C4PathFinder() {}
-C4PathFinder::~C4PathFinder() {}
-C4TransferZones::C4TransferZones() {}
-C4TransferZones::~C4TransferZones() {}
-C4PacketBase::C4PacketBase() {}
-C4PacketList::C4PacketList() {}
-C4PacketBase::~C4PacketBase() {}
-C4PacketList::~C4PacketList() {}
-C4Control::C4Control() {}
-C4Control::~C4Control() {}
-C4GameControl::C4GameControl(): Network(this) {}
-C4GameControl::~C4GameControl() {}
-C4GameControlNetwork::C4GameControlNetwork(C4GameControl*): pParent(NULL) {}
-C4GameControlNetwork::~C4GameControlNetwork() {}
-C4GraphicsResource::C4GraphicsResource(): CaptionFont(FontCaption), TitleFont(FontTitle), TextFont(FontRegular), MiniFont(FontTiny), TooltipFont(FontTooltip) {}
-C4GraphicsResource::~C4GraphicsResource() {}
-C4GameParameters::C4GameParameters() {}
-C4GameParameters::~C4GameParameters() {}
-
-//C4Extra::C4Extra() {}
-//C4Extra::~C4Extra() {}
-void C4Extra::Clear() {}
-
-static C4KeyboardInput KeyboardInput;
-void C4KeyboardInput::Clear() {}
-bool C4KeyboardInput::IsValid = false;
-C4KeyboardInput &C4KeyboardInput_Init() { return KeyboardInput; }
-
-static C4GameParameters GameParameters;
-static C4RoundResults GameRoundResults;
-C4Game::C4Game(): Parameters(GameParameters), Clients(Parameters.Clients), Teams(Parameters.Teams), PlayerInfos(Parameters.PlayerInfos), RestorePlayerInfos(Parameters.RestorePlayerInfos), RoundResults(GameRoundResults), Input(Control.Input), KeyboardInput(C4KeyboardInput_Init()) {}
-C4Game::~C4Game() {}
-#endif
 
 C4Shader::C4Shader() {}
 C4Shader::~C4Shader() {}
