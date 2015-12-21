915c3b13af331817abf2afda9cb08180473abb17
Sven Eberhardt <sven2@goldwipf.de>
2015-12-20 19:02:11 -0500

    Thunderous Skies: Play "Pop" sound when target balloon is hit.

--- planet/Arena.ocf/ThunderousSkies.ocs/TargetBalloon.ocd/Script.c
+++ planet/Arena.ocf/ThunderousSkies.ocs/TargetBalloon.ocd/Script.c
@@ -36,6 +36,7 @@ public func IsProjectileTarget(target,shooter)
 public func OnProjectileHit(object projectile)
 {
 	CreateParticle("Air", 0, -10, PV_Random(-30, 30), PV_Random(-30,30), PV_Random(30, 120), Particles_Air(), 10);
+	Sound("Objects::Balloon::Pop");
 	if(load) load->~Fall(projectile->GetController());
 	RemoveObject();
 }
