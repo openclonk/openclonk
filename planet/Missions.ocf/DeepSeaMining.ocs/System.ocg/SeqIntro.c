0348efa03e0302d29a388df320f18705fd526304
Clonkonaut <Armag3ddon@web.de>
2016-01-02 13:44:23 +0100

    Fix propeller not moving in Deep Sea Mining Intro (#1531).

--- planet/Missions.ocf/DeepSeaMining.ocs/System.ocg/SeqIntro.c
+++ planet/Missions.ocf/DeepSeaMining.ocs/System.ocg/SeqIntro.c
@@ -48,6 +48,14 @@ func Intro_PlaneTimer(...)
 		if (!Random(4)) this.rdir = BoundBy((80+Random(21)-GetR())/5,-1,1);
 		SetXDir(); SetYDir(GetR()*2-GetY()+Random(5),10);
 	}
+	//propellor
+	var change = GetAnimationPosition(this.propanim) + 15 * 3;
+	if(change > GetAnimationLength("Propellor"))
+		change = (GetAnimationPosition(this.propanim) + 15 * 3) - GetAnimationLength("Propellor");
+	if(change < 0)
+		change = (GetAnimationLength("Propellor") - 15 * 3);
+
+	SetAnimationPosition(this.propanim, Anim_Const(change));
 	return rv;
 }
 
