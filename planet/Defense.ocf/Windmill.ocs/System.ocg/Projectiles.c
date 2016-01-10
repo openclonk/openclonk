5fd8bfb14065a2bcb0c86a7856c930987b3027ee
Clonkonaut <Armag3ddon@web.de>
2016-01-10 13:11:26 +0100

    Projectiles will fade out very fast (#1555).

---
+++ planet/Defense.ocf/Windmill.ocs/System.ocg/Projectiles.c
@@ -0,0 +1,18 @@
+#appendto Arrow
+#appendto Javelin
+
+local fade_out_forced = false;
+
+// Arrows and javelins should fade out very fast after hitting, they are useless by then anyway
+
+private func Stick()
+{
+	if (!fade_out_forced)
+	{
+		fade_out_forced = true;
+		if (g_object_fade) AddEffect("IntFadeOut", this, 100, 1, g_object_fade, Rule_ObjectFade);
+	}
+	_inherited();
+}
+
+public func FadeOutForced() { return fade_out_forced; }
