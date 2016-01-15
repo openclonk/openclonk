579cb7e66ccb7949a980fb90292659dbb1bc1925
Sven Eberhardt <sven2@goldwipf.de>
2016-01-14 21:40:42 -0500

    Reset action targets on procedure change #1552

--- src/object/C4Object.cpp
+++ src/object/C4Object.cpp
@@ -2872,10 +2872,12 @@ bool C4Object::SetAction(C4PropList * Act, C4Object *pTarget, C4Object *pTarget2
 	if (Act!=LastAction)
 	{
 		Action.Time=0;
-		// reset action data if procedure is changed
+		// reset action data and targets if procedure is changed
 		if ((Act ? Act->GetPropertyP(P_Procedure) : -1)
 		    != (LastAction ? LastAction->GetPropertyP(P_Procedure) : -1))
 			Action.Data = 0;
+		Action.Target = NULL;
+		Action.Target2 = NULL;
 	}
 	// Set new action
 
