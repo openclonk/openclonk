418964f6e8bf0c49cdc15b33796aeb893715b468
Sven Eberhardt <sven2@goldwipf.de>
2016-01-17 12:22:08 -0500

    Hot Ice: Spawn and map improvements in "small islands" mode #1623

--- planet/Arena.ocf/HotIce.ocs/Script.c
+++ planet/Arena.ocf/HotIce.ocs/Script.c
@@ -5,9 +5,10 @@ func Initialize()
 	// Materials: Chests
 	var i,pos;
 	var ls_wdt = LandscapeWidth(), ls_hgt = LandscapeHeight();
-	var top_area_hgt = ls_hgt*[50,80][SCENPAR_MapType]/100;
+	var chest_area_y = ls_hgt*[0,30][SCENPAR_MapType]/100;
+	var chest_area_hgt = ls_hgt/2;
 	for (i=0; i<6; ++i)
-		if (pos=FindLocation(Loc_InRect(0,0,ls_wdt,top_area_hgt-100), Loc_Wall(CNAT_Bottom))) // Loc_Wall adds us 100 pixels...
+		if (pos=FindLocation(Loc_InRect(0,chest_area_y,ls_wdt,chest_area_hgt-100), Loc_Wall(CNAT_Bottom))) // Loc_Wall adds us 100 pixels...
 		{
 			var chest = CreateObjectAbove(Chest,pos.x,pos.y);
 			if (chest)
@@ -24,27 +25,40 @@ func Initialize()
 		}
 	// Materials: Firestones
 	for (i=0; i<30; ++i)
-		if (pos=FindLocation(Loc_InRect(0,0,ls_wdt,top_area_hgt), Loc_Solid()))
+		if (pos=FindLocation(Loc_InRect(0,chest_area_y,ls_wdt,chest_area_hgt), Loc_Solid()))
 			if (IsFirestoneSpot(pos.x,pos.y))
 				CreateObjectAbove(Firestone,pos.x,pos.y-1);
-	// Some firestones in lower half. For ap type 1, more firestones in lower than upper half.
+	// Some firestones and bombs in lower half. For ap type 1, more firestones in lower than upper half.
 	for (i=0; i<30; ++i)
 		if (pos=FindLocation(Loc_InRect(0,ls_hgt/2,ls_wdt,ls_hgt/3), Loc_Solid()))
 			if (IsFirestoneSpot(pos.x,pos.y))
-				CreateObjectAbove(Firestone,pos.x,pos.y-1);
+				CreateObjectAbove([Firestone,IronBomb][Random(Random(3))],pos.x,pos.y-1);
 	return true;
 }
+
+static g_player_spawn_positions, g_map_width, g_player_spawn_index;
 
 func InitializePlayer(int plr)
 {
 	// everything visible
 	SetFoW(false, plr);
-	// player positioning. In lower area for both maps becuase starting high is an an advantage.
+	// Player positioning. 
 	var ls_wdt = LandscapeWidth(), ls_hgt = LandscapeHeight();
-	var crew = GetCrew(plr);
-	var start_pos = FindLocation(Loc_InRect(ls_wdt/5,ls_hgt/2,ls_wdt*3/5,ls_hgt/3), Loc_Wall(CNAT_Bottom), Loc_Func(Scenario.IsStartSpot));
-	if (!start_pos) start_pos = FindLocation(Loc_InRect(ls_wdt/10,0,ls_wdt*8/10,ls_hgt*4/5), Loc_Wall(CNAT_Bottom), Loc_Func(Scenario.IsStartSpot));
-	if (!start_pos) start_pos = {x=Random(ls_wdt*6/10)+ls_wdt*2/10, y=ls_hgt*58/100};
+	var crew = GetCrew(plr), start_pos;
+	// Position by map type?
+	if (g_player_spawn_positions && g_player_spawn_index < GetLength(g_player_spawn_positions))
+	{
+		start_pos = g_player_spawn_positions[g_player_spawn_index++];
+		var map_zoom = ls_wdt / g_map_width;
+		start_pos = {x=start_pos[0]*map_zoom+map_zoom/2, y=start_pos[1]*map_zoom};
+	}
+	else
+	{
+		// Start positions not defined or exhausted: Spawn in lower area for both maps becuase starting high is an an advantage.
+		start_pos = FindLocation(Loc_InRect(ls_wdt/5,ls_hgt/2,ls_wdt*3/5,ls_hgt/3), Loc_Wall(CNAT_Bottom), Loc_Func(Scenario.IsStartSpot));
+		if (!start_pos) start_pos = FindLocation(Loc_InRect(ls_wdt/10,0,ls_wdt*8/10,ls_hgt*4/5), Loc_Wall(CNAT_Bottom), Loc_Func(Scenario.IsStartSpot));
+		if (!start_pos) start_pos = {x=Random(ls_wdt*6/10)+ls_wdt*2/10, y=ls_hgt*58/100};
+	}
 	crew->SetPosition(start_pos.x, start_pos.y-10);
 	// initial material
 	crew->CreateContents(Shovel);
