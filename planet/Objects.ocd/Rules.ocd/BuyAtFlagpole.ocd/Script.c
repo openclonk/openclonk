62dd3aafd6871873f5cdffc8895b3b2f2d3b0d8b
Sven Eberhardt <sven2@goldwipf.de>
2016-01-17 22:28:57 -0500

    Activate wealth display in scenarios with Rule_BuyAtFlagpole.
    Most missions with home base were missing the display.

--- planet/Objects.ocd/Rules.ocd/BuyAtFlagpole.ocd/Script.c
+++ planet/Objects.ocd/Rules.ocd/BuyAtFlagpole.ocd/Script.c
@@ -1,5 +1,16 @@
 /*-- Buy at flagpole --*/
 
+public func Construction(...)
+{
+	// This rule enables wealth display because it's annoying to not
+	// know your wealth in multiplayer when many people buy/sell at
+	// the flagpole)
+	// Doesn't remove the display on destruction because there's no
+	// refcounting for this command.
+	GUI_Controller->ShowWealth();
+	return _inherited(...);
+}
+
 protected func Activate(int iByPlayer)
 {
 	MessageWindow(GetProperty("Description"), iByPlayer);
