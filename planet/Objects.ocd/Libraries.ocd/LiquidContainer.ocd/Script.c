b64a0e852aaea5039fe027758c37c9bedd6822dc
Mark <markh4b@hotmail.com>
2016-01-28 17:39:16 +0100

    Library: Liquid Container
    Added a new library for liquid containers that is based on the original barrel script.
    
    Library_LiquidContainer
    - Extracted the interface from the barrel. The function names and argument types are not final yet.
    - Fixed an inconsistency in the script: There were two ways of determining whether a barrel is suited
       for a material. Either by name, or material index.
    - renamed argument names to "liquid_name" for liquids, "amount" for quantities, so that the code is more easily understandable
    - replaced comparison 'fill level > 0' with 'is not empty?'
    - moved storage variables to a local proplist, so that names do not clash
    - Ordered the methods by application and documented each section
    
    Barrel:
    - The original barrel functions delegate to Library_LiquidContainer-functions now, so that the original functionality is not disturbed. Obsolete functions are marked with "TODO: deprecated".
    - Moved barrel value change to the update function
    - Moved hit sound effect to separate function
    - Replaced uses of the magic number '3' with a runtime-overloadable call to GetBarrelIntakeY()
    - Replaced comparison 'fill level > 0' with 'is not empty?'
    
    MetalBarrel:
    - Adjusted the hit function. The difference between this and the normal barrel is the hit sound and material position. These should be constants or functions, instead of magic numbers
    - Moved hit sound effect to separate function
    - Removed the Hit()-function, because the hit sound is played in a separate function now
    - With this change the y-offset of the barrel checking for other liquids on a hit is now 3 instead of 7. This should be ok, because the barrels have the same dimensions, it seemed strange that they would have different offsets

---
+++ planet/Objects.ocd/Libraries.ocd/LiquidContainer.ocd/Script.c
@@ -0,0 +1,184 @@
+/**
+ * Liquid Container
+ *
+ * Basic interface for anything that can contain liquids.
+ *
+ * Author: Ringwaul, ST-DDT, Marky
+ */
+
+
+local lib_liquid_container;
+
+// -------------- Properties
+//
+// Simple properties that define the object as a liquid container,
+// what kind of liquid it can hold, how much it can hold
+//
+// naming scheme: [verb]LiquidContainer[attribute], because it concerns the container
+
+func IsLiquidContainer() { return true;}
+
+func IsLiquidContainerForMaterial(string liquid_name)
+{
+	return false;
+}
+
+func GetLiquidContainerMaxFillLevel()
+{
+	return 0;
+}
+
+// -------------- Current Status
+//
+// Simple boolean status checks
+//
+// naming scheme: LiquidContainer[attribute/question]
+
+func LiquidContainerIsEmpty()
+{
+	return GetLiquidFillLevel() == 0;
+}
+
+func LiquidContainerIsFull()
+{
+	return GetLiquidFillLevel() == GetLiquidContainerMaxFillLevel();
+}
+
+func LiquidContainerAccepts(string liquid_name)
+{
+	return IsLiquidContainerForMaterial(liquid_name)
+	   && (LiquidContainerIsEmpty() || GetLiquidName() == liquid_name);
+}
+
+// -------------- Getters
+//
+// Getters for stored liquid and amount
+// - these should be used primarily by objects that include this library
+//
+// naming scheme: GetLiquid[attribute], because it concerns the liquid
+
+func GetLiquidName()
+{
+	if (LiquidContainerIsEmpty())
+		return nil; // TODO: was "", this was inconsistent throughout the barrel
+	return lib_liquid_container.liquid;
+}
+
+func GetLiquidFillLevel()
+{
+	return lib_liquid_container.volume;
+}
+
+// -------------- Setters
+//
+// Setters for stored liquid and amount
+// - these should be used primarily by objects that include this library
+//
+// naming scheme: SetLiquid[attribute], because it concerns the liquid
+
+func SetLiquidName(string liquid_name)
+{
+	lib_liquid_container.liquid = liquid_name;
+}
+
+func SetLiquidFillLevel(int amount)
+{
+	ChangeLiquidFillLevel(amount - GetLiquidFillLevel());
+}
+
+func ChangeLiquidFillLevel(int amount)
+{
+	lib_liquid_container.volume += amount;
+	
+	// Empty the liquid container
+	if (LiquidContainerIsEmpty())
+	{
+		SetLiquidName(nil);
+	}
+	
+	this->UpdateLiquidContainer();
+}
+
+// -------------- Interaction
+//
+// Interfaces for interaction with other objects
+
+/**
+Extracts liquid from the container.
+@param liquid_name: Material to extract; Wildcardsupport
+               Defaults to the current liquid if 'nil' is passed.
+@param amount: Max Amount of liquid being extracted;
+               Defaults to all contained liquid if 'nil' is passed.
+@param destination: Object that extracts the liquid
+@return [returned_liquid, returned_amount]
+	   - returned_liquid: Material being extracted
+	   - returned_amount: Amount being extracted
+*/
+func RemoveLiquid(string liquid_name, int amount, object destination)
+{
+	// default parameters if nothing is provided: the current material and level
+	liquid_name = liquid_name ?? GetLiquidName();
+	amount = amount ?? GetLiquidFillLevel();
+
+	//Wrong material?
+	if (!WildcardMatch(GetLiquidName(), liquid_name))
+		amount = 0;
+	amount = Min(amount, GetLiquidFillLevel());
+	ChangeLiquidFillLevel(-amount);
+	return [GetLiquidName(), amount];
+}
+
+/** 
+Inserts liquid into the container.
+@param liquid_name: Material to insert
+@param amount: Max Amount of Material being inserted 
+@param source: Object which inserts the liquid
+@return returned_amount: The inserted amount
+*/
+func PutLiquid(string liquid_name, int amount, object source)
+{
+	if (LiquidContainerAccepts(liquid_name))
+	{
+		SetLiquidName(liquid_name);
+		amount = BoundBy(GetLiquidContainerMaxFillLevel() - GetLiquidFillLevel(), 0, amount);
+		ChangeLiquidFillLevel(+amount);
+		return amount;
+	}
+	else //Wrong material?
+	{
+		return 0;
+	}
+}
+
+// --------------  Internals --------------
+//
+// Internal stuff
+
+func Construction()
+{
+	// use proplist to avoid name clashes
+	lib_liquid_container = {
+		liquid = nil,	// the liquid - this should be a string, so that the container may contain liquids that are not materials
+	    volume = 0};	// the stored amount
+}
+
+func SaveScenarioObject(props)
+{
+	if (!inherited(props, ...)) return false;
+	if (GetLiquidName())
+		props->AddCall("Fill", this, "SetLiquidContainer", Format("%v", GetLiquidName()), GetLiquidFillLevel());
+	return true;
+}
+
+// set the current state, without sanity checks
+func SetLiquidContainer(string liquid_name, int amount)
+{
+	SetLiquidName(liquid_name);
+	SetLiquidFillLevel(amount);
+}
+
+// interface for updating graphics, etc
+func UpdateLiquidContainer()
+{
+	// do nothing by default
+}
