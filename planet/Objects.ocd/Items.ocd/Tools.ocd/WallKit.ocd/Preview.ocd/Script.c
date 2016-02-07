1e5b04e2d1a556efcd7cd121881927baa7b5a766
Sven Eberhardt <sven2@goldwipf.de>
2016-02-06 23:53:45 -0500

    WallKit preview: Add function to update independent of position.

--- planet/Objects.ocd/Items.ocd/Tools.ocd/WallKit.ocd/Preview.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Tools.ocd/WallKit.ocd/Preview.ocd/Script.c
@@ -1,17 +1,23 @@
 /*-- WallKit preview --*/
 
 // definition call: create line
-func Create(int x1, int y1, int x2, int y2, int clr)
+public func Create(int x1, int y1, int x2, int y2, int clr)
 {
 	var obj = CreateObjectAbove(WallKit_Preview);
 	obj->Set(x1, y1, x2, y2, clr);
 	return obj;
 }
 
-func Set(int x1,int y1,int x2,int y2,int clr)
+public func Set(int x1,int y1,int x2,int y2,int clr)
 {
 	SetVertexXY(0, x1, y1);
 	SetVertexXY(1, x2, y2);
 	this.LineColors = [clr, clr];
-	return;
+	return true;
+}
+
+public func SetColor(int clr)
+{
+	this.LineColors = [clr, clr];
+	return true;
 }
