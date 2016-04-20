f41d3b88699583de01541a66f7636576a2d90095
Sven Eberhardt <sven2@goldwipf.de>
2016-04-19 21:16:47 -0400

    Fix out-of-bounds memory access in C4Team [sphalerite] #1717

--- src/control/C4Teams.cpp
+++ src/control/C4Teams.cpp
@@ -338,7 +338,9 @@ void C4TeamList::AddTeam(C4Team *pNewTeam)
 	// add team; grow vector if necessary
 	if (iTeamCount >= iTeamCapacity)
 	{
-		C4Team **ppNewTeams = new C4Team*[(iTeamCapacity = iTeamCount+4)&~3];
+		// grow up to the nearest multiple of 4 elements
+		// (TODO: Replace the whole thing e.g. with a simple std::vector<C4Team>)
+		C4Team **ppNewTeams = new C4Team*[(iTeamCapacity = ((iTeamCount+4)&~3))];
 		if (iTeamCount) memcpy(ppNewTeams, ppList, iTeamCount*sizeof(C4Team *));
 		delete [] ppList; ppList = ppNewTeams;
 	}
