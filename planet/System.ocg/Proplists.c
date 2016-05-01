a81ccaab2206ab3c739de0216af3e2937bccdd8d
Maikel de Vries <mdt.maikel@gmail.com>
2016-05-01 12:58:25 +0200

    add function to make properties writable
    Thanks to Guenther for the implementation details, documentation will follow when it is considered a stable solution.

--- planet/System.ocg/Proplists.c
+++ planet/System.ocg/Proplists.c
@@ -1,5 +1,29 @@
-/*--
-		Proplists.c
+/**
+	Proplists.c
+	General helper functions that create or work with proplists.
+		
+	@author Guenther, Maikel
+*/
 
-		General helper functions that create or work with proplists.
---*/
+// Turns a property into a writable one. This for example useful if an object
+// inherits a property from a definition which should not stay read-only.
+global func MakePropertyWritable(name, obj)
+{
+	obj = obj ?? this;
+	if (GetType(obj[name]) == C4V_Array)
+	{
+		obj[name] = obj[name][:];
+		var len = GetLength(obj[name]);
+		for (var i = 0; i < len; ++i)
+			MakePropertyWritable(i, obj[name]);
+	}
+	if (GetType(obj[name]) == C4V_PropList)
+	{
+		obj[name] = new obj[name] {};
+		var properties = GetProperties(obj[name]);
+		var len = GetLength(properties);
+		for (var i = 0; i < len; ++i)
+			MakePropertyWritable(properties[i], obj[name]);
+	}
+  	return;
+}
