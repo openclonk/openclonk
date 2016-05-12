be2fdaaae3afee8b8dee49cd0221a3fb5d2957fb
Nicolas Hake <isilkor@openclonk.org>
2016-05-12 18:41:39 +0200

    C4Particles: Fix build with MSVC memory tracing

--- src/landscape/C4Particles.h
+++ src/landscape/C4Particles.h
@@ -18,7 +18,10 @@
 
 #include "platform/StdScheduler.h"
 
+#pragma push_macro("new")
+#undef new
 #include <pcg/pcg_random.hpp>
+#pragma pop_macro("new")
 
 #ifndef INC_C4Particles
 #define INC_C4Particles
