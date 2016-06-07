96068b9a176ded0d5cca528c90f7a86879701808
Sven Eberhardt <sven2@goldwipf.de>
2016-06-07 18:23:53 -0400

    Initialize EditorBase definition with higher priority.
    Many other definitions may depend on it being initialized and it would be verbose and error-prone to add a check to every single dependent definition.

--- planet/Objects.ocd/Helpers.ocd/EditorBase.ocd/Script.c
+++ planet/Objects.ocd/Helpers.ocd/EditorBase.ocd/Script.c
@@ -8,7 +8,7 @@ local EditorProp_Invincibility = { Type = "has_effect", Effect = "IntInvincible"
 local EditorProp_PlayerColor = { Type = "color", AsyncGet = "GetColor", Set = "SetColor" };
 local Plane = 1;
 
-
+local DefinitionPriority=100; // Call this definition early to allow EditorProp initialization
 func Definition(def)
 {
 	// Property delegate types
