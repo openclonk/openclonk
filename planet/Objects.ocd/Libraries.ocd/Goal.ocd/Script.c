a21a1a8051a28c8660c7fb6fcb5edcaec49439e0
Sven Eberhardt <sven2@goldwipf.de>
2016-08-06 02:37:25 -0400

    Limit goal placement in editor to one per goal type

--- planet/Objects.ocd/Libraries.ocd/Goal.ocd/Script.c
+++ planet/Objects.ocd/Libraries.ocd/Goal.ocd/Script.c
@@ -7,6 +7,7 @@
 		bool IsFullfilled(); - is the goal fulfilled?
 --*/
 
+local EditorPlacementLimit = 1; // Goals are to be placed only once (unless overloaded by the goal)
 
 local mission_password; // mission password to be gained when the goal is fulfilled
 
