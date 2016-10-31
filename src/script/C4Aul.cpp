openclonk/openclonk

commit 9caaf1e2988039441c6239e959b21484e8d5af1c

Aul: Split out error handling into a separate class for easier testing
By using an extern error handler in the script engine, we can mock that
handler and make sure something that should fail actually does, instead
of having to parse log messages.

src/script/C4Aul.cpp
@@ -26,15 +26,18 @@
 #include "c4group/C4Components.h"
 #include "c4group/C4LangStringTable.h"
 
-C4AulError::C4AulError(): shown(false) {}
-
-void C4AulError::show()
+static class DefaultErrorHandler : public C4AulErrorHandler
 {
-	shown = true;
-	// simply log error message
-	if (sMessage)
-		DebugLog(sMessage.getData());
-}
+public:
+	void OnError(const char *msg) override
+	{
+		DebugLogF("ERROR: %s", msg);
+	}
+	void OnWarning(const char *msg) override
+	{
+		DebugLogF("WARNING: %s", msg);
+	}
+} DefaultErrorHandler;
 
 const char *C4AulError::what() const noexcept
 {
@@ -44,8 +47,9 @@ const char *C4AulError::what() const noexcept
 /*--- C4AulScriptEngine ---*/
 
 C4AulScriptEngine::C4AulScriptEngine():
-		C4PropListStaticMember(NULL, NULL, ::Strings.RegString("Global")),
-		warnCnt(0), errCnt(0), lineCnt(0)
+	C4PropListStaticMember(NULL, NULL, ::Strings.RegString("Global")),
+	ErrorHandler(&DefaultErrorHandler),
+	warnCnt(0), errCnt(0), lineCnt(0)
 {
 	GlobalNamedNames.Reset();
 	GlobalNamed.Reset();
@@ -229,6 +233,18 @@ C4AulUserFile *C4AulScriptEngine::GetUserFile(int32_t handle)
 	return NULL;
 }
 
+void C4AulScriptEngine::RegisterErrorHandler(C4AulErrorHandler *handler)
+{
+	assert(ErrorHandler == &DefaultErrorHandler);
+	ErrorHandler = handler;
+}
+
+void C4AulScriptEngine::UnregisterErrorHandler(C4AulErrorHandler *handler)
+{
+	assert(ErrorHandler == handler);
+	ErrorHandler = &DefaultErrorHandler;
+}
+
 /*--- C4AulFuncMap ---*/
 
 C4AulFuncMap::C4AulFuncMap(): FuncCnt(0)
@@ -288,4 +304,9 @@ void C4AulFuncMap::Remove(C4AulFunc * func)
 	}
 	*pFunc = (*pFunc)->MapNext;
 	--FuncCnt;
+}
+
+C4AulErrorHandler::~C4AulErrorHandler()
+{
+	::ScriptEngine.UnregisterErrorHandler(this);
 }

src/script/C4Aul.h
@@ -34,11 +34,8 @@ protected:
 	StdCopyStrBuf sMessage;
 
 public:
-	bool shown;
-	C4AulError();
 	virtual ~C4AulError() { } // destructor
 	virtual const char *what() const noexcept;
-	void show(); // present error message
 };
 
 // parse error
@@ -94,6 +91,14 @@ public:
 	int32_t GetHandle() const { return handle; }
 };
 
+class C4AulErrorHandler
+{
+public:
+	virtual ~C4AulErrorHandler();
+	virtual void OnError(const char *msg) = 0;
+	virtual void OnWarning(const char *msg) = 0;
+};
+
 // holds all C4AulScripts
 class C4AulScriptEngine: public C4PropListStaticMember
 {
@@ -108,6 +113,8 @@ protected:
 	// all open user files
 	// user files aren't saved - they are just open temporary e.g. during game saving
 	std::list<C4AulUserFile> UserFiles;
+
+	C4AulErrorHandler *ErrorHandler;
 
 public:
 	int warnCnt, errCnt; // number of warnings/errors
@@ -149,6 +156,13 @@ public:
 	int32_t CreateUserFile(); // create new file and return handle
 	void CloseUserFile(int32_t handle); // close user file given by handle
 	C4AulUserFile *GetUserFile(int32_t handle); // get user file given by handle
+
+	void RegisterErrorHandler(C4AulErrorHandler *handler);
+	void UnregisterErrorHandler(C4AulErrorHandler *handler);
+	C4AulErrorHandler *GetErrorHandler() const
+	{
+		return ErrorHandler;
+	}
 
 	friend class C4AulFunc;
 	friend class C4AulProfiler;

src/script/C4AulCompiler.cpp
@@ -90,13 +90,11 @@ static std::string FormatCodePosition(const C4ScriptHost *source_host, const cha
 template<class... T>
 static void Warn(const C4ScriptHost *target_host, const C4ScriptHost *host, const char *SPos, const C4AulScriptFunc *func, const char *msg, T &&...args)
 {
-	std::string message = "WARNING: ";
-
-	message += sizeof...(T) > 0 ? strprintf(msg, std::forward<T>(args)...) : msg;
+	std::string message = sizeof...(T) > 0 ? strprintf(msg, std::forward<T>(args)...) : msg;
 	message += FormatCodePosition(host, SPos, target_host, func);
 
 	++::ScriptEngine.warnCnt;
-	DebugLog(message.c_str());
+	::ScriptEngine.GetErrorHandler()->OnWarning(message.c_str());
 }
 
 template<class... T>

src/script/C4AulExec.cpp
@@ -172,8 +172,10 @@ C4Value C4AulExec::Exec(C4AulScriptFunc *pSFunc, C4PropList * p, C4Value *pnPars
 	}
 	catch (C4AulError &e)
 	{
-		if(!e.shown) e.show();
+		if (!fPassErrors)
+			::ScriptEngine.GetErrorHandler()->OnError(e.what());
 		// Unwind stack
+		// TODO: The stack dump should be passed to the error handler somehow
 		while (pCurCtx > pOldCtx)
 		{
 			pCurCtx->dump(StdStrBuf(" by: "));
@@ -1044,7 +1046,7 @@ C4Value C4AulExec::DirectExec(C4PropList *p, const char *szScript, const char *s
 	{
 		if(fPassErrors)
 			throw;
-		ex.show();
+		::ScriptEngine.GetErrorHandler()->OnError(ex.what());
 		LogCallStack();
 		StopDirectExec();
 		return C4VNull;

src/script/C4AulFunc.cpp
@@ -69,7 +69,7 @@ bool C4AulFunc::CheckParTypes(const C4Value pPars[], bool fPassErrors) const {
 				throw e;
 			else
 			{
-				e.show();
+				::ScriptEngine.GetErrorHandler()->OnError(e.what());
 				return false;
 			}
 		}

src/script/C4AulLink.cpp
@@ -75,7 +75,7 @@ bool C4ScriptHost::ResolveIncludes(C4DefList *rDefs)
 	// catch circular includes
 	if (Resolving)
 	{
-		C4AulParseError(this, "Circular include chain detected - ignoring all includes!").show();
+		Engine->GetErrorHandler()->OnError(C4AulParseError(this, "Circular include chain detected - ignoring all includes!").what());
 		IncludesResolved = true;
 		State = ASS_LINKED;
 		return false;
@@ -168,7 +168,7 @@ void C4AulScriptEngine::Link(C4DefList *rDefs)
 	catch (C4AulError &err)
 	{
 		// error??! show it!
-		err.show();
+		ErrorHandler->OnError(err.what());
 	}
 
 	// Set name list for globals (FIXME: is this necessary?)

src/script/C4AulParse.cpp
@@ -189,7 +189,6 @@ void C4AulParse::AppendPosition(StdStrBuf & Buf)
 }
 
 C4AulParseError::C4AulParseError(C4AulParse * state, const char *pMsg)
-		: C4AulError()
 {
 	// compose error string
 	sMessage.Ref("ERROR: ");
@@ -211,7 +210,6 @@ C4AulParseError::C4AulParseError(C4ScriptHost *pScript, const char *pMsg)
 }
 
 C4AulParseError::C4AulParseError(C4AulScriptFunc * Fn, const char *SPos, const char *pMsg)
-		: C4AulError()
 {
 	// compose error string
 	sMessage.Ref("ERROR: ");
@@ -786,7 +784,7 @@ std::unique_ptr<::aul::ast::Script> C4AulParse::Parse_Script(C4ScriptHost * scri
 	pOrgScript = scripthost;
 	SPos = pOrgScript->Script.getData();
 	const char * SPos0 = SPos;
-	bool all_ok = true;
+	bool first_error = true;
 	auto script = ::aul::ast::Script::New(SPos0);
 	while (true) try
 	{
@@ -854,19 +852,16 @@ std::unique_ptr<::aul::ast::Script> C4AulParse::Parse_Script(C4ScriptHost * scri
 		default:
 			UnexpectedToken("declaration");
 		}
-		all_ok = true;
+		first_error = true;
 	}
 	catch (C4AulError &err)
 	{
-		// damn! something went wrong, print it out
-		// but only one error per function
-		if (all_ok)
+		if (first_error)
 		{
-			err.show();
-			// and count (visible only ;) )
 			++Engine->errCnt;
+			::ScriptEngine.ErrorHandler->OnError(err.what());
 		}
-		all_ok = false;
+		first_error = false;
 	}
 }
 

src/script/C4PropList.cpp
@@ -652,7 +652,7 @@ C4Value C4PropList::Call(const char * s, C4AulParSet *Pars, bool fPassErrors)
 			C4AulExecError err(FormatString("Undefined function: %s", s).getData());
 			if (fPassErrors)
 				throw err;
-			err.show();
+			::ScriptEngine.GetErrorHandler()->OnError(err.what());
 		}
 		return C4Value();
 	}

commit 3c13c88f56b4294c6e0a5035db6cb53e192964fe

Aul: Count errors and warnings properly
9caaf1e introduced an external error handler for easier testing of
error conditions. The default error handler needs to count errors and
warnings if we want to display them, but didn't.

src/script/C4Aul.cpp
@@ -32,10 +32,12 @@ public:
 	void OnError(const char *msg) override
 	{
 		DebugLogF("ERROR: %s", msg);
+		++::ScriptEngine.errCnt;
 	}
 	void OnWarning(const char *msg) override
 	{
 		DebugLogF("WARNING: %s", msg);
+		++::ScriptEngine.warnCnt;
 	}
 } DefaultErrorHandler;
 