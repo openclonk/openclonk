8e281103b0a2f607637166c080344981cabbf232
Sven Eberhardt <sven2@goldwipf.de>
2016-10-31 00:34:48 -0400

    Improve guidepost editor placement and rotation
    * Shows correct size in preview (by setting the scaling on the definition instead of in Construction)
    * Can be oversized
    * Avoid angles where all signs are facing parallel to the camera
    * Unfortunately, rotation doesn't work well with Mesh Transform (uses the wrong rotation center for the graphics)

--- planet/Decoration.ocd/Village.ocd/Guidepost2.ocd/Script.c
+++ planet/Decoration.ocd/Village.ocd/Guidepost2.ocd/Script.c
@@ -9,5 +9,14 @@ local Description="$Description$";
 
 protected func Construction()
 {
-	SetProperty("MeshTransformation", Trans_Mul(Trans_Rotate(RandomX(-180,180),0,10), Trans_Scale(130)));
+	// Pick an angle range that looks good (some angles show it from the side, which looks weird)
+	var angle;
+	if (!Random(2)) angle = -60 + Random(90); else angle = 120 + Random(90);
+	this.MeshTransformation = Trans_Mul(Trans_Rotate(angle,0,10), GetID().MeshTransformation);
+}
+
+public func Definition(def)
+{
+	// Model file is way too large
+	def.MeshTransformation = Trans_Scale(130);
 }
