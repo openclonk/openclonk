c5623aa7a214fa6d5f04df71c1052fb07e6d7ddd
Sven Eberhardt <sven2@goldwipf.de>
2016-11-19 21:03:03 -0500

    Fix crash on script error in DirectExec scripts

--- src/script/C4AulCompiler.cpp
+++ src/script/C4AulCompiler.cpp
@@ -214,7 +214,10 @@ class C4AulCompiler::CodegenAstVisitor : public ::aul::DefaultRecursiveVisitor
 	void HandleError(const C4AulError &e)
 	{
 		AddBCC(nullptr, AB_ERR, (intptr_t)::Strings.RegString(e.what()));
-		target_host->Engine->ErrorHandler->OnError(e.what());
+		if (target_host) // target_host may be nullptr for DirectExec scripts
+		{
+			target_host->Engine->ErrorHandler->OnError(e.what());
+		}
 	}
 
 	template<class T>
