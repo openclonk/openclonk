d03ac67807211e0589ab52e7a32f42ce82a04710
Maikel de Vries <mdt.maikel@gmail.com>
2016-12-24 12:02:10 +0100

    snow is not a liquid
    We can make it a liquid if we want it to be used in the foundry, or adapt the foundry. But that should be done properly.

--- planet/Objects.ocd/Items.ocd/Resources.ocd/Snow.ocd/Script.c
+++ planet/Objects.ocd/Items.ocd/Resources.ocd/Snow.ocd/Script.c
@@ -3,32 +3,30 @@
 protected func Construction()
 {
 	var graphic = Random(5);
-	if(graphic)
-		SetGraphics(Format("%d",graphic));
+	if (graphic)
+		SetGraphics(Format("%d", graphic));
 	AddTimer("Check", 30);
 }
 
 protected func Check()
 {
-	if(GetTemperature() > 0) Melt();
+	if (GetTemperature() > 0)
+		Melt();
 }
 
 private func Melt()
 {
-	CastPXS("Water",2,0);
+	CastPXS("Water", 2, 0);
 	DoCon(-2);
 }
 
 private func Hit()
 {
-	CastPXS("Snow", GetCon()*4,18);
+	CastPXS("Snow", GetCon() * 4, 18);
 	RemoveObject();
 }
 
-func IsLiquid() { return "Water"; }
-func GetLiquidAmount() { return GetCon(); }
-
-local Collectible = 1;
+local Collectible = true;
 local Name = "$Name$";
 local Description = "$Description$";
 local Plane = 450;
