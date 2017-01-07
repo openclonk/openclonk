db9451dcce45a857c56382dde97fe9744ba8b40f
Mark <markh4b@hotmail.com>
2017-01-07 02:10:00 +0100

    MoltenMonarch: Fix musket/blunderbuss

--- planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/King_MusketBullet.c
+++ planet/Arena.ocf/MoltenMonarch.ocs/System.ocg/King_MusketBullet.c
@@ -1,10 +1,33 @@
 #appendto LeadBullet
 
 local lowering;
-public func LessDamage() { lowering=6; SetClrModulation(RGB(255,128,0)); }
-public func ProjectileDamage() { return 10-lowering; }
+
+public func LessDamage()
+{
+	lowering = 6;
+	SetClrModulation(RGB(255,128,0));
+}
+
+public func ProjectileDamage() { return 10 - lowering; }
+
 public func TrailColor(int time)
 {
-	if(!lowering)	return RGBa(255,255,255,240*Max(0,FlightTime()-time)/FlightTime());
-	else	return RGBa(255-Min(time*5),255-Min(time*18,255),0,240*Max(0,FlightTime()-time)/FlightTime());
+	var r = 255, g = 255, b = 255;
+	if (lowering)
+	{
+		r = 255 - Min(time * 5);
+		g = 255 - Min(time * 18, 255);
+		b = 0;
+	}
+	return RGBa(255, 255, 255, 240 * Max(0, FlightTime() - time) / FlightTime());
+}
+
+public func Launch(object shooter, int angle, int dist, int speed, int offset_x, int offset_y, int prec_angle)
+{
+	if (speed > Blunderbuss.BulletSpeed[1])
+	{
+		LessDamage();
+	}
+
+	return _inherited(shooter, angle, dist, speed, offset_x, offset_y, prec_angle);
 }
