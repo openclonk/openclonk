9bb3dd8877886c0e3ed1ea41beb02252fb0cdb10
Nicolas Hake <isilkor@openclonk.org>
2017-03-05 11:56:45 +0100

    Win32: Don't try to locate Windows SDK
    MSVC already knows where the Windows SDK is located, so we don't have to
    replicate that logic in CMake (then get it wrong and link to an outdated
    one).

--- cmake/FindDbgHelp.cmake
+++ cmake/FindDbgHelp.cmake
@@ -1,6 +1,6 @@
 # OpenClonk, http://www.openclonk.org
 #
-# Copyright (c) 2011-2016, The OpenClonk Team and contributors
+# Copyright (c) 2011-2017, The OpenClonk Team and contributors
 #
 # Distributed under the terms of the ISC license; see accompanying file
 # "COPYING" for details.
@@ -18,12 +18,23 @@
 #  DBGHELP_LIBRARIES, the libraries needed to use DbgHelp.
 #  DBGHELP_FOUND, If false, do not try to use DbgHelp.
 
-find_path(DBGHELP_INCLUDE_DIR NAMES dbghelp.h PATH_SUFFIXES include)
-set(DBGHELP_NAMES ${DBGHELP_NAMES} dbghelp)
-find_library(DBGHELP_LIBRARY NAMES ${DBGHELP_NAMES})
+# MSVC knows how to find the Windows SDK on its own, and replicating that
+# in CMake is very tricky, especially with the SDK's layout changing
+# occasionally. So instead we'll just leave it to MSVC to do correctly.
+if(MSVC AND NOT DEFINED DBGHELP_INCLUDE_DIR AND NOT DEFINED DBGHELP_LIBRARY)
+	set(DBGHELP_INCLUDE_DIR "")
+	set(DBGHELP_LIBRARY "dbghelp")
+	set(DBGHELP_FOUND TRUE)
+endif()
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(DBGHELP DEFAULT_MSG DBGHELP_LIBRARY DBGHELP_INCLUDE_DIR)
+if(NOT DBGHELP_FOUND)
+	find_path(DBGHELP_INCLUDE_DIR NAMES dbghelp.h PATH_SUFFIXES include)
+	set(DBGHELP_NAMES ${DBGHELP_NAMES} dbghelp)
+	find_library(DBGHELP_LIBRARY NAMES ${DBGHELP_NAMES})
+
+	include(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(DBGHELP DEFAULT_MSG DBGHELP_LIBRARY DBGHELP_INCLUDE_DIR)
+endif()
 
 if(DBGHELP_FOUND)
 	set(DBGHELP_LIBRARIES ${DBGHELP_LIBRARY})
