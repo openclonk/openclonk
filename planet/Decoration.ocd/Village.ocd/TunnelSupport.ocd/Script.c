649c61cd2afd2b9c7bf5261a497f9cacdf1bc1a6
Clonkonaut <Armag3ddon@web.de>
2017-05-02 22:53:49 +0200

    New decoration object: tunnel support (graphics by Ringwaul, from a long time ago).
    Can be stretched using the Extend() function.

---
+++ planet/Decoration.ocd/Village.ocd/TunnelSupport.ocd/Script.c
@@ -0,0 +1,43 @@
+/**
+	Tunnel support
+	Purely for decoration
+
+	@authors: Clonkonaut, Ringwaul (Graphics)
+*/
+
+local extension = 0;
+
+func Construction()
+{
+	SetProperty("MeshTransformation", Trans_Rotate(RandomX(75, 105), 0,1,0));
+}
+
+// Stretch the support beams
+// 0 equals standard height (~30 pixels)
+// 100 is about 85 pixels
+public func Extend(int percentage)
+{
+	percentage = BoundBy(percentage, 0, 100);
+
+	extension = percentage;
+
+	percentage = 2500 * percentage / 100;
+	PlayAnimation("extend", 1, Anim_Const(percentage));
+
+	var height = 30 + (55 * percentage / 100);
+	SetShape(-15, -height/2, 30, height);
+}
+
+public func SaveScenarioObject(props)
+{
+	if (!inherited(props, ...)) 
+		return false;
+
+	props->AddCall("Extension", this, "Extend", extension);
+	return true;
+}
+
+/*-- Properties --*/
+
+local Name = "$Name$";
+local Plane = 701;
