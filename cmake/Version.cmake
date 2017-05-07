8fef67378d193e7d116c61b4b359a8788388d6fa
Tushar Maheshwari <tushar27192@gmail.com>
2017-05-07 15:11:28 +0530

    get year from CMake directly

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -23,13 +23,7 @@ git_get_changeset_id(C4REVISION)
 # Get year
 ############################################################################
 
-IF(CMAKE_HOST_UNIX)
-	EXECUTE_PROCESS(COMMAND "date" "+%Y" OUTPUT_VARIABLE DATE)
-ELSEIF(CMAKE_HOST_WIN32)
-	EXECUTE_PROCESS(COMMAND "cscript.exe" "//nologo" "${CMAKE_CURRENT_SOURCE_DIR}/tools/get_current_year.vbs" OUTPUT_VARIABLE DATE)
-ENDIF()
-STRING(REGEX REPLACE "(.+)\n" "\\1" YEARFIXED "${DATE}")
-SET(C4COPYRIGHT_YEAR ${YEARFIXED})
+STRING(TIMESTAMP C4COPYRIGHT_YEAR "%Y")
 
 ############################################################################
 # Build version strings
--- tools/get_current_year.vbs
+++
@@ -1,1 +0,0 @@
-Wscript.Echo DatePart("yyyy", Date()) 
