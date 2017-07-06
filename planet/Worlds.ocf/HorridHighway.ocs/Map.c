d34f0a689b99db6b2d3662d90863f5dc7c21672a
Maikel de Vries <mdt.maikel@gmail.com>
2017-07-06 10:18:27 +0200

    add new world: horrid highway

---
+++ planet/Worlds.ocf/HorridHighway.ocs/Map.c
@@ -0,0 +1,218 @@
+/**
+	Horrid Highway
+	Dynamic map with two separated sky islands.
+	
+	@author Maikel
+*/
+
+#include Library_Map
+
+
+// Called be the engine: draw the complete map here.
+protected func InitializeMap(proplist map)
+{
+	// Set the map size.
+	map->Resize(240 + 40 * SCENPAR_MapSize, 120);
+	
+	// At which height the highway will be constructed.
+	var highway_height = map.Hgt / 2;
+	
+	// Draw left island.
+	map->DrawLeftIsland(map, highway_height);
+	
+	// Draw right island.
+	map->DrawRightIsland(map, highway_height);
+	
+	// Draw middle island.
+	map->DrawMiddleIsland(map, highway_height);
+	
+	// Draw small islands.
+	map->DrawSmallIslands(map);
+	
+	// Return true to tell the engine a map has been successfully created.
+	return true;
+}
+
+public func DrawLeftIsland(proplist map, int highway_height)
+{
+	var width = 54;
+	var height = 64;
+	
+	var island1 = {Algo = MAPALGO_Rect, X = 0, Y = map.Hgt / 2 - height / 2, Wdt = width - 10, Hgt = height};
+	var island2 = {Algo = MAPALGO_Rect, X = 0, Y = highway_height, Wdt = width - 2, Hgt = height / 2 + map.Hgt / 2 - highway_height};
+	var island = {Algo = MAPALGO_Or, Op = [island1, island2]};
+	island = {Algo = MAPALGO_Or, Op = [island, {Algo = MAPALGO_Turbulence, Amplitude = 16, Scale = 12, Iterations = 4, Seed = Random(65536), Op = island}]};
+	island = {Algo = MAPALGO_Or, Op = [island, {Algo = MAPALGO_Turbulence, Amplitude = 16, Scale = 12, Iterations = 4, Seed = Random(65536), Op = island}]};
+	Draw("Earth-earth", island);
+	DrawMaterial("Earth-earth_root", island, 2, 16);
+	DrawMaterial("Earth-earth_spongy", island, 2, 16);
+	
+	DrawMaterial("Rock", island, 4, 10);
+	DrawMaterial("Ore", island, 4, 10);
+	DrawMaterial("Coal", island, 4, 10);
+	DrawMaterial("Firestone", island, 4, 10);
+	
+	var border = {Algo = MAPALGO_Border, Right = 4, Bottom = 4, Op = island};
+	Draw("Granite-granite", border);
+	DrawMaterial("Rock-rock", border, 3, 20);
+	DrawMaterial("Rock-rock_smooth", border, 3, 20);
+	
+	var top = {Algo = MAPALGO_Border, Top = 4, Op = island};
+	Draw("Earth-earth", top);
+	DrawMaterial("Earth-earth_root", top, 2, 16);
+	DrawMaterial("Earth-earth_spongy", top, 2, 16);
+	
+	var passage = {Algo = MAPALGO_Rect, X = 0, Y = highway_height - 7, Wdt = 2 * width, Hgt = 7};
+	passage = {Algo = MAPALGO_And, Op = [passage, island]};
+	Draw("Tunnel-tunnel", passage);
+	DrawMaterial("Tunnel-brickback", passage, 3, 20);
+	
+	var road = {Algo = MAPALGO_Rect, X = 0, Y = highway_height, Wdt = 2 * width, Hgt = 2};
+	road = {Algo = MAPALGO_And, Op = [road, island]};
+	Draw("Brick", road);
+	
+	var cave_up = {Algo = MAPALGO_Rect, X = 0, Y = highway_height - 20, Wdt = 30, Hgt = 10};
+	cave_up = {Algo = MAPALGO_Or, Op = [cave_up, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_up}]};
+	cave_up = {Algo = MAPALGO_And, Op = [cave_up, {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = map.Wdt, Hgt = highway_height - 10}]};
+	Draw("Tunnel-tunnel", cave_up);
+	DrawMaterial("Tunnel-brickback", cave_up, 3, 20);
+	var cave_up_road = {Algo = MAPALGO_And, Op = [cave_up, {Algo = MAPALGO_Rect, X = 0, Y = highway_height - 12, Wdt = map.Wdt, Hgt = 2}]};
+	Draw("Brick", cave_up_road);
+	var cave_up_mat = {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = 2, Hgt = map.Hgt};
+	cave_up_mat = {Algo = MAPALGO_Or, Op = [cave_up_mat, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_up_mat}]};
+	cave_up_mat = {Algo = MAPALGO_And, Op = [cave_up, cave_up_mat]};
+	Draw("Coal:Coal", cave_up_mat);
+	
+	var cave_down = {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 5, Wdt = 30, Hgt = 10};
+	cave_down = {Algo = MAPALGO_Or, Op = [cave_down, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_down}]};
+	cave_down = {Algo = MAPALGO_And, Op = [cave_down, {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 2, Wdt = map.Wdt, Hgt = 13}]};
+	Draw("Tunnel-tunnel", cave_down);
+	DrawMaterial("Tunnel-brickback", cave_down, 3, 20);
+	var cave_down_road = {Algo = MAPALGO_And, Op = [cave_down, {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 13, Wdt = map.Wdt, Hgt = 2}]};
+	Draw("Brick", cave_down_road);
+	var cave_down_mat = {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = 6, Hgt = map.Hgt};
+	cave_down_mat = {Algo = MAPALGO_Or, Op = [cave_down_mat, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_down_mat}]};
+	cave_down_mat = {Algo = MAPALGO_And, Op = [cave_down, cave_down_mat, {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 10, Wdt = map.Wdt, Hgt = 20}]};
+	Draw("Water:Water", cave_down_mat);
+	var cave_down_mat_border = {Algo = MAPALGO_Border, Right = 1, Bottom = 1, Op = cave_down_mat};
+	Draw("Everrock", cave_down_mat_border);
+	return;
+}
+
+public func DrawRightIsland(proplist map, int highway_height)
+{
+	var width = 54;
+	var height = 64;
+	
+	var island1 = {Algo = MAPALGO_Rect, X = map.Wdt - width + 10, Y = map.Hgt / 2 - height / 2, Wdt = width - 10, Hgt = height};
+	var island2 = {Algo = MAPALGO_Rect, X = map.Wdt - width, Y = highway_height, Wdt = width - 2, Hgt = height / 2 + map.Hgt / 2 - highway_height};
+	var island = {Algo = MAPALGO_Or, Op = [island1, island2]};
+	island = {Algo = MAPALGO_Or, Op = [island, {Algo = MAPALGO_Turbulence, Amplitude = 16, Scale = 12, Iterations = 4, Seed = Random(65536), Op = island}]};
+	island = {Algo = MAPALGO_Or, Op = [island, {Algo = MAPALGO_Turbulence, Amplitude = 16, Scale = 12, Iterations = 4, Seed = Random(65536), Op = island}]};
+	Draw("Earth-earth", island);
+	DrawMaterial("Earth-earth_root", island, 2, 16);
+	DrawMaterial("Earth-earth_spongy", island, 2, 16);
+	
+	DrawMaterial("Rock", island, 4, 10);
+	DrawMaterial("Ore", island, 4, 10);
+	DrawMaterial("Coal", island, 4, 10);
+	DrawMaterial("Firestone", island, 4, 10);
+	
+	var border = {Algo = MAPALGO_Border, Left = 4, Bottom = 4, Op = island};
+	Draw("Granite-granite", border);
+	DrawMaterial("Rock-rock", border, 3, 20);
+	DrawMaterial("Rock-rock_smooth", border, 3, 20);
+	
+	var top = {Algo = MAPALGO_Border, Top = 4, Op = island};
+	Draw("Earth-earth", top);
+	DrawMaterial("Earth-earth_root", top, 2, 16);
+	DrawMaterial("Earth-earth_spongy", top, 2, 16);
+	
+	var passage = {Algo = MAPALGO_Rect, X = map.Wdt - 2 * width, Y = highway_height - 7, Wdt = 2 * width, Hgt = 7};
+	passage = {Algo = MAPALGO_And, Op = [passage, island]};
+	Draw("Tunnel-tunnel", passage);
+	DrawMaterial("Tunnel-brickback", passage, 3, 20);
+	
+	var road = {Algo = MAPALGO_Rect, X = map.Wdt - 2 * width, Y = highway_height, Wdt = 2 * width, Hgt = 2};
+	road = {Algo = MAPALGO_And, Op = [road, island]};
+	Draw("Brick", road);
+	
+	var cave_up = {Algo = MAPALGO_Rect, X = map.Wdt - 30, Y = highway_height - 20, Wdt = 30, Hgt = 10};
+	cave_up = {Algo = MAPALGO_Or, Op = [cave_up, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_up}]};
+	cave_up = {Algo = MAPALGO_And, Op = [cave_up, {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = map.Wdt, Hgt = highway_height - 10}]};
+	Draw("Tunnel-tunnel", cave_up);
+	DrawMaterial("Tunnel-brickback", cave_up, 3, 20);
+	var cave_up_road = {Algo = MAPALGO_And, Op = [cave_up, {Algo = MAPALGO_Rect, X = 0, Y = highway_height - 12, Wdt = map.Wdt, Hgt = 2}]};
+	Draw("Brick", cave_up_road);
+	var cave_up_mat = {Algo = MAPALGO_Rect, X = map.Wdt - 2, Y = 0, Wdt = 2, Hgt = map.Hgt};
+	cave_up_mat = {Algo = MAPALGO_Or, Op = [cave_up_mat, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_up_mat}]};
+	cave_up_mat = {Algo = MAPALGO_And, Op = [cave_up, cave_up_mat]};
+	Draw("Ore:Ore", cave_up_mat);
+	
+	var cave_down = {Algo = MAPALGO_Rect, X = map.Wdt - 30, Y = highway_height + 5, Wdt = 30, Hgt = 10};
+	cave_down = {Algo = MAPALGO_Or, Op = [cave_down, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_down}]};
+	cave_down = {Algo = MAPALGO_And, Op = [cave_down, {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 2, Wdt = map.Wdt, Hgt = 13}]};
+	Draw("Tunnel-tunnel", cave_down);
+	DrawMaterial("Tunnel-brickback", cave_down, 3, 20);
+	var cave_down_road = {Algo = MAPALGO_And, Op = [cave_down, {Algo = MAPALGO_Rect, X = 0, Y = highway_height + 13, Wdt = map.Wdt, Hgt = 2}]};
+	Draw("Brick", cave_down_road);
+	var cave_down_mat = {Algo = MAPALGO_Rect, X = map.Wdt - 2, Y = 0, Wdt = 2, Hgt = map.Hgt};
+	cave_down_mat = {Algo = MAPALGO_Or, Op = [cave_down_mat, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 12, Iterations = 4, Seed = Random(65536), Op = cave_down_mat}]};
+	cave_down_mat = {Algo = MAPALGO_And, Op = [cave_down, cave_down_mat]};
+	Draw("Rock:Rock", cave_down_mat);
+	return;
+}
+
+public func DrawMiddleIsland(proplist map, int highway_height)
+{
+	var width = 22;
+	var height = 72;
+	
+	var island = {Algo = MAPALGO_Ellipsis, X = map.Wdt / 2, Y = map.Hgt / 2, Wdt = width / 2, Hgt = height / 2};
+	island = {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 8, Iterations = 2, Seed = Random(65536), Op = island};
+	Draw("Everrock", island);
+	
+	var passage = {Algo = MAPALGO_Rect, X = map.Wdt / 2 - 2 * width, Y = highway_height - 7, Wdt = 4 * width, Hgt = 7};
+	passage = {Algo = MAPALGO_And, Op = [passage, island]};
+	Draw("Tunnel", passage);
+	DrawMaterial("Tunnel-brickback", passage, 3, 20);
+	
+	var road = {Algo = MAPALGO_Rect, X = map.Wdt / 2 - 2 * width, Y = highway_height, Wdt = 4 * width, Hgt = 2};
+	road = {Algo = MAPALGO_And, Op = [road, island]};
+	Draw("Brick", road);
+	
+	var core = {Algo = MAPALGO_And, Op = [island, {Algo = MAPALGO_Not, Op = {Algo = MAPALGO_Or, Op = [passage, road]}}]};
+	var core_border = {Algo = MAPALGO_Border, Op = core, Left = 5, Right = 5, Top = 5};
+	core = {Algo = MAPALGO_And, Op = [core, {Algo = MAPALGO_Not, Op = core_border}]};
+	Draw("Gold", core);
+	var core_bottom = {Algo = MAPALGO_Rect, X = map.Wdt / 2 - 1, Y = map.Hgt / 2 + height / 2 - 12, Wdt = 2, Hgt = map.Hgt};
+	core_bottom = {Algo = MAPALGO_Or, Op = [core_bottom, {Algo = MAPALGO_Turbulence, Amplitude = 8, Scale = 8, Iterations = 2, Seed = Random(65536), Op = core_bottom}]};
+	core = {Algo = MAPALGO_And, Op = [island, core_bottom]};
+	Draw("Gold", core);
+	return;
+}
+
+public func DrawSmallIslands(proplist map)
+{
+	var width = 10;
+	var height = 14;
+
+	var x1 = 4 * map.Wdt / 11 + RandomX(-3, 3);
+	var x2 = 7 * map.Wdt / 11 + RandomX(-3, 3);
+	var y = 10 * map.Hgt / 11;
+	var island1 = {Algo = MAPALGO_Ellipsis, X = x1, Y = y, Wdt = width / 2, Hgt = height};
+	var island2 = {Algo = MAPALGO_Ellipsis, X = x2, Y = y, Wdt = width / 2, Hgt = height};
+	island1 = {Algo = MAPALGO_And, Op = [island1, {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = map.Wdt, Hgt = y}]};
+	island2 = {Algo = MAPALGO_And, Op = [island2, {Algo = MAPALGO_Rect, X = 0, Y = 0, Wdt = map.Wdt, Hgt = y}]};
+	island1 = {Algo = MAPALGO_Or, Op = [island1, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 10, Iterations = 3, Seed = Random(65536), Op = island1}]};
+	island2 = {Algo = MAPALGO_Or, Op = [island2, {Algo = MAPALGO_Turbulence, Amplitude = 12, Scale = 10, Iterations = 3, Seed = Random(65536), Op = island2}]};
+	
+	var islands = {Algo = MAPALGO_Or, Op = [island1, island2]};
+	var islands_border = {Algo = MAPALGO_Border, Left = 3, Right = 3, Top = 3, Op = islands};
+	var no_border = {Algo = MAPALGO_Not, Op = {Algo = MAPALGO_Or, Op = [{Algo = MAPALGO_Rect, X = x1 - 1, Y = y - 3, Wdt = 3, Hgt = map.Hgt}, {Algo = MAPALGO_Rect, X = x2 - 1, Y = y - 3, Wdt = 3, Hgt = map.Hgt}]}};
+	islands_border = {Algo = MAPALGO_And, Op = [islands_border, no_border]};
+	
+	Draw("Gold", islands);
+	Draw("Everrock", islands_border);
+	return;
+}
---
+++ planet/Worlds.ocf/HorridHighway.ocs/Script.c
@@ -0,0 +1,300 @@
+/**
+	Horrid Highway
+	Players need to completely excavate the sky island far away.
+		
+	@author Maikel
+*/
+
+
+// Whether the intro has been initialized.
+static intro_init;
+
+protected func Initialize()
+{
+	// Show wealth in HUD.
+	GUI_Controller->ShowWealth();
+	
+	// Goal: locomotive highway.
+	var goal = CreateObject(Goal_LocomotiveHighway);
+	goal.LocomotiveGoal = 8 * SCENPAR_Difficulty;
+	
+	// Rules: team account and buying at flagpole.
+	CreateObject(Rule_TeamAccount);
+	CreateObject(Rule_BuyAtFlagpole);
+	
+	// Allow for base respawns.
+	var relaunch_rule = GetRelaunchRule();
+	relaunch_rule->SetInventoryTransfer(false);
+	relaunch_rule->SetLastClonkRespawn(true);
+	relaunch_rule->SetFreeCrew(false);
+	relaunch_rule->SetAllowPlayerRestart(true);
+	relaunch_rule->SetBaseRespawn(true);
+	relaunch_rule->SetRespawnDelay(0);
+	
+	// Initialize different parts of the scenario.
+	InitEnvironment(SCENPAR_Difficulty);
+	InitVegetation();
+	InitAnimals(SCENPAR_Difficulty);
+	InitBridges();
+	InitLeftIsland();
+	InitMiddleIsland();
+	InitRightIsland();
+	InitDisasters(SCENPAR_Difficulty);
+	return;
+}
+
+protected func OnGoalsFulfilled()
+{
+	// Give the remaining players their achievement.
+	GainScenarioAchievement("Done", BoundBy(SCENPAR_Difficulty, 1, 3));
+	return false;
+}
+
+
+/*-- Player Initialization --*/
+
+protected func InitializePlayer(int plr)
+{
+	// Zoom range.
+	SetPlayerZoomByViewRange(plr, 1200, nil, PLRZOOM_LimitMax);
+	SetPlayerZoomByViewRange(plr, 500, nil, PLRZOOM_Direct | PLRZOOM_Set);
+	SetPlayerViewLock(plr, true);
+	
+	// Position and materials.
+	var i, crew;
+	for (i = 0; crew = GetCrew(plr, i); ++i)
+	{
+		crew->SetPosition(100, LandscapeHeight() / 2 - 10);
+		crew->CreateContents(Shovel);
+	}
+	
+	// Give the player its knowledge.
+	GivePlayerBasicKnowledge(plr);
+	GivePlayerPumpingKnowledge(plr);
+	GivePlayerFarmingKnowledge(plr);
+	GivePlayerWeaponryKnowledge(plr);
+	GivePlayerArtilleryKnowledge(plr);
+	GivePlayerAdvancedKnowledge(plr);
+	GivePlayerAirKnowledge(plr);
+	
+	// Give the player its base materials.
+	GivePlayerElementaryBaseMaterial(plr);
+	GivePlayerToolsBaseMaterial(plr);
+	GivePlayerSpecificBaseMaterial(plr, [[Dynamite, 20, 10]]);
+	
+	// Claim ownership of structures, last player who joins owns all the flags.
+	for (var structure in FindObjects(Find_Or(Find_Category(C4D_Structure), Find_Func("IsFlagpole"))))
+		structure->SetOwner(plr);
+		
+	// Ensure mimimum player wealth.
+	var add_wealth = Max(0, 75 - 25 * SCENPAR_Difficulty - GetWealth(plr));
+	DoWealth(plr, add_wealth);	
+		
+	// Initialize the intro sequence if not yet started.
+	if (!intro_init)
+	{
+		StartSequence("Intro", 0);
+		intro_init = true;
+	}
+	return;
+}
+
+
+/*-- Scenario Initialization --*/
+
+// Initializes environment and disasters.
+private func InitEnvironment(int difficulty)
+{
+	// Init time and have normal cycle.
+	Time->Init();
+
+	// Set a certain parallax.
+	SetSkyParallax(0, 20, 20);
+	
+	// Clouds and rain.
+	Cloud->Place(15);
+	Cloud->SetPrecipitation("Water", 60);
+	if (difficulty == 3)
+		Cloud->SetPrecipitation("Acid", 60);
+	return;
+}
+
+// Initializes grass, trees and in-earth objects.
+private func InitVegetation()
+{
+	// Grass on all islands.
+	Grass->Place(100);
+	
+	// Vegetation around all islands.
+	Flower->Place(20);
+	Mushroom->Place(20);
+	Fern->Place(12);
+	Branch->Place(30);
+	Trunk->Place(8);
+	Vine->Place(18, nil, {attach_material = Loc_Or(Loc_Material("Granite"), Loc_Material("Rock"), Loc_Material("Everrock"), Loc_Material("Gold"))});
+	Cotton->Place(8);
+	Wheat->Place(8);
+	
+	// Some objects in the earth.	
+	PlaceObjects(Rock, 40, "Earth");
+	PlaceObjects(Firestone, 40, "Earth");
+	PlaceObjects(Loam, 30, "Earth");
+
+	// Place some trees.
+	Tree_Deciduous->Place(20, Rectangle(0, 0, 600, LandscapeHeight()));
+	Tree_Deciduous->Place(20, Rectangle(LandscapeWidth() - 600, 0, 600, LandscapeHeight()));
+	Tree_Coniferous2->Place(12, Rectangle(0, 0, 600, LandscapeHeight()));
+	Tree_Coniferous2->Place(12, Rectangle(LandscapeWidth() - 600, 0, 600, LandscapeHeight()));
+	return;
+}
+
+// Initializes animals.
+private func InitAnimals(int difficulty)
+{
+	// Some fireflies attracted to trees.
+	Firefly->Place(6);
+	// Place some bats depending on difficulties.
+	Bat->Place(10 * difficulty**2, Rectangle(LandscapeWidth() - 600, 0, 600, LandscapeHeight()), {tunnel_only = true});
+	// Place zaps on higher difficulties.
+	if (difficulty >= 2)
+		Zaphive->Place(2 * difficulty);
+	return;
+}
+
+private func InitBridges()
+{
+	// Create four indestructible bridges to connect.
+	var height = LandscapeHeight() / 2 + 6;
+	var x4 = 4;
+	while (GetMaterial(x4, height) == Material("Brick"))
+		x4 += 8;
+	var x1 = x4;
+	while (GetMaterial(x4, height) != Material("Brick") || x4 < LandscapeWidth() / 4)
+		x4 += 8;
+	var x2 = x4;
+	while (GetMaterial(x4, height) == Material("Brick") || x4 < LandscapeWidth() / 2)
+		x4 += 8;
+	var x3 = x4;
+	while (GetMaterial(x4, height) != Material("Brick") || 3 * x4 < LandscapeWidth() / 4)
+		x4 += 8;
+	var bridge;
+	bridge = CreateObject(WoodenBridge, x1 + 20, height);
+	bridge->MakeInvincible();
+	bridge->SetClrModulation(RGB(80, 120, 200));
+	bridge = CreateObject(WoodenBridge, x2 - 20, height);
+	bridge->MakeInvincible();
+	bridge->SetClrModulation(RGB(80, 120, 200));
+	bridge = CreateObject(WoodenBridge, x3 + 20, height);
+	bridge->MakeInvincible();
+	bridge->SetClrModulation(RGB(80, 120, 200));
+	bridge = CreateObject(WoodenBridge, x4 - 20, height);
+	bridge->MakeInvincible();
+	bridge->SetClrModulation(RGB(80, 120, 200));
+	return;
+}
+
+private func InitLeftIsland()
+{
+	var switch = CreateObjectAbove(Switch, 20, LandscapeHeight() / 2);
+	var goal = FindObject(Find_ID(Goal_LocomotiveHighway));
+	switch->SetTarget(goal);
+	switch->SetSwitchDir(-1);
+	
+	var guidepost = CreateObjectAbove(EnvPack_Guidepost2, 40, LandscapeHeight() / 2);
+	guidepost->SetInscription("$MsgHorridHighwayEast$");
+	
+	var lorry = CreateObjectAbove(Lorry, 80, LandscapeHeight() / 2 - 2);
+	lorry->CreateContents(Shovel, 2);
+	lorry->CreateContents(Hammer, 2);
+	lorry->CreateContents(Axe, 2);
+	lorry->CreateContents(Barrel, 2);
+	
+	var elevator = CreateObjectAbove(Elevator, 180, 384);
+	elevator->CreateShaft(196);
+	CreateObjectAbove(Compensator, 230, 384);
+	
+	var workshop = CreateObjectAbove(ToolsWorkshop, 156, 480);
+	workshop->CreateContents(Wood, 8);
+	workshop->CreateContents(Metal, 8);
+	
+	var chemical_lab = CreateObjectAbove(ChemicalLab, 160, 584);
+	chemical_lab->CreateContents(Firestone, 8);
+	chemical_lab->CreateContents(Coal, 8);
+	CreateObjectAbove(Flagpole, 120, 584);
+	
+	var bridge = FindObject(Find_ID(WoodenBridge), Sort_Distance(0, LandscapeHeight() / 2));
+	bridge->CreateObjectAbove(WindGenerator, 0, -4);
+	bridge->CreateObjectAbove(Flagpole, -36, -4);
+	bridge->CreateObjectAbove(Catapult, 6, -6);
+	return;
+}
+
+private func InitMiddleIsland()
+{
+	var lorry = CreateObjectAbove(Lorry, LandscapeWidth() / 2 + RandomX(-20, 20), LandscapeHeight() / 2 - 2);
+	lorry->CreateContents(Firestone, 12);
+	lorry->CreateContents(Dynamite, 12);
+	lorry->CreateContents(DynamiteBox, 8);
+	lorry->CreateContents(PowderKeg, 8);
+	lorry->CreateContents(Pickaxe, 2);
+	return;
+}
+
+private func InitRightIsland()
+{
+	var guidepost = CreateObjectAbove(EnvPack_Guidepost2, LandscapeWidth() - 40, LandscapeHeight() / 2);
+	guidepost->SetInscription("$MsgHorridHighwayWest$");
+	
+	var elevator = CreateObjectAbove(Elevator, LandscapeWidth() - 180, 384);
+	elevator->CreateShaft(196);
+	
+	var foundry = CreateObjectAbove(Foundry, LandscapeWidth() - 100, 384);
+	foundry->CreateContents(Coal, 8);
+	
+	CreateObjectAbove(WoodenCabin, LandscapeWidth() - 100, 480);
+	return;
+}
+
+private func InitDisasters(int difficulty)
+{
+	// Lightning: clouds are already inited.
+	Cloud->SetLightning(3 * difficulty**2 + difficulty + 2);
+	// Rockfall: appears around the central sky island.
+	if (difficulty >= 2)
+	{
+		Rockfall->SetChance(10 * (difficulty - 1));
+		Rockfall->SetArea(Rectangle(LandscapeWidth() / 2 - 200, 0, 400, 20));
+		if (difficulty >= 3)
+			Rockfall->SetExplosiveness(true);
+	}
+	// Meteors: controlled by effect to happen between the main islands.
+	var meteor_chance = 15 * difficulty;
+	if (difficulty == 3)
+		meteor_chance = 60;
+	CreateEffect(FxControlMeteors, 100, 1, meteor_chance);
+	return;
+}
+
+static const FxControlMeteors = new Effect
+{
+	Construction = func(int chance)
+	{
+		this.chance = chance;
+		this.Interval = 10;
+		// Find spawn range according to outer bridges.
+		var bridge_left = FindObject(Find_ID(WoodenBridge), Sort_Distance(0, LandscapeHeight() / 2));
+		var bridge_right = FindObject(Find_ID(WoodenBridge), Sort_Distance(LandscapeWidth(), LandscapeHeight() / 2));
+		this.spawn_range = [bridge_left->GetX() + 132, bridge_right->GetX() - 132];
+		return FX_OK;
+	},
+	Timer = func(int time)
+	{
+		if (Random(100) >= 100 - this.chance)
+		{ 
+			var x = RandomX(this.spawn_range[0], this.spawn_range[1]);
+			LaunchMeteor(x, 0, RandomX(40, 60), RandomX(-15, 15), RandomX(40, 50));
+		}
+		return FX_OK;
+	}
+};
+
