1e63d088837628337beaf45dcc46e4bb74667022
David Dormagen <czapper@gmx.de>
2017-08-05 20:12:28 +0200

    C4GUI: allow modifying the message of a progress bar
    In case a bar stands for multiple downloading items, but should always show the currently being downloaded one.

--- src/gui/C4Gui.h
+++ src/gui/C4Gui.h
@@ -2368,6 +2368,7 @@ namespace C4GUI
 	{
 	protected:
 		ProgressBar *pBar; // progress bar component
+		Label *pLblMessage; // message label
 
 		const char *GetID() override { return "ProgressDialog"; }
 
@@ -2376,6 +2377,7 @@ namespace C4GUI
 
 		void SetProgress(int32_t iToProgress) { pBar->SetProgress(iToProgress); } // change progress
 		bool OnEnter() override { return false; } // don't close on Enter!
+		void SetMessage(const char *szMessage);
 	};
 
 
