e0662bc2066ff14c859419aa4362f067952c19df
Mark <markh4b@hotmail.com>
2017-08-10 22:43:09 +0200

    Fight for Gidl: Adjust statue position
    Made the statue not movable, so there is no need to make it stuck in the
    ground.
    Other scenarios do not need an adjustment, because they use
    CreateObjectAbove(), so the positions will be OK (tested with 3
    scenarios).

--- planet/Defense.ocf/FightForGidl.ocs/Objects.c
+++ planet/Defense.ocf/FightForGidl.ocs/Objects.c
@@ -12,8 +12,9 @@ func InitializeObjects()
 	
 	CreateObject(Rule_NoFriendlyFire, 0, 0);
 
-	g_statue = CreateObjectAbove(Idol, 632, 445);
+	g_statue = CreateObjectAbove(Idol, 632, 440);
 	g_statue.StaticSaveVar = "g_statue";
+	g_statue.Touchable = 0;
 
 	g_doorleft = CreateObjectAbove(StoneDoor, 495, 449);
 	g_doorleft->SetComDir(COMD_Down);
