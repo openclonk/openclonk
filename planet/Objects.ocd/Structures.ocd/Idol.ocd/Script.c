be9f477a16b3e0a423b1cb398768d38e6838dbe1
Mark <markh4b@hotmail.com>
2017-08-10 22:17:46 +0200

    Gold statue: New graphics
    Added a new mesh with 6 different poses for the gold statue.

--- planet/Objects.ocd/Structures.ocd/Idol.ocd/Script.c
+++ planet/Objects.ocd/Structures.ocd/Idol.ocd/Script.c
@@ -9,3 +9,59 @@ local Components = {GoldBar = 3};
 public func IsHammerBuildable() { return true; }
 
 public func NoConstructionFlip() { return true; }
+
+public func Initialize()
+{
+	SetAction("Default");
+	return _inherited(...);
+}
+
+local ActMap = {
+	Default = {
+		Prototype = Action,
+		Name = "Default",
+		NextAction = "Default",
+		Animation = "Pose01",
+		Procedure = DFA_NONE,
+	},
+
+	ItemRightLow = {
+		Prototype = Action,
+		Name = "ItemRightLow",
+		NextAction = "ItemRightLow",
+		Animation = "Pose02",
+		Procedure = DFA_NONE,
+	},
+	
+	ItemRightHigh = {
+		Prototype = Action,
+		Name = "ItemRightHigh",
+		NextAction = "ItemRightHigh",
+		Animation = "Pose03",
+		Procedure = DFA_NONE,
+	},
+	
+	ItemLeftLow = {
+		Prototype = Action,
+		Name = "ItemLeftLow",
+		NextAction = "ItemLeftLow",
+		Animation = "Pose04",
+		Procedure = DFA_NONE,
+	},
+	
+	ItemLeftHigh = {
+		Prototype = Action,
+		Name = "ItemLeftHigh",
+		NextAction = "ItemLeftHigh",
+		Animation = "Pose05",
+		Procedure = DFA_NONE,
+	},
+	
+	ItemCentral = {
+		Prototype = Action,
+		Name = "ItemCentral",
+		NextAction = "ItemCentral",
+		Animation = "Pose06",
+		Procedure = DFA_NONE,
+	},
+};
