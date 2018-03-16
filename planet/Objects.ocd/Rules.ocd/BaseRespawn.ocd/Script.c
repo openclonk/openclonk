5c6fadfb266d97b27904fd77f3f6e7f808bcb85e
Maikel de Vries <mdt.maikel@gmail.com>
2018-03-16 18:16:05 +0100

    warn about deprecated use of base respawn rule

--- planet/Objects.ocd/Rules.ocd/BaseRespawn.ocd/Script.c
+++ planet/Objects.ocd/Rules.ocd/BaseRespawn.ocd/Script.c
@@ -2,6 +2,7 @@
 
 protected func Construction()
 {
+	DebugLog("WARNING: Rule_BaseRespawn is deprecated and will be removed in version 9.0, use Rule_Relaunch instead.");
 	var relaunch_rule = GetRelaunchRule();
 	relaunch_rule->SetBaseRespawn(true);
 	relaunch_rule->SetFreeCrew(false);
