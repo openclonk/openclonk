e8cacb3c2fc7ea1331dc75f39f29a079404211f0
Mark <markh4b@hotmail.com>
2019-04-03 14:57:13 +0200

    Make var args visible

--- planet/System.ocg/Debug.c
+++ planet/System.ocg/Debug.c
@@ -11,8 +11,9 @@
 
  @par message The warning message, no need to include "WARNING" or similar, this is already included here.
               Accepts additional parameters as a normal log message would.
+ @par ... additional formatting parameters for the message.
  */
-global func Warning(string message)
+global func Warning(string message, ...)
 {
 	DebugLog("WARNING: %s", Format(message, ...));
 	LogCallStack();
