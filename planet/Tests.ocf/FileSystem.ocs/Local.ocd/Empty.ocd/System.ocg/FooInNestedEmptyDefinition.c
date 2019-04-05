60772209985b1657f0cfb7a3b123b2c1005c7442
Mark <markh4b@hotmail.com>
2019-04-05 09:18:44 +0200

    Added test scenario for #1251

---
+++ planet/Tests.ocf/FileSystem.ocs/Local.ocd/Empty.ocd/System.ocg/FooInNestedEmptyDefinition.c
@@ -0,0 +1,9 @@
+
+/**
+	Dummy function, only exists to see if it gets loaded.
+ */
+global func FooInNestedEmptyDefinition()
+{
+	Log("FooInNestedEmptyDefinition was loaded");
+	return true;
+}
---
+++ planet/Tests.ocf/FileSystem.ocs/System.ocg/Foo001.c
@@ -0,0 +1,9 @@
+
+/**
+	Dummy function, only exists to see if it gets loaded.
+ */
+global func Foo001()
+{
+	Log("Foo001 was loaded");
+	return true;
+}
