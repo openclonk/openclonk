63b045288cca96c6c2201a0ab281314b5b4acb2a
Philip Holzmann <philip.holzmann@tum.de>
2019-05-03 19:01:55 +0200

    Proper burnt graphics and swinging animation for tree deciduous4. Rotate trees correctly when burnt.

--- planet/Decoration.ocd/Vegetation.ocd/Deciduous4.ocd/Script.c
+++ planet/Decoration.ocd/Vegetation.ocd/Deciduous4.ocd/Script.c
@@ -15,6 +15,27 @@ public func GetTreetopPosition(pos)
 	return Shape->Rectangle(-30,-30, 60,80)->GetRandomPoint(pos);
 }
 
+public func Construction() {
+	var ret = _inherited(...);
+	StartSwing();
+	return ret;
+}
+
+public func ChopDown() {
+	StopSwing();
+	return _inherited(...);
+}
+
+public func StartSwing() {
+	var anim_length = GetAnimationLength("swing");
+	PlayAnimation("swing", 1, Anim_Linear(Random(anim_length), 0, anim_length, 500, ANIM_Loop));
+}
+
+public func StopSwing() {
+	var anim_length = GetAnimationLength("swing");
+	PlayAnimation("swing", 1, Anim_Const(GetAnimationPosition(GetRootAnimation(1)), 0, anim_length, 500, ANIM_Loop));
+}
+
 local Name = "$Name$";
 local Touchable = 0;
 local BlastIncinerate = 2;
