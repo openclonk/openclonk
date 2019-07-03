ec500c5101f4a2d8b1c0fb9ad93146f160df42ce
Mark <markh4b@hotmail.com>
2019-07-03 19:11:58 +0200

    Code style

--- planet/System.ocg/Mesh.c
+++ planet/System.ocg/Mesh.c
@@ -14,18 +14,30 @@ global func SetMeshTransformation(array transformation, int layer)
 
 	var mesh_transformation_list = GetEffect(FxLayeredMeshTransformation.Name, this) ?? CreateEffect(FxLayeredMeshTransformation, 1);
 
-	if(!mesh_transformation_list.Layers) mesh_transformation_list.Layers = [];
-	if(GetLength(mesh_transformation_list.Layers) < layer)
-		SetLength(mesh_transformation_list.Layers, layer+1);
+	if (!mesh_transformation_list.Layers)
+	{
+		mesh_transformation_list.Layers = [];
+	}
+	if (GetLength(mesh_transformation_list.Layers) < layer)
+	{
+		SetLength(mesh_transformation_list.Layers, layer + 1);
+	}
 	mesh_transformation_list.Layers[layer] = transformation;
 	var all_transformations = nil;
-	for(var trans in mesh_transformation_list.Layers)
+	for (var trans in mesh_transformation_list.Layers)
 	{
-		if(!trans) continue;
-		if(all_transformations)
+		if (!trans)
+		{
+			continue;
+		}
+		if (all_transformations)
+		{
 			all_transformations = Trans_Mul(trans, all_transformations);
+		}
 		else
+		{
 			all_transformations = trans;
+		}
 	}
 	SetProperty("MeshTransformation", all_transformations);
 }
