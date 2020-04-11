openclonk/openclonk

commit 102047f5379ea365f1aa08540eaba801010e701b

Implement IPv6 support to C4NetIO

src/network/C4Network2.cpp
@@ -247,7 +247,10 @@ C4Network2::InitResult C4Network2::InitClient(const C4Network2Reference &Ref, bo
 		// copy addresses
 		C4Network2Address Addrs[C4ClientMaxAddr];
 		for (int i = 0; i < Ref.getAddrCnt(); i++)
+		{
 			Addrs[i] = Ref.getAddr(i);
+			Addrs[i].getAddr().SetScopeId(Ref.GetSourceAddress().GetScopeId());
+		}
 		// Try to connect to host
 		if (InitClient(Addrs, Ref.getAddrCnt(), HostCore, Password.getData()) == IR_Fatal)
 			return IR_Fatal;
@@ -1043,18 +1046,16 @@ void C4Network2::DrawStatus(C4TargetFacet &cgo)
 		// connections
 		if (pClient->isConnected())
 		{
-			Stat.AppendFormat( "|   Connections: %s: %s (%s:%d p%d l%d)",
+			Stat.AppendFormat( "|   Connections: %s: %s (%s p%d l%d)",
 			                   pClient->getMsgConn() == pClient->getDataConn() ? "Msg/Data" : "Msg",
 			                   NetIO.getNetIOName(pClient->getMsgConn()->getNetClass()),
-			                   inet_ntoa(pClient->getMsgConn()->getPeerAddr().sin_addr),
-			                   htons(pClient->getMsgConn()->getPeerAddr().sin_port),
+							   pClient->getMsgConn()->getPeerAddr().ToString().getData(),
 			                   pClient->getMsgConn()->getPingTime(),
 			                   pClient->getMsgConn()->getPacketLoss());
 			if (pClient->getMsgConn() != pClient->getDataConn())
 				Stat.AppendFormat( ", Data: %s (%s:%d p%d l%d)",
 				                   NetIO.getNetIOName(pClient->getDataConn()->getNetClass()),
-				                   inet_ntoa(pClient->getDataConn()->getPeerAddr().sin_addr),
-				                   htons(pClient->getDataConn()->getPeerAddr().sin_port),
+								   pClient->getDataConn()->getPeerAddr().ToString().getData(),
 				                   pClient->getDataConn()->getPingTime(),
 				                   pClient->getDataConn()->getPacketLoss());
 		}
@@ -1183,7 +1184,7 @@ void C4Network2::HandleConn(const C4PacketConn &Pkt, C4Network2IOConnection *pCo
 	else
 	{
 		// log & close
-		LogSilentF("Network: connection by %s (%s:%d) blocked: %s", CCore.getName(), inet_ntoa(pConn->getPeerAddr().sin_addr), htons(pConn->getPeerAddr().sin_port), reply.getData());
+		LogSilentF("Network: connection by %s (%s) blocked: %s", CCore.getName(), pConn->getPeerAddr().ToString().getData(), reply.getData());
 		pConn->Close();
 	}
 }
@@ -1198,7 +1199,7 @@ bool C4Network2::CheckConn(const C4ClientCore &CCore, C4Network2IOConnection *pC
 	if (CCore.getDiffLevel(pClient->getCore()) > C4ClientCoreDL_IDMatch)
 		{ *szReply = "wrong client core"; return false; }
 	// check address
-	if (pClient->isConnected() && pClient->getMsgConn()->getPeerAddr().sin_addr.s_addr != pConn->getPeerAddr().sin_addr.s_addr)
+	if (pClient->isConnected() && pClient->getMsgConn()->getPeerAddr() != pConn->getPeerAddr())
 		{ *szReply = "wrong address"; return false; }
 	// accept
 	return true;
@@ -1277,7 +1278,7 @@ void C4Network2::HandleConnRe(const C4PacketConnRe &Pkt, C4Network2IOConnection 
 		// wrong password?
 		fWrongPassword = Pkt.isPasswordWrong();
 		// show message
-		LogSilentF("Network: connection to %s (%s:%d) refused: %s", pClient->getName(), inet_ntoa(pConn->getPeerAddr().sin_addr), htons(pConn->getPeerAddr().sin_port), Pkt.getMsg());
+		LogSilentF("Network: connection to %s (%s) refused: %s", pClient->getName(), pConn->getPeerAddr().ToString().getData(), Pkt.getMsg());
 		// close connection
 		pConn->Close();
 		return;
@@ -1302,7 +1303,7 @@ void C4Network2::HandleConnRe(const C4PacketConnRe &Pkt, C4Network2IOConnection 
 	if (pConn->getNetClass() == NetIO.DataIO()) pClient->SetDataConn(pConn);
 
 	// add peer connect address to client address list
-	if (pConn->getConnectAddr().sin_addr.s_addr)
+	if (!pConn->getConnectAddr().IsNull())
 	{
 		C4Network2Address Addr(pConn->getConnectAddr(), pConn->getProtocol());
 		pClient->AddAddr(Addr, Status.getState() != GS_Init);
@@ -1432,8 +1433,8 @@ void C4Network2::HandleJoinData(const C4PacketJoinData &rPkt)
 void C4Network2::OnConnect(C4Network2Client *pClient, C4Network2IOConnection *pConn, const char *szMsg, bool fFirstConnection)
 {
 	// log
-	LogSilentF("Network: %s %s connected (%s:%d/%s) (%s)", pClient->isHost() ? "host" : "client",
-	           pClient->getName(), inet_ntoa(pConn->getPeerAddr().sin_addr), htons(pConn->getPeerAddr().sin_port),
+	LogSilentF("Network: %s %s connected (%s/%s) (%s)", pClient->isHost() ? "host" : "client",
+	           pClient->getName(), pConn->getPeerAddr().ToString().getData(),
 	           NetIO.getNetIOName(pConn->getNetClass()), szMsg ? szMsg : "");
 
 	// first connection for this peer? call special handler
@@ -1442,8 +1443,8 @@ void C4Network2::OnConnect(C4Network2Client *pClient, C4Network2IOConnection *pC
 
 void C4Network2::OnConnectFail(C4Network2IOConnection *pConn)
 {
-	LogSilentF("Network: %s connection to %s:%d failed!", NetIO.getNetIOName(pConn->getNetClass()),
-	           inet_ntoa(pConn->getPeerAddr().sin_addr), htons(pConn->getPeerAddr().sin_port));
+	LogSilentF("Network: %s connection to %s failed!", NetIO.getNetIOName(pConn->getNetClass()),
+	           pConn->getPeerAddr().ToString().getData());
 
 	// maybe client connection failure
 	// (happens if the connection is not fully accepted and the client disconnects.
@@ -1455,8 +1456,8 @@ void C4Network2::OnConnectFail(C4Network2IOConnection *pConn)
 
 void C4Network2::OnDisconnect(C4Network2Client *pClient, C4Network2IOConnection *pConn)
 {
-	LogSilentF("Network: %s connection to %s (%s:%d) lost!", NetIO.getNetIOName(pConn->getNetClass()),
-	           pClient->getName(), inet_ntoa(pConn->getPeerAddr().sin_addr), htons(pConn->getPeerAddr().sin_port));
+	LogSilentF("Network: %s connection to %s (%s) lost!", NetIO.getNetIOName(pConn->getNetClass()),
+	           pClient->getName(), pConn->getPeerAddr().ToString().getData());
 
 	// connection lost?
 	if (!pClient->isConnected())

CMakeLists.txt
@@ -1248,6 +1248,7 @@ target_link_libraries(netpuncher
 )
 
 if(WIN32)
+	target_link_libraries(libmisc iphlpapi)
 	target_link_libraries(openclonk ws2_32)
 	target_link_libraries(openclonk-server ws2_32)
 	target_link_libraries(c4group ws2_32)

src/gui/C4StartupNetDlg.cpp
@@ -920,8 +920,7 @@ void C4StartupNetDlg::UpdateList(bool fGotReference)
 	C4NetIO::addr_t Discover;
 	while (DiscoverClient.PopDiscover(Discover))
 	{
-		StdStrBuf Address;
-		Address.Format("%s:%d", inet_ntoa(Discover.sin_addr), htons(Discover.sin_port));
+		StdStrBuf Address(Discover.ToString());
 		AddReferenceQuery(Address.getData(), C4StartupNetListEntry::NRQT_GameDiscovery);
 	}
 

src/netpuncher/C4PuncherHash.h
@@ -60,10 +60,25 @@ namespace std {
 	};
 
 	template<>
-	struct hash<::sockaddr_in> {
-		size_t operator()(const ::sockaddr_in& addr) const {
-			auto unpack = make_tuple(addr.sin_family, addr.sin_addr.s_addr, addr.sin_port);
-			return hash<decltype(unpack)>()(unpack);
+	struct hash<C4NetIO::addr_t> {
+		size_t operator()(const C4NetIO::addr_t& addr) const {
+			switch (addr.GetFamily())
+			{
+			case C4NetIO::HostAddress::IPv4:
+			{
+				sockaddr_in v4 = addr;
+				auto unpack = make_tuple(v4.sin_family, v4.sin_addr.s_addr, v4.sin_port);
+				return hash<decltype(unpack)>()(unpack);
+			}
+			case C4NetIO::HostAddress::IPv6:
+			{
+				sockaddr_in6 v6 = addr;
+				auto unpack = make_tuple(v6.sin6_family, v6.sin6_port, v6.sin6_flowinfo, std::string((char*) v6.sin6_addr.s6_addr, 16), v6.sin6_scope_id);
+				return hash<decltype(unpack)>()(unpack);
+			}
+			default:
+				return 0;
+			}
 		}
 	};
 }

src/netpuncher/C4PuncherPacket.cpp
@@ -47,9 +47,7 @@ C4NetpuncherPacketCReq::C4NetpuncherPacketCReq(const C4NetIOPacket& rpack) {
 	C4Network2Address parse_addr;
 	CompileFromBuf<StdCompilerBinRead>(parse_addr, rpack.getPBuf());
 	if (parse_addr.getProtocol() != P_UDP) throw P_UDP;
-	addr.sin_addr.s_addr = parse_addr.getAddr().sin_addr.s_addr,
-	addr.sin_family      = parse_addr.getAddr().sin_family,
-	addr.sin_port        = parse_addr.getAddr().sin_port;
+	addr = parse_addr.getAddr();
 }
 
 StdBuf C4NetpuncherPacketCReq::PackInto() const {

src/netpuncher/main.cpp
@@ -48,7 +48,7 @@ private:
 		peer_ids.emplace(AddrPeer, nid);
 		peer_addrs.emplace(nid, AddrPeer);
 		Send(C4NetpuncherPacketAssID(nid).PackTo(AddrPeer));
-		printf("Punched %s:%d... #%u\n", inet_ntoa(AddrPeer.sin_addr), htons(AddrPeer.sin_port), nid);
+		printf("Punched %s... #%u\n", AddrPeer.ToString().getData(), nid);
 		return true;
 	}
 	virtual void OnPacket(const class C4NetIOPacket &rPacket, C4NetIO *pNetIO) {
@@ -63,12 +63,12 @@ private:
 	virtual void OnDisconn(const addr_t &AddrPeer, C4NetIO *pNetIO, const char *szReason) {
 		auto it = peer_ids.find(AddrPeer);
 		if (it == peer_ids.end()) {
-			printf("ERROR: closing connection for %s:%d: (%s) but no connection is known\n", inet_ntoa(AddrPeer.sin_addr), htons(AddrPeer.sin_port), szReason);
+			printf("ERROR: closing connection for %s: (%s) but no connection is known\n", AddrPeer.ToString().getData(), szReason);
 			return;
 		}
 		peer_addrs.erase(it->second);
 		peer_ids.erase(it);
-		printf("Stopped punching %s:%d: %s...\n", inet_ntoa(AddrPeer.sin_addr), htons(AddrPeer.sin_port), szReason);
+		printf("Stopped punching %s: %s...\n", AddrPeer.ToString().getData(), szReason);
 	};
 } Puncher;
 

src/network/C4NetIO.h
@@ -65,9 +65,145 @@ public:
 
 	// *** constants / types
 	static const int TO_INF; // = -1;
-	static const uint16_t P_NONE; // = -1
 
-	typedef sockaddr_in addr_t;
+	struct HostAddress
+	{
+		enum AddressFamily
+		{
+			IPv6 = AF_INET6,
+			IPv4 = AF_INET,
+			UnknownFamily = 0
+		};
+		enum SpecialAddress
+		{
+			Loopback,	// IPv6 localhost (::1)
+			Any,	// IPv6 any-address (::)
+			AnyIPv4	// IPv4 any-address (0.0.0.0)
+		};
+
+		enum ToStringFlags
+		{
+			TSF_SkipZoneId = 1,
+			TSF_SkipPort = 2
+		};
+		
+		HostAddress() { Clear(); }
+		HostAddress(const HostAddress &other) { SetHost(other); }
+		HostAddress(SpecialAddress addr) { SetHost(addr); }
+		explicit HostAddress(uint32_t addr) { SetHost(addr); }
+		HostAddress(const StdStrBuf &addr) { SetHost(addr); }
+
+		AddressFamily GetFamily() const;
+		
+		void SetScopeId(int scopeId);
+		int GetScopeId() const;
+
+		void Clear();
+		void SetHost(const sockaddr *addr);
+		void SetHost(const HostAddress &host);
+		void SetHost(SpecialAddress host);
+		void SetHost(const StdStrBuf &host);
+		void SetHost(uint32_t host);
+
+		C4NetIO::HostAddress AsIPv6() const; // convert an IPv4 address to an IPv6-mapped IPv4 address
+
+		// General categories
+		bool IsNull() const;
+		bool IsMulticast() const;
+		bool IsLoopback() const;
+//		bool IsBroadcast() const;
+
+		StdStrBuf ToString(int flags = 0) const;
+
+		bool operator ==(const HostAddress &rhs) const;
+		bool operator !=(const HostAddress &rhs) const { return !(*this == rhs); }
+
+	protected:
+		// data
+		union
+		{
+			sockaddr gen;
+			sockaddr_in v4;
+			sockaddr_in6 v6;
+		};
+	};
+
+	struct EndpointAddress : public HostAddress	// Host and port
+	{
+		static const uint16_t IPPORT_NONE = 0;
+
+		EndpointAddress() { Clear(); }
+		EndpointAddress(const EndpointAddress &other) : HostAddress() { SetAddress(other); }
+		EndpointAddress(const HostAddress &host, uint16_t port = IPPORT_NONE) : HostAddress(host) { SetPort(port); }
+		EndpointAddress(HostAddress::SpecialAddress addr, uint16_t port = IPPORT_NONE) : HostAddress(addr) { SetPort(port); }
+		explicit EndpointAddress(const StdStrBuf &addr) { SetAddress(addr); }
+
+		StdStrBuf ToString(int flags = 0) const;
+
+		void Clear();
+
+		void SetAddress(const sockaddr *addr);
+		void SetAddress(const EndpointAddress &other);
+		void SetAddress(HostAddress::SpecialAddress addr, uint16_t port = IPPORT_NONE);
+		void SetAddress(const HostAddress &host, uint16_t port = IPPORT_NONE);
+		void SetAddress(const StdStrBuf &addr);
+
+		HostAddress GetHost() const { return *this; }	// HostAddress copy ctor slices off port information
+		EndpointAddress AsIPv6() const; // convert an IPv4 address to an IPv6-mapped IPv4 address
+		
+		void SetPort(uint16_t port);
+		uint16_t GetPort() const;
+
+		bool IsNull() const;
+		bool IsNullHost() const { return HostAddress::IsNull(); }
+
+		// Pointer wrapper to be able to implicitly convert to sockaddr*
+		class EndpointAddressPtr;
+		const EndpointAddressPtr operator &() const;
+		EndpointAddressPtr operator &();
+		class EndpointAddressPtr
+		{
+			EndpointAddress * const p;
+			friend EndpointAddressPtr EndpointAddress::operator &();
+			friend const EndpointAddressPtr EndpointAddress::operator &() const;
+			EndpointAddressPtr(EndpointAddress *p) : p(p) {}
+		public:
+			const EndpointAddress &operator *() const { return *p; }
+			EndpointAddress &operator *() { return *p; }
+
+			const EndpointAddress &operator ->() const { return *p; }
+			EndpointAddress &operator ->() { return *p; }
+
+			operator const EndpointAddress*() const { return p; }
+			operator EndpointAddress*() { return p; }
+
+			operator const sockaddr*() const { return &p->gen; }
+			operator sockaddr*() { return &p->gen; }
+
+			operator const sockaddr_in*() const { return &p->v4; }
+			operator sockaddr_in*() { return &p->v4; }
+
+			operator const sockaddr_in6*() const { return &p->v6; }
+			operator sockaddr_in6*() { return &p->v6; }
+		};
+
+		bool operator ==(const EndpointAddress &rhs) const;
+		bool operator !=(const EndpointAddress &rhs) const { return !(*this == rhs); }
+
+		// conversions
+		operator sockaddr() const { return gen; }
+/*		operator sockaddr_in() const { assert(gen.sa_family == AF_INET); return v4; }
+		operator sockaddr_in6() const { assert(gen.sa_family == AF_INET6); return v6; }*/
+
+		// StdCompiler
+		void CompileFunc(StdCompiler *comp);
+
+	private:
+		bool SetAddressByString(const StdStrBuf &address, short family);
+
+		friend class EndpointAddressPtr;
+	};
+	typedef EndpointAddress addr_t;
 
 	// callback class
 	class CBClass
@@ -108,10 +244,8 @@ public:
 	// *** interface
 
 	// * not multithreading safe
-	virtual bool Init(uint16_t iPort = P_NONE) = 0;
-	virtual bool InitBroadcast(addr_t *pBroadcastAddr) = 0;
+	virtual bool Init(uint16_t iPort = addr_t::IPPORT_NONE) = 0;
 	virtual bool Close() = 0;
-	virtual bool CloseBroadcast() = 0;
 
 	virtual bool Execute(int iTimeout = -1, pollfd * = 0) = 0; // (for StdSchedulerProc)
 	virtual bool IsNotify() { return true; }
@@ -128,6 +262,9 @@ public:
 	virtual bool GetStatistic(int *pBroadcastRate) = 0;
 	virtual bool GetConnStatistic(const addr_t &addr, int *pIRate, int *pORate, int *pLoss) = 0;
 	virtual void ClearStatistic() = 0;
+
+protected:
+//	virtual SOCKET CreateSocket() = 0;
 
 	// *** errors
 protected:
@@ -193,7 +330,7 @@ public:
 	// *** interface
 
 	// * not multithreading safe
-	virtual bool Init(uint16_t iPort = P_NONE);
+	virtual bool Init(uint16_t iPort = addr_t::IPPORT_NONE);
 	virtual bool InitBroadcast(addr_t *pBroadcastAddr);
 	virtual bool Close();
 	virtual bool CloseBroadcast();
@@ -352,7 +489,7 @@ public:
 	C4NetIOSimpleUDP();
 	virtual ~C4NetIOSimpleUDP();
 
-	virtual bool Init(uint16_t iPort = P_NONE);
+	virtual bool Init(uint16_t iPort = addr_t::IPPORT_NONE);
 	virtual bool InitBroadcast(addr_t *pBroadcastAddr);
 	virtual bool Close();
 	virtual bool CloseBroadcast();
@@ -440,7 +577,7 @@ public:
 
 	// *** interface
 
-	virtual bool Init(uint16_t iPort = P_NONE);
+	virtual bool Init(uint16_t iPort = addr_t::IPPORT_NONE);
 	virtual bool InitBroadcast(addr_t *pBroadcastAddr);
 	virtual bool Close();
 	virtual bool CloseBroadcast();
@@ -807,15 +944,6 @@ private:
 };
 
 // helpers
-inline bool AddrEqual(const C4NetIO::addr_t addr1, const C4NetIO::addr_t addr2)
-{
-	return addr1.sin_addr.s_addr == addr2.sin_addr.s_addr &&
-	       addr1.sin_family      == addr2.sin_family      &&
-	       addr1.sin_port        == addr2.sin_port;
-}
-inline bool operator == (const C4NetIO::addr_t addr1, const C4NetIO::addr_t addr2) { return AddrEqual(addr1, addr2); }
-inline bool operator != (const C4NetIO::addr_t addr1, const C4NetIO::addr_t addr2) { return !AddrEqual(addr1, addr2); }
-
 // there seems to be no standard way to get these numbers, so let's do it the dirty way...
 inline uint8_t &in_addr_b(in_addr &addr, int i)
 {
@@ -829,19 +957,6 @@ inline void CompileFunc(in_addr &ip, StdCompiler *pComp)
 	pComp->Value(in_addr_b(ip, 1)); pComp->Separator(StdCompiler::SEP_PART);
 	pComp->Value(in_addr_b(ip, 2)); pComp->Separator(StdCompiler::SEP_PART);
 	pComp->Value(in_addr_b(ip, 3));
-}
-
-inline void CompileFunc(C4NetIO::addr_t &addr, StdCompiler *pComp)
-{
-	pComp->Value(addr.sin_addr); pComp->Separator(StdCompiler::SEP_PART2);
-	uint16_t iPort = htons(addr.sin_port);
-	pComp->Value(iPort);
-	addr.sin_port = htons(iPort);
-	if (pComp->isCompiler())
-	{
-		addr.sin_family = AF_INET;
-		ZeroMem(addr.sin_zero, sizeof(addr.sin_zero));
-	}
 }
 
 #ifdef HAVE_WINSOCK

commit 52de219c1e084c8d99df1e149a9c928b384a5561

Fix a format string that did not match its arguments.
This was broken in 102047f (Feb 2013).

src/network/C4Network2.cpp
@@ -1177,7 +1177,7 @@ void C4Network2::DrawStatus(C4TargetFacet &cgo)
 			                   pClient->getMsgConn()->getPingTime(),
 			                   pClient->getMsgConn()->getPacketLoss());
 			if (pClient->getMsgConn() != pClient->getDataConn())
-				Stat.AppendFormat( ", Data: %s (%s:%d p%d l%d)",
+				Stat.AppendFormat( ", Data: %s (%s p%d l%d)",
 				                   NetIO.getNetIOName(pClient->getDataConn()->getNetClass()),
 								   pClient->getDataConn()->getPeerAddr().ToString().getData(),
 				                   pClient->getDataConn()->getPingTime(),