27d9a791812aca7b17aae26997db3347fdde547b
Julius Michaelis <gitter@liftm.de>
2020-05-05 18:42:47 +0900

    license-to-source: Pretend that unicode doesn't exist and hope for the best

--- tools/generate_license_headers.cpp
+++ tools/generate_license_headers.cpp
@@ -9,7 +9,7 @@
 #include <locale>
 
 // iswalnum will backstab
-static const std::wstring SAFE_CHARS = L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-',.() ";
+static const std::string SAFE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-',.() ";
 
 int main(int argc, char ** argv) {
 	if (argc < 4 || argc % 2 != 0) {
@@ -20,22 +20,21 @@ int main(int argc, char ** argv) {
 	for (int i = 2; i < argc; i += 2)
 		inputs.push_back(std::make_pair(argv[i], argv[i+1]));
 
-	std::wofstream output(argv[1]);
+	std::ofstream output(argv[1]);
 	output << "#include<C4Licenses.h>" << std::endl
 		   << "const std::vector<OCLicenseInfo> OCLicenses {" << std::endl;
 
 	for(const auto& input : inputs) {
 		output << std::endl << "\t{ \"" << input.first << "\", \"" << input.second << "\", std::string()" << std::endl;
-		std::wifstream input_file(input.first);
-		input_file.imbue(std::locale("C.UTF-8"));
-		std::wstring line;
+		std::ifstream input_file(input.first);
+		std::string line;
 		while (std::getline(input_file, line)) {
-			std::wstringstream out;
+			std::stringstream out;
 			for (const auto c : line) {
-				if (SAFE_CHARS.find(c) != std::wstring::npos)
+				if (SAFE_CHARS.find(c) != std::string::npos)
 					out << c;
 				else
-					out << "\\U" << std::setfill(L'0') << std::setw(8) << std::right << std::hex << +c;
+					out << "\\" << std::setfill('0') << std::setw(3) << std::right << std::oct << +uint8_t(c);
 			}
 			output << "\t\t+ std::string(\"" << out.str() << "\\n\")" << std::endl;
 		}
