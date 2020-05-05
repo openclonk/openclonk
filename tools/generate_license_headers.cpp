c9266b7a47159355043c26c1387de446e2e9c0fd
Julius Michaelis <gitter@liftm.de>
2020-05-05 16:42:23 +0900

    license-to-source: It seems MSVC on appveyor has trouble with long strings > 16K, so split.

--- tools/generate_license_headers.cpp
+++ tools/generate_license_headers.cpp
@@ -3,26 +3,47 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <array>
+#include <sstream>
+#include <iomanip>
+#include <locale>
 
-static const auto ALNUM = std::string("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
-static const char * delim = "=====#_#-#_#";
+// iswalnum will backstab
+static const std::wstring SAFE_CHARS = L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-',.() ";
 
 int main(int argc, char ** argv) {
 	if (argc < 4 || argc % 2 != 0) {
 		std::cerr << (argc > 0 ? argv[0] : "<null>") << ": Error: expected at least three arguments: one header output path and several pairs of (text file path, display name)" << std::endl;
 		return -1;
 	}
-	std::vector<std::pair<std::string, std::string>> inputs;
+	std::vector<std::pair<char *, char *>> inputs;
 	for (int i = 2; i < argc; i += 2)
 		inputs.push_back(std::make_pair(argv[i], argv[i+1]));
 
-	std::ofstream output(argv[1]);
+	std::wofstream output(argv[1]);
 	output << "#include<C4Licenses.h>" << std::endl
 		   << "const std::vector<OCLicenseInfo> OCLicenses {" << std::endl;
 
-	for(const auto& input : inputs)
-		output << std::endl << "{ \"" << input.first << "\", \"" << input.second << "\", "
-			   << "R\"" << delim << "(" << std::ifstream(input.first).rdbuf() << ")" << delim << "\" }," << std::endl;
+	for(const auto& input : inputs) {
+		output << std::endl << "\t{ \"" << input.first << "\", \"" << input.second << "\", std::string()" << std::endl;
+		std::wifstream input_file(input.first);
+		input_file.imbue(std::locale("C.UTF-8"));
+		std::wstring line;
+		while (std::getline(input_file, line)) {
+			std::wstringstream out;
+			for (const auto c : line) {
+				if (SAFE_CHARS.find(c) != std::wstring::npos)
+					out << c;
+				else
+					out << "\\U" << std::setfill(L'0') << std::setw(8) << std::right << std::hex << +c;
+			}
+			output << "\t\t+ std::string(\"" << out.str() << "\\n\")" << std::endl;
+		}
+		if (!input_file.eof()) {
+			std::cerr << "Failure reading " << input.first << std::endl;
+		}
+		output << "\t}," << std::endl;
+	}
 
 	output << std::endl << "};" << std::endl;
 	return 0;
