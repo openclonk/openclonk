5737af90d8054c27147e5cdcb1c57b75746518a8
Lukas Werling <lukas@lwrl.de>
2025-01-11 23:55:07 +0100

    Fix build with miniupnpc 2.2.8

--- src/network/C4Network2UPnPLinux.cpp
+++ src/network/C4Network2UPnPLinux.cpp
@@ -77,7 +77,12 @@ void C4Network2UPnPP::Init()
 	if ((devlist = upnpDiscover(2000, nullptr, nullptr, UPNP_LOCAL_PORT_ANY, 0, 2, &error)))
 #endif
 	{
+#if MINIUPNPC_API_VERSION >= 18
+		char wanaddr[64];
+		if ((status = UPNP_GetValidIGD(devlist, &upnp_urls, &igd_data, lanaddr, sizeof(lanaddr), wanaddr, sizeof(wanaddr))))
+#else
 		if ((status = UPNP_GetValidIGD(devlist, &upnp_urls, &igd_data, lanaddr, sizeof(lanaddr))))
+#endif
 		{
 			ThreadLogS("UPnP: Found IGD %s (status %d)", upnp_urls.controlURL, status);
 			initialized = true;
